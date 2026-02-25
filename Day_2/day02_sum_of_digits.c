60d32b8ae2fe9a5f1fab2dd7ffb1369a34610ce7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-02-25 23:28:02 +0530

    added logic to calculate sum of digits

--- Day_2/day02_sum_of_digits.c
+++ Day_2/day02_sum_of_digits.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n, sum = 0;
+
     printf("Enter a number: ");
     scanf("%d", &n);
+
+    while (n > 0) {
+        sum += n % 10;
+        n = n / 10;
+    }
+
+    printf("Sum of digits = %d", sum);
     return 0;
 }
