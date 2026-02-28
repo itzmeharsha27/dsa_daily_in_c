fa91e7f70be55a3b46445089aa832121261a37e0
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-02-28 22:34:57 +0530

    basic structure for palindrome check

--- Day_4/day04_palindrome_number.c
+++ Day_4/day04_palindrome_number.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n, original, reverse = 0;
+
     printf("Enter a number: ");
     scanf("%d", &n);
+
+    original = n;
+
+    while (n > 0) {
+        reverse = reverse * 10 + (n % 10);
+        n = n / 10;
+    }
+
+    if (original == reverse)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
     return 0;
 }
