10c02a73c75917feac61b7e90ad57f71ee828a5f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-02 20:26:07 +0530

    added cube sum logic for Armstrong check

--- Day_6/armstrong_number.c
+++ Day_6/armstrong_number.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n, original, sum = 0;
+
     printf("Enter a number: ");
     scanf("%d", &n);
+
+    original = n;
+
+    while (n > 0) {
+        int digit = n % 10;
+        sum += digit * digit * digit;   // cube of digit
+        n = n / 10;
+    }
+
+    if (sum == original)
+        printf("Armstrong Number\n");
+    else
+        printf("Not an Armstrong Number\n");
+
     return 0;
 }
