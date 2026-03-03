1f7d078068f7bd4497b11340fde0c9503da5c951
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-03 23:39:58 +0530

    added nested loop to check primes in range

--- Day_7/prime_numbers_in_range.c
+++ Day_7/prime_numbers_in_range.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    int start, end;
+    int start, end, i, j, flag;
 
     printf("Enter start and end: ");
     scanf("%d %d", &start, &end);
+
+    for(i = start; i <= end; i++) {
+        flag = 0;
+
+        if(i <= 1)
+            continue;
+
+        for(j = 2; j < i; j++) {
+            if(i % j == 0) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag == 0)
+            printf("%d ", i);
+    }
 
     return 0;
 }
