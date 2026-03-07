c120db94e15cb6686a120f8264b2b2fdd182c8c9
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-07 22:50:29 +0530

    added outer loop for pattern rows

--- Day_9/inverted_triangle_star_pattern.c
+++ Day_9/inverted_triangle_star_pattern.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n, i;
 
     printf("Enter number of rows: ");
     scanf("%d", &n);
+
+    for(i = n; i >= 1; i--) {
+        printf("\n");
+    }
 
     return 0;
 }
