a8624288751b8a4cf8f989621cee8850bbc9c588
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-07 22:50:51 +0530

    added inner loop to print stars

--- Day_9/inverted_triangle_star_pattern.c
+++ Day_9/inverted_triangle_star_pattern.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int n, i;
+    int n, i, j;
 
     printf("Enter number of rows: ");
     scanf("%d", &n);
 
     for(i = n; i >= 1; i--) {
+
+        for(j = 1; j <= i; j++) {
+            printf("* ");
+        }
+
         printf("\n");
     }
 
