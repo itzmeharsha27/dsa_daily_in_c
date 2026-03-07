1248a9180b8cef8a5039c15ab60f73c343993807
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-07 22:51:08 +0530

    handled invalid row input

--- Day_9/inverted_triangle_star_pattern.c
+++ Day_9/inverted_triangle_star_pattern.c
@@ -6,6 +6,11 @@ int main() {
     printf("Enter number of rows: ");
     scanf("%d", &n);
 
+    if(n <= 0) {
+        printf("Enter a positive number\n");
+        return 0;
+    }
+
     for(i = n; i >= 1; i--) {
 
         for(j = 1; j <= i; j++) {
