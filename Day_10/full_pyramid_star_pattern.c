a5642055e43ebdb7f041e550d3048dde1a711162
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-08 22:57:19 +0530

    added comments explaining pyramid logic

--- Day_10/full_pyramid_star_pattern.c
+++ Day_10/full_pyramid_star_pattern.c
@@ -3,9 +3,11 @@
 int main() {
     int n, i, j;
 
+    // Take number of rows from user
     printf("Enter number of rows: ");
     scanf("%d", &n);
 
+    // Validate input
     if(n <= 0) {
         printf("Enter a positive number\n");
         return 0;
@@ -13,16 +15,20 @@ int main() {
 
     printf("\nFull Pyramid Pattern:\n\n");
 
+    // Outer loop controls rows
     for(i = 1; i <= n; i++) {
 
+        // Print spaces before stars
         for(j = 1; j <= n - i; j++) {
             printf(" ");
         }
 
+        // Print stars
         for(j = 1; j <= i; j++) {
             printf("* ");
         }
 
+        // Move to next line
         printf("\n");
     }
 
