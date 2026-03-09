f9474885429489eef2c4229e68ad742d8eaca1b5
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-09 21:25:07 +0530

    added input validation

--- Day_11/number_pyramid_pattern.c
+++ Day_11/number_pyramid_pattern.c
@@ -6,6 +6,11 @@ int main() {
     printf("Enter number of rows: ");
     scanf("%d", &n);
 
+    if(n <= 0) {
+        printf("Enter a positive number\n");
+        return 0;
+    }
+
     for(i = 1; i <= n; i++) {
 
         for(j = 1; j <= i; j++) {
