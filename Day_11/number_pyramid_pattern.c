363c86a9459c1168bafc619f6b3d0109ae9ddd25
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-09 21:26:30 +0530

    added comments explaining nested loops

--- Day_11/number_pyramid_pattern.c
+++ Day_11/number_pyramid_pattern.c
@@ -3,9 +3,11 @@
 int main() {
     int n, i, j;
 
+    // Get number of rows
     printf("Enter number of rows: ");
     scanf("%d", &n);
 
+    // Validate input
     if(n <= 0) {
         printf("Enter a positive number\n");
         return 0;
@@ -13,8 +15,10 @@ int main() {
 
     printf("\nNumber Pyramid Pattern:\n\n");
 
+    // Outer loop controls rows
     for(i = 1; i <= n; i++) {
 
+        // Inner loop prints numbers
         for(j = 1; j <= i; j++) {
             printf("%d ", j);
         }
