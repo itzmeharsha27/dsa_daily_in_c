9f85e5cb13182379cad23a98e2124ad53697feef
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-11 00:08:26 +0530

    added variables for largest and second largest

--- Day_13/second_largest_array.c
+++ Day_13/second_largest_array.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n, i;
     int arr[100];
+    int largest, second;
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
+
+    printf("Enter elements:\n");
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    largest = arr[0];
+    second = -999999;
 
     return 0;
 }
