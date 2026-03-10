84876706e7bf8ec4598e24466150a44c61f10f6b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-10 23:52:03 +0530

    added validation for array size

--- Day_12/largest_element_array.c
+++ Day_12/largest_element_array.c
@@ -8,6 +8,11 @@ int main() {
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
+    if(n <= 0 || n > 100) {
+        printf("Invalid number of elements\n");
+        return 0;
+    }
+
     printf("Enter elements:\n");
 
     for(i = 0; i < n; i++) {
