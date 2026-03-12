d8d72ded814c2d48499060c3b6ce3106757b05a9
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-12 22:59:26 +0530

    added comments explaining reverse array logic

--- Day_14/reverse_array.c
+++ Day_14/reverse_array.c
@@ -5,22 +5,25 @@ int main() {
     int n, i;
     int arr[100];
 
+    // Input size
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
+    // Validate size
     if(n <= 0 || n > 100)
     {
         printf("Invalid array size\n");
         return 0;
     }
 
+    // Input elements
     printf("Enter elements:\n");
-
     for(i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
 
+    // Reverse array using swapping
     for(i = 0; i < n/2; i++)
     {
         int temp = arr[i];
@@ -28,8 +31,8 @@ int main() {
         arr[n-i-1] = temp;
     }
 
+    // Print reversed array
     printf("Reversed array:\n");
-
     for(i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
