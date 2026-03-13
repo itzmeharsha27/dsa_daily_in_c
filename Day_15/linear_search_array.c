95c15141de6f05cc3fb1f0788a0b2c4c4fa627ca
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-13 23:03:16 +0530

    added variable for search element

--- Day_15/linear_search_array.c
+++ Day_15/linear_search_array.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    int n,i;
+    int n,i,key;
     int arr[100];
 
     printf("Enter number of elements: ");
@@ -15,12 +15,8 @@ int main() {
         scanf("%d",&arr[i]);
     }
 
-    printf("Array elements:\n");
-
-    for(i=0;i<n;i++)
-    {
-        printf("%d ",arr[i]);
-    }
+    printf("Enter element to search: ");
+    scanf("%d",&key);
 
     return 0;
 }
