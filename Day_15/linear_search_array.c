146c53c7abe324e286581f11cea4a452b563b010
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-13 23:07:05 +0530

    added comments

--- Day_15/linear_search_array.c
+++ Day_15/linear_search_array.c
@@ -2,41 +2,41 @@
 
 int main() {
 
-    int n,i,key,found=0;
-    int arr[100];
+    int n,i,key,found=0;   // n = number of elements, i = loop variable, key = element to search, found = flag
+    int arr[100];          // array to store maximum 100 elements
 
     printf("Enter number of elements: ");
-    scanf("%d",&n);
+    scanf("%d",&n);        // reading number of elements
 
-    if(n<=0 || n>100)
+    if(n<=0 || n>100)      // checking if array size is valid
     {
         printf("Invalid array size\n");
-        return 0;
+        return 0;          // stop program if size is invalid
     }
 
     printf("Enter elements:\n");
 
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++)       // loop to take array elements from user
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&arr[i]);  // storing each element in array
     }
 
     printf("Enter element to search: ");
-    scanf("%d",&key);
+    scanf("%d",&key);      // reading element to be searched
 
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++)       // loop to check each element in array
     {
-        if(arr[i]==key)
+        if(arr[i]==key)    // comparing array element with key
         {
-            printf("Element found at position %d\n",i+1);
-            found=1;
+            printf("Element found at position %d\n",i+1); // printing position
+            found=1;       // setting found flag to true
         }
     }
 
-    if(found==0)
+    if(found==0)           // if element was not found in array
     {
         printf("Element not found\n");
     }
 
-    return 0;
+    return 0;              // end of program
 }
