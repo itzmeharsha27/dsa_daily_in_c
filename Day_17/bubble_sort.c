688305d4549bb8df36e6f82d9e3735f7851697da
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-15 15:27:52 +0530

    added final bubble sort implementation with comments

--- Day_17/bubble_sort.c
+++ Day_17/bubble_sort.c
@@ -2,47 +2,52 @@
 
 int main() {
 
-    int n,i;
+    int n,i,j,temp;
     int arr[100];
-    int j,temp;
 
     printf("Enter number of elements: ");
     scanf("%d",&n);
 
+    if(n<=0 || n>100)
+    {
+        printf("Invalid array size\n");
+        return 0;
+    }
+
     printf("Enter elements:\n");
-  printf("\nSorted array:\n");
-
-for(i=0;i<n;i++)
-{
-    printf("%d ",arr[i]);
-}
-for(j=0;j<n-i-1;j++)
-{
-
-}
-if(n<=0 || n>100)
-{
-    printf("Invalid array size\n");
-    return 0;
-}
-if(arr[j] > arr[j+1])
-{
-    temp = arr[j];
-arr[j] = arr[j+1];
-arr[j+1] = temp;
-
-}
 
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
+
     printf("Original array:\n");
 
-for(i=0;i<n;i++)
-{
-    printf("%d ",arr[i]);
-}
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    // Bubble Sort algorithm
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    printf("\nSorted array:\n");
+
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
 
     return 0;
 }
