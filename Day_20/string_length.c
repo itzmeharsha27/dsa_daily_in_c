34b9b9b6cb70f16f32a066f938db11cb98c6503e
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-18 23:21:29 +0530

    added final string length program with comments

--- Day_20/string_length.c
+++ Day_20/string_length.c
@@ -6,34 +6,31 @@ int main()
     int i;
 
     printf("Enter a string: ");
-    scanf("%s", str);
 
+    // Read full line including spaces
+    fgets(str, sizeof(str), stdin);
+
+    // Check empty input
+    if(str[0] == '\0')
+    {
+        printf("Empty string\n");
+        return 0;
+    }
+
+    // Find length
     for(i = 0; str[i] != '\0'; i++)
     {
 
     }
 
-    printf("Length = %d\n", i);
-
-    fgets(str, sizeof(str), stdin);
-
+    // Remove newline if present
     if(str[i-1] == '\n')
-{
-    str[i-1] = '\0';
-    i--;
-}
+    {
+        str[i-1] = '\0';
+        i--;
+    }
 
-if(str[0] == '\0')
-{
-    printf("Empty string\n");
-    return 0;
-}
-
-// Traverse string until null character
-for(i = 0; str[i] != '\0'; i++)
-{
-
-}
+    printf("Length = %d\n", i);
 
     return 0;
 }
