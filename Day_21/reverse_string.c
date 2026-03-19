aebb303d4846360792970a6ef9752edfe2036bde
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-19 22:51:14 +0530

    added final reverse string implementation with comments

--- Day_21/reverse_string.c
+++ Day_21/reverse_string.c
@@ -3,40 +3,47 @@
 int main()
 {
     char str[100];
+    int i, start, end;
+    char temp;
 
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
-    int i;
-    int start = 0;
-int end = i - 1;
-char temp;
 
- if(str[0] == '\0')
+    // Check empty string
+    if(str[0] == '\0')
     {
         printf("Empty string\n");
         return 0;
     }
 
-for(i = 0; str[i] != '\0'; i++)
-{
-  
+    // Find length
+    for(i = 0; str[i] != '\0'; i++)
+    {
+
+    }
+
+    // Remove newline if present
     if(str[i-1] == '\n')
-{
-    str[i-1] = '\0';
-    i--;
-}
+    {
+        str[i-1] = '\0';
+        i--;
+    }
 
-}
-// Reverse string using two-pointer technique
-while(start < end)
-{
-    temp = str[start];
-    str[start] = str[end];
-    str[end] = temp;
+    start = 0;
+    end = i - 1;
 
-    start++;
-    end--;
-}
-printf("Reversed string: %s\n", str);
+    // Reverse string using two-pointer approach
+    while(start < end)
+    {
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    printf("Reversed string: %s\n", str);
+
     return 0;
 }
