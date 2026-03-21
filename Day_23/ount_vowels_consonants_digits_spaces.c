b1c089aeb55407e98ce76ad3725f64805ace26fc
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-21 22:55:25 +0530

    added vowel counting logic

--- Day_23/ount_vowels_consonants_digits_spaces.c
+++ Day_23/ount_vowels_consonants_digits_spaces.c
@@ -8,6 +8,11 @@ int vowels = 0, consonants = 0, digits = 0, spaces = 0;
 
 for(i = 0; str[i] != '\0'; i++)
 {
+    if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||
+   str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')
+{
+    vowels++;
+}
 
 }
 
