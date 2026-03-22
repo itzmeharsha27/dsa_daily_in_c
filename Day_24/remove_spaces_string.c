6639b3b7738e54f51eea62d2233e5537612f10fb
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-22 23:54:09 +0530

    removed newline character from input

--- Day_24/remove_spaces_string.c
+++ Day_24/remove_spaces_string.c
@@ -5,6 +5,16 @@ int main()
     char str[200];
     int i, j;
 
+
+    for(i = 0; str[i] != '\0'; i++)
+{
+    if(str[i] == '\n')
+    {
+        str[i] = '\0';
+        break;
+    }
+}
+
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
 
