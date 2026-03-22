f6af9c4bbf74010c13fef9b6f00ed834ae79d200
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-22 23:57:03 +0530

    added final remove spaces program with comments

--- Day_24/remove_spaces_string.c
+++ Day_24/remove_spaces_string.c
@@ -5,47 +5,40 @@ int main()
     char str[200];
     int i, j;
 
-
-    for(i = 0; str[i] != '\0'; i++)
-{
-    if(str[i] == '\n')
-    {
-        str[i] = '\0';
-        break;
-    }
-}
-
-for(i = 0; str[i] != '\0'; i++)
-{
-    if(str[i] == ' ')
-    {
-        if(str[0] == '\0')
-{
-    printf("Empty string\n");
-    return 0;
-}
-    }
-}
-// Remove spaces by shifting characters
-for(i = 0; str[i] != '\0'; i++)
-{
-    if(str[i] == ' ')
-    {
-        for(j = i; str[j] != '\0'; j++)
-        {
-            str[j] = str[j+1];
-        }
-        i--;
-    }
-}
-for(j = i; str[j] != '\0'; j++)
-{
-    str[j] = str[j+1];
-    i--;
-    printf("String without spaces: %s\n", str);
-}
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
+
+    // Check empty input
+    if(str[0] == '\0')
+    {
+        printf("Empty string\n");
+        return 0;
+    }
+
+    // Remove newline
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] == '\n')
+        {
+            str[i] = '\0';
+            break;
+        }
+    }
+
+    // Remove spaces by shifting characters
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] == ' ')
+        {
+            for(j = i; str[j] != '\0'; j++)
+            {
+                str[j] = str[j+1];
+            }
+            i--;
+        }
+    }
+
+    printf("String without spaces: %s\n", str);
 
     return 0;
 }
