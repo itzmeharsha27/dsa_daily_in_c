7d35ad176130b9e86bdbb0759d4ed80f48dc71d7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-22 23:55:13 +0530

    added logic to shift characters left

--- Day_24/remove_spaces_string.c
+++ Day_24/remove_spaces_string.c
@@ -22,6 +22,10 @@ for(i = 0; str[i] != '\0'; i++)
         
     }
 }
+for(j = i; str[j] != '\0'; j++)
+{
+    str[j] = str[j+1];
+}
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
 
