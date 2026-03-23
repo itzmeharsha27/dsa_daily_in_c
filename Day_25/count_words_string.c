b00697f09f6d1f082194f32cbd6fe4a95c825fe8
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-23 19:53:39 +0530

    improved word count to handle multiple spaces

--- Day_25/count_words_string.c
+++ Day_25/count_words_string.c
@@ -4,18 +4,18 @@ int main()
 {
     char str[200];
     int i, words = 0;
+    int inWord = 0;
 
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
 
-    // Check empty input
     if(str[0] == '\0')
     {
         printf("Empty string\n");
         return 0;
     }
 
-    // Remove newline character
+    // Remove newline
     for(i = 0; str[i] != '\0'; i++)
     {
         if(str[i] == '\n')
@@ -25,12 +25,17 @@ int main()
         }
     }
 
-    // Count words using space detection
+    // Count words ignoring multiple spaces
     for(i = 0; str[i] != '\0'; i++)
     {
-        if(str[i] != ' ' && (i == 0 || str[i-1] == ' '))
+        if(str[i] != ' ' && inWord == 0)
         {
             words++;
+            inWord = 1;
+        }
+        else if(str[i] == ' ')
+        {
+            inWord = 0;
         }
     }
 
