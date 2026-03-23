c69867167b203b773d784b8230a32cc56080dc0b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-23 19:49:46 +0530

    removed newline character

--- Day_25/count_words_string.c
+++ Day_25/count_words_string.c
@@ -10,7 +10,11 @@ int main()
 
     for(i = 0; str[i] != '\0'; i++)
     {
-
+        if(str[i] == '\n')
+        {
+            str[i] = '\0';
+            break;
+        }
     }
 
     return 0;
