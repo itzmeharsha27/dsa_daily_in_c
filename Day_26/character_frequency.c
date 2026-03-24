ff0033df6c455645fcc6ee61acc61ed5efbaf3d5
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-24 18:35:29 +0530

    added validation for empty string

--- Day_26/character_frequency.c
+++ Day_26/character_frequency.c
@@ -9,7 +9,12 @@ int main()
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
 
-    // Remove newline
+    if(str[0] == '\0')
+    {
+        printf("Empty string\n");
+        return 0;
+    }
+
     for(i = 0; str[i] != '\0'; i++)
     {
         if(str[i] == '\n')
@@ -17,6 +22,11 @@ int main()
             str[i] = '\0';
             break;
         }
+    }
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        freq[str[i]]++;
     }
 
     return 0;
