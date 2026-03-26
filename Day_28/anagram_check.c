7b76df3a798c68f636d63e5e24e88eff9e3e10c2
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-26 18:48:33 +0530

    basic structure for anagram check program

---
+++ Day_28/anagram_check.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char str1[100], str2[100];
+
+    printf("Enter first string: ");
+    fgets(str1, sizeof(str1), stdin);
+
+    printf("Enter second string: ");
+    fgets(str2, sizeof(str2), stdin);
+
+    return 0;
+}
