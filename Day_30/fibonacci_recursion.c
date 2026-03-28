07aee700a3014602208f4b82052abaa8c6f81393
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-28 22:18:29 +0530

    added fibonacci function structure

--- Day_30/fibonacci_recursion.c
+++ Day_30/fibonacci_recursion.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int fib(int n);
+
 int main()
 {
     int n;
@@ -7,5 +9,10 @@ int main()
     printf("Enter number of terms: ");
     scanf("%d", &n);
 
+    return 0;
+}
+
+int fib(int n)
+{
     return 0;
 }
