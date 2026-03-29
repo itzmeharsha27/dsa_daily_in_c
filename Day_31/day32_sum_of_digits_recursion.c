b0c72465e9756f1e76b7152a0a20d5ea1799f94a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-29 18:26:30 +0530

    added recursive function structure

--- Day_31/day32_sum_of_digits_recursion.c
+++ Day_31/day32_sum_of_digits_recursion.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int sum(int n);
+
 int main()
 {
     int n;
@@ -7,5 +9,10 @@ int main()
     printf("Enter a number: ");
     scanf("%d", &n);
 
+    return 0;
+}
+
+int sum(int n)
+{
     return 0;
 }
