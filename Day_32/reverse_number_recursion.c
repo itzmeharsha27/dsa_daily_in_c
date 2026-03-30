60cf400a6bfafd04c03bc197d9dd62fb5f44ec8e
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-30 18:15:33 +0530

    added validation for negative number

--- Day_32/reverse_number_recursion.c
+++ Day_32/reverse_number_recursion.c
@@ -16,6 +16,12 @@ int main()
     printf("Enter a number: ");
     scanf("%d", &n);
 
+    if(n < 0)
+    {
+        printf("Invalid input\n");
+        return 0;
+    }
+
     result = reverse(n, 0);
     printf("Reversed number = %d\n", result);
 
