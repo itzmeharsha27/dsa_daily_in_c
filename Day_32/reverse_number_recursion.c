7cfe32a177537964f6196d1a0cc4ad80f3714131
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-30 18:13:37 +0530

    added function call and output

--- Day_32/reverse_number_recursion.c
+++ Day_32/reverse_number_recursion.c
@@ -4,10 +4,13 @@ int reverse(int n);
 
 int main()
 {
-    int n;
+    int n, result;
 
     printf("Enter a number: ");
     scanf("%d", &n);
+
+    result = reverse(n);
+    printf("Reversed number = %d\n", result);
 
     return 0;
 }
