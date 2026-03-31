3b87b785ee57223040aed1b1cf0051b2ddfca427
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-03-31 20:29:04 +0530

    added reverse function call

--- Day_33/palindrome_number_recursion.c
+++ Day_33/palindrome_number_recursion.c
@@ -4,10 +4,12 @@ int reverse(int n, int rev);
 
 int main()
 {
-    int n;
+    int n, revNum;
 
     printf("Enter a number: ");
     scanf("%d", &n);
+
+    revNum = reverse(n, 0);
 
     return 0;
 }
