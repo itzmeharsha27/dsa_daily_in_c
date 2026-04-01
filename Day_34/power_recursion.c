7661127569511502bc3e599f3470829d59df8d8b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-01 20:29:25 +0530

    added function call and output

--- Day_34/power_recursion.c
+++ Day_34/power_recursion.c
@@ -4,10 +4,13 @@ int powerFunc(int base, int power);
 
 int main()
 {
-    int base, power;
+    int base, power, result;
 
     printf("Enter base and power: ");
     scanf("%d %d", &base, &power);
+
+    result = powerFunc(base, power);
+    printf("Result = %d\n", result);
 
     return 0;
 }
