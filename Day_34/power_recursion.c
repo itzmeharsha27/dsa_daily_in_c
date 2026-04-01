c217346894f4d7b9565c31b074a9f9f9f18a0a11
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-01 20:29:05 +0530

    added power function structure

--- Day_34/power_recursion.c
+++ Day_34/power_recursion.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int powerFunc(int base, int power);
+
 int main()
 {
     int base, power;
@@ -7,5 +9,10 @@ int main()
     printf("Enter base and power: ");
     scanf("%d %d", &base, &power);
 
+    return 0;
+}
+
+int powerFunc(int base, int power)
+{
     return 0;
 }
