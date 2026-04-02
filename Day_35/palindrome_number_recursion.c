42c4f08ba8fe35acd4220f0178ece02031068e1b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-02 22:51:21 +0530

    final palindrome number recursion program

--- Day_35/palindrome_number_recursion.c
+++ Day_35/palindrome_number_recursion.c
@@ -3,9 +3,11 @@
 // Function to reverse digits using recursion
 int reverse(int n, int rev)
 {
+    // Base case
     if(n == 0)
         return rev;
 
+    // Recursive case
     return reverse(n/10, rev*10 + n%10);
 }
 
@@ -16,6 +18,7 @@ int main()
     printf("Enter a number: ");
     scanf("%d", &n);
 
+    // Validate input
     if(n < 0)
     {
         printf("Invalid input\n");
