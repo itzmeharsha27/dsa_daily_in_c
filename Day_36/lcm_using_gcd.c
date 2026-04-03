a67307eaf9c032603e15e24d813eed297ea9fc49
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-03 18:40:41 +0530

    added lcm calculation using gcd

--- Day_36/lcm_using_gcd.c
+++ Day_36/lcm_using_gcd.c
@@ -4,10 +4,14 @@ int gcd(int a, int b);
 
 int main()
 {
-    int a, b;
+    int a, b, lcm;
 
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
+
+    lcm = (a * b) / gcd(a, b);
+
+    printf("LCM = %d\n", lcm);
 
     return 0;
 }
