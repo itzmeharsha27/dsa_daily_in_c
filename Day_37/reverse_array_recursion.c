1dfa4fb5a919d7df4f516fe30367d7641985dcba
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-04 23:35:04 +0530

    added swapping logic

--- Day_37/reverse_array_recursion.c
+++ Day_37/reverse_array_recursion.c
@@ -16,6 +16,10 @@ int main()
 
     reverse(arr, 0, n-1);
 
+    printf("Reversed array:\n");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
     return 0;
 }
 
@@ -23,4 +27,8 @@ void reverse(int arr[], int start, int end)
 {
     if(start >= end)
         return;
+
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
 }
