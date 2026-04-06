5ed8e726933f6ad1ce63cb04452a91aa2bb9d001
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-06 19:49:31 +0530

    added recursive binary search logic

--- Day_39/binary_search_recursion.c
+++ Day_39/binary_search_recursion.c
@@ -19,6 +19,11 @@ int main()
 
     pos = binarySearch(arr, 0, n-1, key);
 
+    if(pos == -1)
+        printf("Element not found\n");
+    else
+        printf("Element found at position %d\n", pos + 1);
+
     return 0;
 }
 
@@ -26,4 +31,13 @@ int binarySearch(int arr[], int low, int high, int key)
 {
     if(low > high)
         return -1;
+
+    int mid = (low + high) / 2;
+
+    if(arr[mid] == key)
+        return mid;
+    else if(arr[mid] > key)
+        return binarySearch(arr, low, mid - 1, key);
+    else
+        return binarySearch(arr, mid + 1, high, key);
 }
