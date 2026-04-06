11fa0b4076d3c30705f222144021cb583ee852fd
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-06 19:47:21 +0530

    added array input and key input

--- Day_39/binary_search_recursion.c
+++ Day_39/binary_search_recursion.c
@@ -4,10 +4,18 @@ int binarySearch(int arr[], int low, int high, int key);
 
 int main()
 {
-    int n;
+    int n, i, key;
+    int arr[100];
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
+
+    printf("Enter sorted elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
 
     return 0;
 }
