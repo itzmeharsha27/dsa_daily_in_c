2e8a551169dd4817dcf873b2e87d372bd71eb410
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-07 18:54:33 +0530

    added array input

--- Day_40/check_sorted_array_recursion.c
+++ Day_40/check_sorted_array_recursion.c
@@ -4,10 +4,15 @@ int isSorted(int arr[], int n);
 
 int main()
 {
-    int n;
+    int n, i;
+    int arr[100];
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
 
     return 0;
 }
