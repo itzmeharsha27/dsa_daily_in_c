9023fd5fdd4ee4006a40326882b617001640e453
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-09 19:04:36 +0530

    added base case

--- Day_42/sum_array_recursion.c
+++ Day_42/sum_array_recursion.c
@@ -1,2 +1,26 @@
-git add .
-git commit -m "added array input"
+#include <stdio.h>
+
+int sumArray(int arr[], int n);
+
+int main()
+{
+    int n, i, sum;
+    int arr[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    sum = sumArray(arr, n);
+
+    return 0;
+}
+
+int sumArray(int arr[], int n)
+{
+    if(n == 0)
+        return 0;
+}
