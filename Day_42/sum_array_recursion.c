05c38f62ff1028880180134076fddb884f61921d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-09 19:02:53 +0530

    added array input

--- Day_42/sum_array_recursion.c
+++ Day_42/sum_array_recursion.c
@@ -1,18 +1,2 @@
-#include <stdio.h>
-
-int sumArray(int arr[], int n);
-
-int main()
-{
-    int n;
-
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
-
-    return 0;
-}
-
-int sumArray(int arr[], int n)
-{
-    return 0;
-}
+git add .
+git commit -m "added array input"
