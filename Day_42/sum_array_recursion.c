22e75e6a529749482d85de963c5446709c1e53b9
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-09 19:14:56 +0530

    final sum array recursion program

--- Day_42/sum_array_recursion.c
+++ Day_42/sum_array_recursion.c
@@ -16,7 +16,44 @@ int main()
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
+#include <stdio.h>
 
+// Function to calculate sum of array using recursion
+int sumArray(int arr[], int n)
+{
+    // Base case
+    if(n == 0)
+        return 0;
+
+    // Recursive case
+    return arr[n-1] + sumArray(arr, n-1);
+}
+
+int main()
+{
+    int n, i, sum;
+    int arr[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    // Validate input
+    if(n <= 0)
+    {
+        printf("Invalid input\n");
+        return 0;
+    }
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    sum = sumArray(arr, n);
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
     if(n <= 0)
     {
         printf("Invalid input\n");
