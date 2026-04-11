32e23b534b280d1dfa68f5a22b918d98aa798566
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-11 23:17:52 +0530

    added base case

--- Day_44/last_occurrence_recursion.c
+++ Day_44/last_occurrence_recursion.c
@@ -4,7 +4,7 @@ int lastOcc(int arr[], int n, int key, int index);
 
 int main()
 {
-    int n, i, key;
+    int n, i, key, pos;
     int arr[100];
 
     printf("Enter number of elements: ");
@@ -17,10 +17,13 @@ int main()
     printf("Enter element to search: ");
     scanf("%d", &key);
 
+    pos = lastOcc(arr, n, key, 0);
+
     return 0;
 }
 
 int lastOcc(int arr[], int n, int key, int index)
 {
-    return -1;
+    if(index >= n)
+        return -1;
 }
