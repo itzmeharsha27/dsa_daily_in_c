c225587223ba3f67053bb5058a9cf82fe6698d52
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-11 23:20:22 +0530

    added validation for array size

--- Day_44/last_occurrence_recursion.c
+++ Day_44/last_occurrence_recursion.c
@@ -19,11 +19,17 @@ int lastOcc(int arr[], int n, int key, int index)
 
 int main()
 {
-    int n, i, key,  pos;
+    int n, i, key, pos;
     int arr[100];
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
+
+    if(n <= 0)
+    {
+        printf("Invalid input\n");
+        return 0;
+    }
 
     printf("Enter elements:\n");
     for(i = 0; i < n; i++)
