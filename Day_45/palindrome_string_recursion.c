c3c67b48f36e0847f111609534b39ffd6e3ffdd5
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-12 20:03:12 +0530

    added string length calculation

--- Day_45/palindrome_string_recursion.c
+++ Day_45/palindrome_string_recursion.c
@@ -6,9 +6,12 @@ int isPalindrome(char str[], int start, int end);
 int main()
 {
     char str[100];
+    int len;
 
     printf("Enter a string: ");
     scanf("%s", str);
+
+    len = strlen(str);
 
     return 0;
 }
