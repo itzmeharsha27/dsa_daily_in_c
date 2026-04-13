abd17b436f8ab09af1cf3c0b119cf45062adfe9d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-13 19:23:19 +0530

    added comments explaining recursion

--- Day_46/remove_duplicates_recursion.c
+++ Day_46/remove_duplicates_recursion.c
@@ -3,18 +3,21 @@
 // Array to track visited characters
 int visited[256] = {0};
 
-// Recursive function
+// Function to remove duplicates using recursion
 void removeDup(char str[], int index)
 {
+    // Base case
     if(str[index] == '\0')
         return;
 
+    // If not visited
     if(visited[str[index]] == 0)
     {
         printf("%c", str[index]);
         visited[str[index]] = 1;
     }
 
+    // Recursive call
     removeDup(str, index + 1);
 }
 
