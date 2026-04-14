d69564349025946b68745ab8dfa0e9266d67ecc7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-14 11:26:37 +0530

    added function call with result array

--- Day_47/generate_subsets_recursion.c
+++ Day_47/generate_subsets_recursion.c
@@ -5,9 +5,12 @@ void subsets(char str[], char result[], int i, int j);
 int main()
 {
     char str[100];
+    char result[100];
 
     printf("Enter a string: ");
     scanf("%s", str);
+
+    subsets(str, result, 0, 0);
 
     return 0;
 }
