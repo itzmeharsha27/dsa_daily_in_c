e98209b748a0bd970aebd3f997e2fd2cf7d1cf03
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-15 15:15:02 +0530

    added base case

--- Day_48/permutations_recursion.c
+++ Day_48/permutations_recursion.c
@@ -13,9 +13,16 @@ int main()
 
     len = strlen(str);
 
+    permute(str, 0, len - 1);
+
     return 0;
 }
 
 void permute(char str[], int l, int r)
 {
+    if(l == r)
+    {
+        printf("%s\n", str);
+        return;
+    }
 }
