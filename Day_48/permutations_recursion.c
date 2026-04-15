aebe37d707252a3a0fcfa791ab3544060a94df1b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-15 15:15:25 +0530

    reorganized function placement

--- Day_48/permutations_recursion.c
+++ Day_48/permutations_recursion.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void permute(char str[], int l, int r);
-
-int main()
-{
-    char str[100];
-    int len;
-
-    printf("Enter a string: ");
-    scanf("%s", str);
-
-    len = strlen(str);
-
-    permute(str, 0, len - 1);
-
-    return 0;
-}
-
+// Function to generate permutations
 void permute(char str[], int l, int r)
 {
     if(l == r)
@@ -39,4 +23,19 @@ void permute(char str[], int l, int r)
         str[l] = str[i];
         str[i] = temp;
     }
+}
+
+int main()
+{
+    char str[100];
+    int len;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+    permute(str, 0, len - 1);
+
+    return 0;
 }
