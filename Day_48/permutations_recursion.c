3e89cc83598e8281e72ca925e00f74e569b5ee97
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-15 15:14:51 +0530

    added string length

--- Day_48/permutations_recursion.c
+++ Day_48/permutations_recursion.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 void permute(char str[], int l, int r);
 
 int main()
 {
     char str[100];
+    int len;
 
     printf("Enter a string: ");
     scanf("%s", str);
+
+    len = strlen(str);
 
     return 0;
 }
