b4a20580530760ea778f05f8ab3fa1c1946a75df
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-16 23:30:08 +0530

    added board and function call

--- Day_49/nqueens_basic.c
+++ Day_49/nqueens_basic.c
@@ -4,10 +4,12 @@ void solve(int board[10][10], int n, int row);
 
 int main()
 {
-    int n;
+    int n, board[10][10] = {0};
 
     printf("Enter number of queens: ");
     scanf("%d", &n);
+
+    solve(board, n, 0);
 
     return 0;
 }
