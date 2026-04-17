a504b8a15527d2be038dc684cc8c2ac3d8e33ca3
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-17 11:24:54 +0530

    added n display

--- Day_50/rat_in_maze.c
+++ Day_50/rat_in_maze.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Safety check
 int isSafe(int maze[10][10], int x, int y, int n)
 {
     if(x >= 0 && y >= 0 && x < n && y < n && maze[x][y] == 1)
@@ -8,7 +7,6 @@ int isSafe(int maze[10][10], int x, int y, int n)
     return 0;
 }
 
-// Backtracking function
 void solve(int maze[10][10], int sol[10][10], int x, int y, int n)
 {
     if(x == n-1 && y == n-1)
@@ -40,19 +38,15 @@ int main()
 {
     int n, maze[10][10], sol[10][10] = {0};
 
-    printf("Enter size of maze: ");
+    printf("Enter size: ");
     scanf("%d", &n);
-
-    if(n <= 0)
-    {
-        printf("Invalid input\n");
-        return 0;
-    }
 
     printf("Enter maze:\n");
     for(int i=0;i<n;i++)
         for(int j=0;j<n;j++)
             scanf("%d",&maze[i][j]);
+
+    printf("N = %d\n", n);
 
     solve(maze, sol, 0, 0, n);
 
