f3386b1ac95c6a159fa4d564c2c5ce567b6ae195
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-17 11:12:18 +0530

    added solve function structure

--- Day_50/rat_in_maze.c
+++ Day_50/rat_in_maze.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void solve(int maze[10][10], int sol[10][10], int x, int y, int n);
+
 int main()
 {
     int n;
@@ -8,4 +10,8 @@ int main()
     scanf("%d", &n);
 
     return 0;
+}
+
+void solve(int maze[10][10], int sol[10][10], int x, int y, int n)
+{
 }
