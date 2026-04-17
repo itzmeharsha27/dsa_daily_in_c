53f02d4f7c23f47197c9b3ea058ca05bde52fb42
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-17 11:12:36 +0530

    added maze input and call

--- Day_50/rat_in_maze.c
+++ Day_50/rat_in_maze.c
@@ -4,10 +4,17 @@ void solve(int maze[10][10], int sol[10][10], int x, int y, int n);
 
 int main()
 {
-    int n;
+    int n, maze[10][10], sol[10][10] = {0};
 
     printf("Enter size of maze: ");
     scanf("%d", &n);
+
+    printf("Enter maze:\n");
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            scanf("%d",&maze[i][j]);
+
+    solve(maze, sol, 0, 0, n);
 
     return 0;
 }
