d282cf533c2c40d09917b6f668ea1f283d85a639
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-18 21:21:07 +0530

    added grid input

--- Day_51/sudoku_solver.c
+++ Day_51/sudoku_solver.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
+int solve(int grid[9][9]);
+
 int main()
 {
     int grid[9][9];
 
     printf("Enter Sudoku grid:\n");
+    for(int i=0;i<9;i++)
+        for(int j=0;j<9;j++)
+            scanf("%d",&grid[i][j]);
+
+    solve(grid);
+
+    return 0;
+}
+
+int solve(int grid[9][9])
+{
     return 0;
 }
