f965e75268790e01fbd7bf3b6f61f79c84782202
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-18 21:23:54 +0530

    final sudoku solver backtracking program

--- Day_51/sudoku_solver.c
+++ Day_51/sudoku_solver.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
+// Check if placing number is valid
 int isSafe(int grid[9][9], int row, int col, int num)
 {
+    // Row & column check
     for(int x=0;x<9;x++)
         if(grid[row][x]==num || grid[x][col]==num)
             return 0;
 
+    // 3x3 box check
     int startRow = row - row%3;
     int startCol = col - col%3;
 
@@ -17,6 +20,7 @@ int isSafe(int grid[9][9], int row, int col, int num)
     return 1;
 }
 
+// Backtracking Sudoku solver
 int solve(int grid[9][9])
 {
     for(int i=0;i<9;i++)
@@ -30,9 +34,11 @@ int solve(int grid[9][9])
                     if(isSafe(grid,i,j,num))
                     {
                         grid[i][j]=num;
+
                         if(solve(grid))
                             return 1;
-                        grid[i][j]=0;
+
+                        grid[i][j]=0; // backtrack
                     }
                 }
                 return 0;
@@ -46,21 +52,27 @@ int main()
 {
     int grid[9][9];
 
-    printf("Enter Sudoku:\n");
+    printf("Enter Sudoku grid:\n");
+
     for(int i=0;i<9;i++)
         for(int j=0;j<9;j++)
             scanf("%d",&grid[i][j]);
 
-    printf("Start = %d\n", grid[0][0]);
-
+    // Solve Sudoku
     if(solve(grid))
     {
+        printf("Solved Sudoku:\n");
+
         for(int i=0;i<9;i++)
         {
             for(int j=0;j<9;j++)
                 printf("%d ",grid[i][j]);
             printf("\n");
         }
+    }
+    else
+    {
+        printf("No solution exists\n");
     }
 
     return 0;
