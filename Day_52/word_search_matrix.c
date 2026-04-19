d2c2a4bb12557dcc31aad928170afbcf70afecaf
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-19 10:29:40 +0530

    reorganized function placement

--- Day_52/word_search_matrix.c
+++ Day_52/word_search_matrix.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int search(char grid[10][10], int n, int m, char word[], int i, int j, int k);
+// Recursive search function
+int search(char grid[10][10], int n, int m, char word[], int i, int j, int k)
+{
+    if(word[k] == '\0')
+        return 1;
+
+    if(i<0 || j<0 || i>=n || j>=m || grid[i][j] != word[k])
+        return 0;
+
+    char temp = grid[i][j];
+    grid[i][j] = '#';
+
+    int res = search(grid,n,m,word,i+1,j,k+1) ||
+              search(grid,n,m,word,i-1,j,k+1) ||
+              search(grid,n,m,word,i,j+1,k+1) ||
+              search(grid,n,m,word,i,j-1,k+1);
+
+    grid[i][j] = temp;
+
+    return res;
+}
 
 int main()
 {
@@ -21,39 +41,14 @@ int main()
     int found = 0;
 
     for(int i=0;i<n;i++)
-    {
         for(int j=0;j<m;j++)
-        {
             if(search(grid,n,m,word,i,j,0))
                 found = 1;
-        }
-    }
 
     if(found)
         printf("Found\n");
     else
-        printf("Not Found\n");
+        printf("Not  Found\n");
 
     return 0;
-}
-
-int search(char grid[10][10], int n, int m, char word[], int i, int j, int k)
-{
-    if(word[k] == '\0')
-        return 1;
-
-    if(i<0 || j<0 || i>=n || j>=m || grid[i][j] != word[k])
-        return 0;
-
-    char temp = grid[i][j];
-    grid[i][j] = '#';
-
-    int res = search(grid,n,m,word,i+1,j,k+1) ||
-              search(grid,n,m,word,i-1,j,k+1) ||
-              search(grid,n,m,word,i,j+1,k+1) ||
-              search(grid,n,m,word,i,j-1,k+1);
-
-    grid[i][j] = temp;
-
-    return res;
 }
