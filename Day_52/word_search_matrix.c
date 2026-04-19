b6d238fda9b1d7df10b2a6461d4681dc6ccf7ee9
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-19 10:27:33 +0530

    added function structure

--- Day_52/word_search_matrix.c
+++ Day_52/word_search_matrix.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int search(char grid[10][10], int n, int m, char word[], int i, int j, int k);
+
 int main()
 {
     int n, m;
@@ -7,5 +9,10 @@ int main()
     printf("Enter rows and cols: ");
     scanf("%d %d", &n, &m);
 
+    return 0;
+}
+
+int search(char grid[10][10], int n, int m, char word[], int i, int j, int k)
+{
     return 0;
 }
