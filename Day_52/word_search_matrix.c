ce683ce0dc66a3353f1cc22574245671d3b87f37
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-19 10:27:45 +0530

    added grid and word input

--- Day_52/word_search_matrix.c
+++ Day_52/word_search_matrix.c
@@ -5,9 +5,18 @@ int search(char grid[10][10], int n, int m, char word[], int i, int j, int k);
 int main()
 {
     int n, m;
+    char grid[10][10], word[100];
 
     printf("Enter rows and cols: ");
     scanf("%d %d", &n, &m);
+
+    printf("Enter grid:\n");
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+            scanf(" %c",&grid[i][j]);
+
+    printf("Enter word: ");
+    scanf("%s", word);
 
     return 0;
 }
