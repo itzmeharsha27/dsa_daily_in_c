daf51df219a2e409594e189a52654b923a49dc49
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-19 10:28:15 +0530

    added backtracking search logic

--- Day_52/word_search_matrix.c
+++ Day_52/word_search_matrix.c
@@ -18,6 +18,22 @@ int main()
     printf("Enter word: ");
     scanf("%s", word);
 
+    int found = 0;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(search(grid,n,m,word,i,j,0))
+                found = 1;
+        }
+    }
+
+    if(found)
+        printf("Found\n");
+    else
+        printf("Not Found\n");
+
     return 0;
 }
 
@@ -25,4 +41,19 @@ int search(char grid[10][10], int n, int m, char word[], int i, int j, int k)
 {
     if(word[k] == '\0')
         return 1;
+
+    if(i<0 || j<0 || i>=n || j>=m || grid[i][j] != word[k])
+        return 0;
+
+    char temp = grid[i][j];
+    grid[i][j] = '#';
+
+    int res = search(grid,n,m,word,i+1,j,k+1) ||
+              search(grid,n,m,word,i-1,j,k+1) ||
+              search(grid,n,m,word,i,j+1,k+1) ||
+              search(grid,n,m,word,i,j-1,k+1);
+
+    grid[i][j] = temp;
+
+    return res;
 }
