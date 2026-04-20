35c83f08eb32820f8297bd3edc29a03cc3994976
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-20 23:13:47 +0530

    added dfs function structure

--- Day_53/graph_dfs_bfs.c
+++ Day_53/graph_dfs_bfs.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void dfs(int graph[10][10], int visited[], int node, int n);
+
 int main()
 {
     int n;
@@ -8,4 +10,8 @@ int main()
     scanf("%d", &n);
 
     return 0;
+}
+
+void dfs(int graph[10][10], int visited[], int node, int n)
+{
 }
