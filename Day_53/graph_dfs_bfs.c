d0d0f17e2ac6ee22f01a3b2b04e24057af41c4ac
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-20 23:14:08 +0530

    added basic dfs visit

--- Day_53/graph_dfs_bfs.c
+++ Day_53/graph_dfs_bfs.c
@@ -14,6 +14,7 @@ int main()
         for(int j=0;j<n;j++)
             scanf("%d",&graph[i][j]);
 
+    printf("DFS: ");
     dfs(graph, visited, 0, n);
 
     return 0;
@@ -21,4 +22,6 @@ int main()
 
 void dfs(int graph[10][10], int visited[], int node, int n)
 {
+    visited[node] = 1;
+    printf("%d ", node);
 }
