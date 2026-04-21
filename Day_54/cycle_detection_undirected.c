a3893089f6b04c712078b523feafd5c4de843b4d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-21 20:26:40 +0530

    added graph input and dfs call

--- Day_54/cycle_detection_undirected.c
+++ Day_54/cycle_detection_undirected.c
@@ -4,10 +4,17 @@ int dfs(int graph[10][10], int visited[], int node, int parent, int n);
 
 int main()
 {
-    int n;
+    int n, graph[10][10], visited[10] = {0};
 
     printf("Enter number of vertices: ");
     scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            scanf("%d",&graph[i][j]);
+
+    dfs(graph, visited, 0, -1, n);
 
     return 0;
 }
