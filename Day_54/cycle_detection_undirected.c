0110bcadc7fc54952452ab437673a4a19b471493
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-21 20:26:26 +0530

    added dfs function structure

--- Day_54/cycle_detection_undirected.c
+++ Day_54/cycle_detection_undirected.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int dfs(int graph[10][10], int visited[], int node, int parent, int n);
+
 int main()
 {
     int n;
@@ -7,5 +9,10 @@ int main()
     printf("Enter number of vertices: ");
     scanf("%d", &n);
 
+    return 0;
+}
+
+int dfs(int graph[10][10], int visited[], int node, int parent, int n)
+{
     return 0;
 }
