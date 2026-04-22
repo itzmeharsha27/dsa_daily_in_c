6f41a2ed2c2e810f83313ede95761917ae7efd3d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-22 20:19:38 +0530

    final LCA binary tree program

--- Day_55/owest_common_ancestor.c
+++ Day_55/owest_common_ancestor.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Node structure
 struct Node
 {
     int data;
     struct Node *left, *right;
 };
 
+// Create node
 struct Node* createNode(int val)
 {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
@@ -15,25 +17,30 @@ struct Node* createNode(int val)
     return newNode;
 }
 
+// Find Lowest Common Ancestor
 struct Node* LCA(struct Node* root, int p, int q)
 {
     if(root == NULL)
         return NULL;
 
+    // If either p or q matches root
     if(root->data == p || root->data == q)
         return root;
 
     struct Node* left = LCA(root->left, p, q);
     struct Node* right = LCA(root->right, p, q);
 
+    // If both sides return non-null → root is LCA
     if(left && right)
         return root;
 
+    // Otherwise return non-null side
     return left ? left : right;
 }
 
 int main()
 {
+    // Build tree
     struct Node* root = createNode(3);
     root->left = createNode(5);
     root->right = createNode(1);
@@ -46,7 +53,7 @@ int main()
 
     struct Node* ans = LCA(root, p, q);
 
-    printf("Answer: %d\n", ans->data);
+    printf("Lowest Common Ancestor: %d\n", ans->data);
 
     return 0;
 }
