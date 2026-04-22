7e9cbc678950dd7ec97380102b1c431c531b517a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-22 20:18:40 +0530

    added comments

--- Day_55/owest_common_ancestor.c
+++ Day_55/owest_common_ancestor.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// LCA in binary tree
 struct Node
 {
     int data;
     struct Node *left, *right;
 };
 
-struct Node* createNode(int val);
-struct Node* LCA(struct Node* root, int p, int q);
-
-int main()
+struct Node* createNode(int val)
 {
-    struct Node* root = createNode(3);
-    root->left = createNode(5);
-    root->right = createNode(1);
-    root->left->left = createNode(6);
-    root->left->right = createNode(2);
-
-    return 0;
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
 }
 
 struct Node* LCA(struct Node* root, int p, int q)
@@ -36,4 +31,15 @@ struct Node* LCA(struct Node* root, int p, int q)
         return root;
 
     return left ? left : right;
+}
+
+int main()
+{
+    struct Node* root = createNode(3);
+    root->left = createNode(5);
+    root->right = createNode(1);
+    root->left->left = createNode(6);
+    root->left->right = createNode(2);
+
+    return 0;
 }
