710b993f4bb93a9079a1febe848b9fc5b71bc9b6
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-23 19:53:11 +0530

    added comments

--- Day_56/diameter_binary_tree.c
+++ Day_56/diameter_binary_tree.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Diameter of binary tree
+
 struct Node
 {
     int data;
     struct Node *left, *right;
 };
 
-struct Node* createNode(int val);
-int height(struct Node* root);
-int diameter(struct Node* root);
-
-int main()
+struct Node* createNode(int val)
 {
-    struct Node* root = createNode(1);
-    root->left = createNode(2);
-    root->right = createNode(3);
-    root->left->left = createNode(4);
-    root->left->right = createNode(5);
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
 
-    return 0;
+int height(struct Node* root)
+{
+    if(root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return (left > right ? left : right) + 1;
 }
 
 int diameter(struct Node* root)
@@ -41,4 +47,15 @@ int diameter(struct Node* root)
         return leftD;
     else
         return rightD;
+}
+
+int main()
+{
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    return 0;
 }
