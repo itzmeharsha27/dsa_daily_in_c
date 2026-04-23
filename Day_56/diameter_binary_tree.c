f874e107149a27f922859a53ca155db60c8b47c9
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-23 19:51:11 +0530

    added node creation function

--- Day_56/diameter_binary_tree.c
+++ Day_56/diameter_binary_tree.c
@@ -7,7 +7,17 @@ struct Node
     struct Node *left, *right;
 };
 
+struct Node* createNode(int val);
+
 int main()
 {
     return 0;
+}
+
+struct Node* createNode(int val)
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
 }
