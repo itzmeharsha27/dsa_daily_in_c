d208fb808fb455649b127def1660f80ca101e99c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-24 18:20:45 +0530

    added optimized diameter logic

--- Day_57/iameter_optimized.c
+++ Day_57/iameter_optimized.c
@@ -21,5 +21,25 @@ int main()
 
 int diameterUtil(struct Node* root, int* height)
 {
-    return 0;
+    if(root == NULL)
+    {
+        *height = 0;
+        return 0;
+    }
+
+    int lh = 0, rh = 0;
+
+    int ld = diameterUtil(root->left, &lh);
+    int rd = diameterUtil(root->right, &rh);
+
+    *height = (lh > rh ? lh : rh) + 1;
+
+    int curr = lh + rh + 1;
+
+    if(curr > ld && curr > rd)
+        return curr;
+    else if(ld > rd)
+        return ld;
+    else
+        return rd;
 }
