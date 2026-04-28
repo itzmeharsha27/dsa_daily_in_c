f7f870d11c85a97cceed1e273185c292c22590f2
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-28 14:37:13 +0530

    added comments

--- Day_60/balanced_tree_optimized.c
+++ Day_60/balanced_tree_optimized.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Optimized balance check
+
 struct Node
 {
     int data;
     struct Node *left, *right;
 };
 
-struct Node* createNode(int val);
-int checkHeight(struct Node* root);
-
-int main()
+struct Node* createNode(int val)
 {
-    struct Node* root = createNode(1);
-    root->left = createNode(2);
-    root->left->left = createNode(3);
-
-    return 0;
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    n->data = val;
+    n->left = n->right = NULL;
+    return n;
 }
 
 int checkHeight(struct Node* root)
@@ -34,4 +32,12 @@ int checkHeight(struct Node* root)
         return -1;
 
     return (lh > rh ? lh : rh) + 1;
+}
+
+int main()
+{
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+
+    return 0;
 }
