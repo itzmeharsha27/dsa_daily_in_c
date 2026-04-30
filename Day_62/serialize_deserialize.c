896db6a8394b49091d2688077c67b6f488a5dea4
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-30 21:48:29 +0530

    added comments

--- Day_62/serialize_deserialize.c
+++ Day_62/serialize_deserialize.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Serialize & Deserialize
+
 struct Node
 {
     int data;
     struct Node *left, *right;
 };
 
-struct Node* createNode(int val);
-void serialize(struct Node* root);
-struct Node* deserialize(int arr[], int* index);
-
-int main()
+struct Node* createNode(int val)
 {
-    struct Node* root = createNode(1);
-    root->left = createNode(2);
-    root->right = createNode(3);
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    n->data = val;
+    n->left = n->right = NULL;
+    return n;
+}
 
-    serialize(root);
+void serialize(struct Node* root)
+{
+    if(root == NULL)
+    {
+        printf("# ");
+        return;
+    }
 
-    return 0;
+    printf("%d ", root->data);
+    serialize(root->left);
+    serialize(root->right);
 }
 
 struct Node* deserialize(int arr[], int* index)
@@ -37,4 +45,15 @@ struct Node* deserialize(int arr[], int* index)
     root->right = deserialize(arr, index);
 
     return root;
+}
+
+int main()
+{
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    serialize(root);
+
+    return 0;
 }
