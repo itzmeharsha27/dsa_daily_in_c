9f697a9920028f767242bea5d20e4c4a3e51c887
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-30 21:46:31 +0530

    added node creation

--- Day_62/serialize_deserialize.c
+++ Day_62/serialize_deserialize.c
@@ -7,7 +7,17 @@ struct Node
     struct Node *left, *right;
 };
 
+struct Node* createNode(int val);
+
 int main()
 {
     return 0;
+}
+
+struct Node* createNode(int val)
+{
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    n->data = val;
+    n->left = n->right = NULL;
+    return n;
 }
