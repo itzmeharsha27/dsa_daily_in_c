d9e0c6fdbe2a287ec17a6c85cecc5871b51ff96a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-04-30 21:49:17 +0530

    final serialize deserialize program

--- Day_62/serialize_deserialize.c
+++ Day_62/serialize_deserialize.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Node structure
 struct Node
 {
     int data;
     struct Node *left, *right;
 };
 
+// Create node
 struct Node* createNode(int val)
 {
     struct Node* n = (struct Node*)malloc(sizeof(struct Node));
@@ -15,6 +17,7 @@ struct Node* createNode(int val)
     return n;
 }
 
+// Serialize (preorder)
 void serialize(struct Node* root)
 {
     if(root == NULL)
@@ -28,14 +31,34 @@ void serialize(struct Node* root)
     serialize(root->right);
 }
 
+// Deserialize using array (-1 = NULL)
+struct Node* deserialize(int arr[], int* index)
+{
+    if(arr[*index] == -1)
+    {
+        (*index)++;
+        return NULL;
+    }
+
+    struct Node* root = createNode(arr[*index]);
+    (*index)++;
+
+    root->left = deserialize(arr, index);
+    root->right = deserialize(arr, index);
+
+    return root;
+}
+
 int main()
 {
+    // Example tree
     struct Node* root = createNode(1);
     root->left = createNode(2);
     root->right = createNode(3);
     root->right->left = createNode(4);
+    root->right->right = createNode(5);
 
-    printf("Serialized: ");
+    printf("Serialized Tree: ");
     serialize(root);
 
     return 0;
