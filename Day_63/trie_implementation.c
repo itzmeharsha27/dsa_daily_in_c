b981a5654722a395f20260fd457f065d35615719
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-01 11:51:14 +0530

    added search function structure

--- Day_63/trie_implementation.c
+++ Day_63/trie_implementation.c
@@ -9,6 +9,7 @@ struct Trie
 
 struct Trie* createNode();
 void insert(struct Trie* root, char* word);
+int search(struct Trie* root, char* word);
 
 int main()
 {
@@ -19,19 +20,7 @@ int main()
     return 0;
 }
 
-void insert(struct Trie* root, char* word)
+int search(struct Trie* root, char* word)
 {
-    struct Trie* curr = root;
-
-    for(int i=0; word[i]!='\0'; i++)
-    {
-        int index = word[i] - 'a';
-
-        if(curr->children[index] == NULL)
-            curr->children[index] = createNode();
-
-        curr = curr->children[index];
-    }
-
-    curr->isEnd = 1;
+    return 0;
 }
