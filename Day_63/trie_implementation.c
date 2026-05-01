e655bdd294ee5e6b454548f6b6b2d69af27d1866
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-01 11:50:33 +0530

    added node creation

--- Day_63/trie_implementation.c
+++ Day_63/trie_implementation.c
@@ -7,7 +7,20 @@ struct Trie
     int isEnd;
 };
 
+struct Trie* createNode();
+
 int main()
 {
     return 0;
+}
+
+struct Trie* createNode()
+{
+    struct Trie* node = (struct Trie*)malloc(sizeof(struct Trie));
+    node->isEnd = 0;
+
+    for(int i=0;i<26;i++)
+        node->children[i] = NULL;
+
+    return node;
 }
