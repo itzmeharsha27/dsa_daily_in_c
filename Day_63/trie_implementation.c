2d0d007c861a7114159f8e3581b1f715dcb9a965
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-01 11:50:58 +0530

    implemented insert function

--- Day_63/trie_implementation.c
+++ Day_63/trie_implementation.c
@@ -14,9 +14,24 @@ int main()
 {
     struct Trie* root = createNode();
 
+    insert(root, "cat");
+
     return 0;
 }
 
 void insert(struct Trie* root, char* word)
 {
+    struct Trie* curr = root;
+
+    for(int i=0; word[i]!='\0'; i++)
+    {
+        int index = word[i] - 'a';
+
+        if(curr->children[index] == NULL)
+            curr->children[index] = createNode();
+
+        curr = curr->children[index];
+    }
+
+    curr->isEnd = 1;
 }
