748963eb5180f4b8898a2c8591f96afc1792c244
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-02 21:38:19 +0530

    added function structure

--- Day_64/word_break.c
+++ Day_64/word_break.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+int wordBreak(char str[], char dict[][20], int size);
+
 int main()
 {
     char str[100];
     scanf("%s", str);
 
+    return 0;
+}
+
+int wordBreak(char str[], char dict[][20], int size)
+{
     return 0;
 }
