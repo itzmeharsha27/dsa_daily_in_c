5e54623a2876c6f98c3ac44ec3a848f3676fb73d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-03 20:17:58 +0530

    added node creation

--- Day_65/lru_cache.c
+++ Day_65/lru_cache.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Node
 struct Node
 {
     int key, value;
     struct Node *prev, *next;
 };
 
-// Cache structure
 struct LRU
 {
     int capacity;
     struct Node *head, *tail;
 };
 
+struct Node* createNode(int key, int value);
+
 int main()
 {
     return 0;
+}
+
+struct Node* createNode(int key, int value)
+{
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    n->key = key;
+    n->value = value;
+    n->prev = n->next = NULL;
+    return n;
 }
