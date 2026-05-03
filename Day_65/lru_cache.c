875cf1b7833f7f2bcb2ce4f125737dd74d930701
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-03 20:19:54 +0530

    added hashmap array

--- Day_65/lru_cache.c
+++ Day_65/lru_cache.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE 100
+
 struct Node
 {
     int key, value;
@@ -11,26 +13,10 @@ struct LRU
 {
     int capacity;
     struct Node *head, *tail;
+    struct Node* map[SIZE];
 };
-
-struct Node* createNode(int key, int value);
-void insertFront(struct LRU* cache, struct Node* node);
-void removeNode(struct LRU* cache, struct Node* node);
 
 int main()
 {
     return 0;
-}
-
-void removeNode(struct LRU* cache, struct Node* node)
-{
-    if(node->prev)
-        node->prev->next = node->next;
-    else
-        cache->head = node->next;
-
-    if(node->next)
-        node->next->prev = node->prev;
-    else
-        cache->tail = node->prev;
 }
