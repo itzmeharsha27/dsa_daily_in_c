0b922cc438058a066231e5364cc2a93c208a28ef
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-03 20:24:25 +0530

    final LRU cache implementation

--- Day_65/lru_cache.c
+++ Day_65/lru_cache.c
@@ -12,17 +12,37 @@ struct Node
 struct LRU
 {
     int capacity;
+    int count;
     struct Node *head, *tail;
     struct Node* map[SIZE];
 };
 
-void put(struct LRU* cache, int key, int value);
-
-int main()
-{
-    return 0;
-}
+void insertFront(struct LRU* cache, struct Node* node);
+void removeNode(struct LRU* cache, struct Node* node);
 
 void put(struct LRU* cache, int key, int value)
 {
+    if(cache->map[key])
+    {
+        struct Node* node = cache->map[key];
+        node->value = value;
+
+        removeNode(cache, node);
+        insertFront(cache, node);
+    }
+    else
+    {
+        struct Node* node = createNode(key, value);
+
+        if(cache->count == cache->capacity)
+        {
+            cache->map[cache->tail->key] = NULL;
+            removeNode(cache, cache->tail);
+            cache->count--;
+        }
+
+        insertFront(cache, node);
+        cache->map[key] = node;
+        cache->count++;
+    }
 }
