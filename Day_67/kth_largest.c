9eed4d1840c484516c172fe4b7c01e5b29d5291e
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-05 08:23:29 +0530

    implemented kth largest logic

--- Day_67/kth_largest.c
+++ Day_67/kth_largest.c
@@ -9,14 +9,25 @@ struct Heap
     int size;
 };
 
-int kthLargest(int arr[], int n, int k);
+int kthLargest(int arr[], int n, int k)
+{
+    struct Heap h;
+    h.size = 0;
+
+    for(int i=0;i<n;i++)
+    {
+        insert(&h, arr[i]);
+
+        if(h.size > k)
+            extractMin(&h);
+    }
+
+    return h.arr[0];
+}
 
 int main()
 {
-    return 0;
-}
-
-int kthLargest(int arr[], int n, int k)
-{
+    int arr[] = {3,2,1,5,6,4};
+    printf("%d\n", kthLargest(arr, 6, 2));
     return 0;
 }
