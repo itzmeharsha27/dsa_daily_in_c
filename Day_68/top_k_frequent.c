433af7658f1dda69bf4104f96393b70bc418f92b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-07 11:36:33 +0530

    Final cleanup and code formatting

--- Day_68/top_k_frequent.c
+++ Day_68/top_k_frequent.c
@@ -1,37 +1,58 @@
 #include <stdio.h>
 
-int main() {
-    struct Pair arr[100];
-int size = 0;
+// Structure to store number and its frequency
+struct Pair {
+    int num;
+    int count;
+};
 
-for(int i = 0; i < 100; i++) {
-    if(freq[i] > 0) {
-        arr[size].num = i;
-        arr[size].count = freq[i];
-        size++;
-    }
-}
-    int nums[] = {1,1,1,2,2,3};
-    int freq[100] = {0};
+int main() {
+
+    // Input array
+    int nums[] = {1, 1, 1, 2, 2, 3};
     int n = 6;
-for(int i = 0; i < size - 1; i++) {
-    for(int j = 0; j < size - i - 1; j++) {
-        if(arr[j].count < arr[j+1].count) {
-            struct Pair temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
+
+    // Number of top frequent elements required
+    int k = 2;
+
+    // Frequency array (assuming elements range 0–99)
+    int freq[100] = {0};
+
+    // Count frequency of each element
+    for(int i = 0; i < n; i++) {
+        freq[nums[i]]++;
+    }
+
+    // Store frequency data into struct array
+    struct Pair arr[100];
+    int size = 0;
+
+    for(int i = 0; i < 100; i++) {
+        if(freq[i] > 0) {
+            arr[size].num = i;
+            arr[size].count = freq[i];
+            size++;
         }
     }
-}printf("Top %d frequent elements:\n", k);
-for(int i = 0; i < k; i++) {
-    printf("%d ", arr[i].num);
-}
-    for(int i = 0; i < n; i++) {
-        printf("\n");
-    freq[nums[i]]++;// Count frequency of elements
-// Sort based on frequency
-// Print top k elements
-}
-int k = 2;
+
+    // Sort based on frequency (descending) using bubble sort
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = 0; j < size - i - 1; j++) {
+            if(arr[j].count < arr[j + 1].count) {
+                struct Pair temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // Print top k frequent elements
+    printf("Top %d frequent elements:\n", k);
+    for(int i = 0; i < k && i < size; i++) {
+        printf("%d ", arr[i].num);
+    }
+
+    printf("\n");
+
     return 0;
 }
