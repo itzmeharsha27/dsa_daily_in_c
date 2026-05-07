3729b2f54dfab66f7f24e2140ae14386c5ebfd23
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-07 11:35:24 +0530

    Stored frequencies into struct array

--- Day_68/top_k_frequent.c
+++ Day_68/top_k_frequent.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    struct Pair {
-    int num;
-    int count;
-};
+    struct Pair arr[100];
+int size = 0;
+
+for(int i = 0; i < 100; i++) {
+    if(freq[i] > 0) {
+        arr[size].num = i;
+        arr[size].count = freq[i];
+        size++;
+    }
+}
     int nums[] = {1,1,1,2,2,3};
     int freq[100] = {0};
     int n = 6;
