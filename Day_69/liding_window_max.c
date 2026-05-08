32d5bc15174bdf8ced84f12830874234e7722e6c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-08 21:44:11 +0530

    Improve output formatting

--- Day_69/liding_window_max.c
+++ Day_69/liding_window_max.c
@@ -1,10 +1,2 @@
-void slidingWindowMax(int arr[], int n, int k) {
-    for(int i = 0; i <= n - k; i++) {
-        int max = arr[i];
-        for(int j = i; j < i + k; j++) {
-            if(arr[j] > max)
-                max = arr[j];
-        }
-        printf("%d ", max);
-    }
-}
+git add .
+git commit -m "Add brute force sliding window logic"
