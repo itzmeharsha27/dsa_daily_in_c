7244e1ac4cc76d225b160c462fc1731aadc2cc19
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-08 21:44:02 +0530

    Add brute force sliding window logic

--- Day_69/liding_window_max.c
+++ Day_69/liding_window_max.c
@@ -1,2 +1,10 @@
-git add .
-git commit -m "Add function prototype for sliding window max"
+void slidingWindowMax(int arr[], int n, int k) {
+    for(int i = 0; i <= n - k; i++) {
+        int max = arr[i];
+        for(int j = i; j < i + k; j++) {
+            if(arr[j] > max)
+                max = arr[j];
+        }
+        printf("%d ", max);
+    }
+}
