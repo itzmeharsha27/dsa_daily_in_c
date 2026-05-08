7a0d68a5b1bd71bb1fdadec00ea384c4037f8e4d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-08 21:43:19 +0530

    Add input handling for array and k

--- Day_69/liding_window_max.c
+++ Day_69/liding_window_max.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[1000];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
     return 0;
 }
