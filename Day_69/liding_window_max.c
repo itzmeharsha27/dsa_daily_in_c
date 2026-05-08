86df6310488296c67429d2f41ed62f574714099d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-08 21:44:48 +0530

    Add deque-based sliding window logic

--- Day_69/liding_window_max.c
+++ Day_69/liding_window_max.c
@@ -1,4 +1,13 @@
-void slidingWindowMax(int arr[], int n, int k) {
-    int dq[1000];
-    int front = 0, rear = -1;
+for(int i = 0; i < n; i++) {
+
+    while(front <= rear && dq[front] <= i - k)
+        front++;
+
+    while(front <= rear && arr[dq[rear]] <= arr[i])
+        rear--;
+
+    dq[++rear] = i;
+
+    if(i >= k - 1)
+        printf("%d ", arr[dq[front]]);
 }
