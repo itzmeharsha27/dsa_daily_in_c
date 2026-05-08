2faf0790b1705481b08cb58c3584d087bbe7ba56
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-08 21:45:05 +0530

    Finalize output formatting

--- Day_69/liding_window_max.c
+++ Day_69/liding_window_max.c
@@ -1,13 +1,1 @@
-for(int i = 0; i < n; i++) {
-
-    while(front <= rear && dq[front] <= i - k)
-        front++;
-
-    while(front <= rear && arr[dq[rear]] <= arr[i])
-        rear--;
-
-    dq[++rear] = i;
-
-    if(i >= k - 1)
-        printf("%d ", arr[dq[front]]);
-}
+printf("\n");
