c5f7289145d6c94cfa8fcf92af11e8725407c92a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-09 09:51:28 +0530

    added expand window logic

--- Day_70/min_window_substring.c
+++ Day_70/min_window_substring.c
@@ -8,12 +8,21 @@ int main()
     char s[] = "ADOBECODEBANC";
     char t[] = "ABC";
 
-    int freqT[MAX] = {0}, freqS[MAX] = {0};
-    int required = strlen(t);
-    int formed = 0;
+    int freqT[MAX]={0}, freqS[MAX]={0};
+    int required=strlen(t), formed=0;
 
     for(int i=0;t[i];i++)
         freqT[t[i]]++;
+
+    int l=0;
+
+    for(int r=0;s[r];r++)
+    {
+        freqS[s[r]]++;
+
+        if(freqS[s[r]] <= freqT[s[r]])
+            formed++;
+    }
 
     return 0;
 }
