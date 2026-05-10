bee0d444e74402905862781f853498009e6b5d3b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-11 04:20:24 +0530

    tracking max frequency

--- Day_72/longest_repeating_replacement.c
+++ Day_72/longest_repeating_replacement.c
@@ -9,11 +9,14 @@ int main()
     int k = 1;
 
     int freq[MAX] = {0};
-    int l = 0, maxLen = 0;
+    int l = 0, maxLen = 0, maxFreq = 0;
 
     for(int r = 0; s[r]; r++)
     {
         freq[s[r]-'A']++;
+
+        if(freq[s[r]-'A'] > maxFreq)
+            maxFreq = freq[s[r]-'A'];
     }
 
     return 0;
