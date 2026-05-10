8e2832e06e7617ca8ff680aef9077ff1314ef2b6
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-10 19:44:54 +0530

    added output

--- Day_71/longest_unique_substring.c
+++ Day_71/longest_unique_substring.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-// Longest Unique Substring
-
 #define MAX 256
 
 int main()
@@ -26,6 +24,8 @@ int main()
         if(len > maxLen)
             maxLen = len;
     }
+
+    printf("Length: %d\n", maxLen);
 
     return 0;
 }
