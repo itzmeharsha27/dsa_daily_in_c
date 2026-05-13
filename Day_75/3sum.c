945a18b4073eb14125c89e7d2ae80fce04d77d9b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-13 14:58:20 +0530

    added sort function structure

--- Day_75/3sum.c
+++ Day_75/3sum.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+void sort(int arr[], int n);
+
 int main()
 {
     int arr[] = {-1,0,1,2,-1,-4};
     int n = 6;
 
+    sort(arr, n);
+
     return 0;
+}
+
+void sort(int arr[], int n)
+{
 }
