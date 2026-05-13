9faf3cd7f5b79a97d2d97725b0e584b64a71f851
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-13 14:59:08 +0530

    added sum logic

--- Day_75/3sum.c
+++ Day_75/3sum.c
@@ -12,6 +12,11 @@ int main()
     for(int i=0;i<n;i++)
     {
         int l = i+1, r = n-1;
+
+        while(l < r)
+        {
+            int sum = arr[i] + arr[l] + arr[r];
+        }
     }
 
     return 0;
