2634520f037099568f7aeabc79a8f64cbab73128
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-13 14:58:32 +0530

    implemented sorting

--- Day_75/3sum.c
+++ Day_75/3sum.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
-void sort(int arr[], int n);
-
-int main()
-{
-    int arr[] = {-1,0,1,2,-1,-4};
-    int n = 6;
-
-    sort(arr, n);
-
-    return 0;
-}
-
 void sort(int arr[], int n)
 {
+    for(int i=0;i<n;i++)
+        for(int j=i+1;j<n;j++)
+            if(arr[i] > arr[j])
+            {
+                int t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;
+            }
 }
