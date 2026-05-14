38a34c444e21a05c014810a1c812d577f2fcb467
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-14 15:49:33 +0530

    called sort function

--- Day_76/4sum.c
+++ Day_76/4sum.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-void sort(int arr[], int n)
+void sort(int arr[], int n);
+
+int main()
 {
-    for(int i=0;i<n;i++)
-        for(int j=i+1;j<n;j++)
-            if(arr[i] > arr[j])
-            {
-                int t = arr[i];
-                arr[i] = arr[j];
-                arr[j] = t;
-            }
+    int arr[] = {1,0,-1,0,-2,2};
+    int n = 6;
+    int target = 0;
+
+    sort(arr, n);
+
+    return 0;
 }
