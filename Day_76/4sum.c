0138204bc3746c7e19cdb38a00d3a8d61d5a21c6
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-14 15:49:09 +0530

    added sort structure

--- Day_76/4sum.c
+++ Day_76/4sum.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+void sort(int arr[], int n);
+
 int main()
 {
     int arr[] = {1,0,-1,0,-2,2};
     int n = 6;
     int target = 0;
 
+    sort(arr, n);
+
     return 0;
+}
+
+void sort(int arr[], int n)
+{
 }
