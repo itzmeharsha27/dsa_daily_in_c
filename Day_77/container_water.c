163a870f1e0c148f8f08cdb48729d19a219ebab1
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-15 07:59:02 +0530

    final container with most water program

--- Day_77/container_water.c
+++ Day_77/container_water.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
+// Container With Most Water using Two Pointers
+
 int main()
 {
-    int arr[] = {1,1};
-    int n = 2;
+    int arr[] = {1,8,6,2,5,4,8,3,7};
+    int n = 9;
 
     int l = 0, r = n-1;
     int maxArea = 0;
 
+    // Two pointer approach
     while(l < r)
     {
         int height = arr[l] < arr[r] ? arr[l] : arr[r];
@@ -17,13 +20,14 @@ int main()
         if(area > maxArea)
             maxArea = area;
 
+        // Move smaller height pointer
         if(arr[l] < arr[r])
             l++;
         else
             r--;
     }
 
-    printf("Max Area: %d\n", maxArea);
+    printf("Maximum Water Stored: %d\n", maxArea);
 
     return 0;
 }
