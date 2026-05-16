225285a73b8509af9bc5197460319f296a2770a0
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-16 17:55:08 +0530

    final trapping rain water program

--- Day_78/trapping_rain_water.c
+++ Day_78/trapping_rain_water.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
+// Trapping Rain Water using Two Pointers
+
 int main()
 {
-    int arr[] = {4,2,0,3,2,5};
-    int n = 6;
+    int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+    int n = 12;
 
     int l = 0, r = n-1;
     int leftMax = 0, rightMax = 0;
     int water = 0;
 
+    // Two pointer approach
     while(l < r)
     {
         if(arr[l] < arr[r])
@@ -31,7 +34,7 @@ int main()
         }
     }
 
-    printf("Water Trapped: %d\n", water);
+    printf("Total Trapped Water: %d\n", water);
 
     return 0;
 }
