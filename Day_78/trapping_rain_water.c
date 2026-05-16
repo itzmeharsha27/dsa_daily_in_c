48a18e1cfc21a7d30f2597bbd1a3a2a0f8cac532
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-16 17:54:54 +0530

    added new test case

--- Day_78/trapping_rain_water.c
+++ Day_78/trapping_rain_water.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
-    int n = 12;
+    int arr[] = {4,2,0,3,2,5};
+    int n = 6;
 
     int l = 0, r = n-1;
     int leftMax = 0, rightMax = 0;
