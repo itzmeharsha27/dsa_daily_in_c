6a379c7eedac0c9128c33ce4b95b60f1d8d27fd1
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-18 10:41:00 +0530

    implemented histogram logic

--- Day_80/maximal_rectangle.c
+++ Day_80/maximal_rectangle.c
@@ -5,7 +5,33 @@
 int stack[MAX];
 int top = -1;
 
+void push(int val)
+{
+    stack[++top] = val;
+}
+
+int pop()
+{
+    return stack[top--];
+}
+
 int largestArea(int arr[], int n)
 {
-    return 0;
+    int maxArea = 0;
+
+    for(int i=0;i<=n;i++)
+    {
+        while(top != -1 && (i==n || arr[stack[top]] > arr[i]))
+        {
+            int h = arr[pop()];
+            int width = (top == -1) ? i : i - stack[top] - 1;
+            int area = h * width;
+
+            if(area > maxArea)
+                maxArea = area;
+        }
+        push(i);
+    }
+
+    return maxArea;
 }
