bc0c12cc5c7ee98b13f5263d11ebeab56229414f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-18 10:40:44 +0530

    added stack logic

--- Day_80/maximal_rectangle.c
+++ Day_80/maximal_rectangle.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 
-int main()
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int val)
 {
-    int matrix[4][5] = {
-        {1,0,1,0,0},
-        {1,0,1,1,1},
-        {1,1,1,1,1},
-        {1,0,0,1,0}
-    };
+    stack[++top] = val;
+}
 
-    int rows = 4, cols = 5;
-
-    int height[5] = {0};
-
-    for(int i=0;i<rows;i++)
-    {
-        for(int j=0;j<cols;j++)
-        {
-            if(matrix[i][j] == 1)
-                height[j]++;
-            else
-                height[j] = 0;
-        }
-    }
-
-    return 0;
+int pop()
+{
+    return stack[top--];
 }
