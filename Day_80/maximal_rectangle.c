c9048d510d3c29425d4a09b8e4005d41dbee519a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-18 10:40:04 +0530

    added sample matrix

--- Day_80/maximal_rectangle.c
+++ Day_80/maximal_rectangle.c
@@ -2,8 +2,14 @@
 
 int main()
 {
-    int matrix[10][10];
-    int rows, cols;
+    int matrix[4][5] = {
+        {1,0,1,0,0},
+        {1,0,1,1,1},
+        {1,1,1,1,1},
+        {1,0,0,1,0}
+    };
+
+    int rows = 4, cols = 5;
 
     return 0;
 }
