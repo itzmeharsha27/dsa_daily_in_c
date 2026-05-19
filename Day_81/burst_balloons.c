c29652cf379e4716a485af378abdeca845af4fdf
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-19 08:16:13 +0530

    added padding with 1

--- Day_81/burst_balloons.c
+++ Day_81/burst_balloons.c
@@ -7,7 +7,12 @@ int main()
     int nums[] = {3,1,5,8};
     int n = 4;
 
-    int dp[MAX][MAX] = {0};
+    int arr[6];
+    arr[0] = 1;
+    arr[n+1] = 1;
+
+    for(int i=0;i<n;i++)
+        arr[i+1] = nums[i];
 
     return 0;
 }
