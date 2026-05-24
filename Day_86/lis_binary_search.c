2ca8bb060e95b5d99ae2e4363c86494d2e1d59bb
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-24 12:50:56 +0530

    initialized tail

--- Day_86/lis_binary_search.c
+++ Day_86/lis_binary_search.c
@@ -2,7 +2,19 @@
 
 #define MAX 100
 
-int binarySearch(int arr[], int l, int r, int key);
+int binarySearch(int arr[], int l, int r, int key)
+{
+    while(l < r)
+    {
+        int mid = (l + r) / 2;
+
+        if(arr[mid] < key)
+            l = mid + 1;
+        else
+            r = mid;
+    }
+    return l;
+}
 
 int main()
 {
@@ -11,6 +23,8 @@ int main()
 
     int tail[MAX];
     int size = 0;
+
+    tail[size++] = arr[0];
 
     return 0;
 }
