70dc157f83f28d0667b7b318e96b289676e1e7da
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-24 12:50:47 +0530

    linked binary search

--- Day_86/lis_binary_search.c
+++ Day_86/lis_binary_search.c
@@ -2,16 +2,15 @@
 
 #define MAX 100
 
-int binarySearch(int arr[], int l, int r, int key)
-{
-    while(l < r)
-    {
-        int mid = (l + r) / 2;
+int binarySearch(int arr[], int l, int r, int key);
 
-        if(arr[mid] < key)
-            l = mid + 1;
-        else
-            r = mid;
-    }
-    return l;
+int main()
+{
+    int arr[] = {10,9,2,5,3,7,101,18};
+    int n = 8;
+
+    int tail[MAX];
+    int size = 0;
+
+    return 0;
 }
