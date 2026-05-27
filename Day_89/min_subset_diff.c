2ac6ad9be3208188f49ce0beda3a19d91641e7fd
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-27 18:48:17 +0530

    computed total sum

--- Day_89/min_subset_diff.c
+++ Day_89/min_subset_diff.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+#define MAX 100
+
 int main()
 {
     int arr[] = {1,6,11,5};
     int n = 4;
+
+    int sum = 0;
+
+    for(int i=0;i<n;i++)
+        sum += arr[i];
 
     return 0;
 }
