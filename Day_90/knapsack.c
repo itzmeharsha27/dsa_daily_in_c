cfe9572675c58101d4399a70733fcaaa45f04f40
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-28 18:13:21 +0530

    added dp table

--- Day_90/knapsack.c
+++ Day_90/knapsack.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+#define MAX 100
+
 int main()
 {
     int wt[] = {1,3,4,5};
     int val[] = {1,4,5,7};
     int n = 4;
     int W = 7;
+
+    int dp[MAX][MAX];
 
     return 0;
 }
