4921fa326f2319172ec97e051887a1918b9cabe9
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-28 18:14:57 +0530

    clean final knapsack version

--- Day_90/knapsack.c
+++ Day_90/knapsack.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// 0/1 Knapsack using DP
+// 0/1 Knapsack (Classic DP Problem)
 
 #define MAX 100
 
@@ -18,7 +18,7 @@ int main()
 
     int dp[MAX][MAX] = {0};
 
-    // DP table
+    // Fill DP table
     for(int i=0;i<=n;i++)
     {
         for(int w=0;w<=W;w++)
@@ -26,13 +26,16 @@ int main()
             if(i==0 || w==0)
                 dp[i][w] = 0;
             else if(wt[i-1] <= w)
-                dp[i][w] = max(val[i-1] + dp[i-1][w-wt[i-1]], dp[i-1][w]);
+                dp[i][w] = max(
+                    val[i-1] + dp[i-1][w-wt[i-1]],  // take item
+                    dp[i-1][w]                      // skip item
+                );
             else
                 dp[i][w] = dp[i-1][w];
         }
     }
 
-    printf("Maximum Value in Knapsack: %d\n", dp[n][W]);
+    printf("Max Profit: %d\n", dp[n][W]);
 
     return 0;
 }
