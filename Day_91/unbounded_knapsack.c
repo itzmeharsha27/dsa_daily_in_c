dec01ec7695836e0140d8b45dd59940a51df6995
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-29 19:53:14 +0530

    final unbounded knapsack program

--- Day_91/unbounded_knapsack.c
+++ Day_91/unbounded_knapsack.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Unbounded Knapsack using DP
+
 #define MAX 100
 
 int max(int a,int b)
@@ -9,13 +11,14 @@ int max(int a,int b)
 
 int main()
 {
-    int wt[] = {1,3,4};
-    int val[] = {10,40,50};
+    int wt[] = {2,3,4};
+    int val[] = {4,5,6};
     int n = 3;
-    int W = 6;
+    int W = 5;
 
     int dp[MAX][MAX] = {0};
 
+    // Fill DP table
     for(int i=0;i<=n;i++)
     {
         for(int w=0;w<=W;w++)
@@ -23,13 +26,16 @@ int main()
             if(i==0 || w==0)
                 dp[i][w] = 0;
             else if(wt[i-1] <= w)
-                dp[i][w] = max(val[i-1] + dp[i][w-wt[i-1]], dp[i-1][w]);
+                dp[i][w] = max(
+                    val[i-1] + dp[i][w-wt[i-1]],  // reuse item
+                    dp[i-1][w]                    // skip item
+                );
             else
                 dp[i][w] = dp[i-1][w];
         }
     }
 
-    printf("Max Value: %d\n", dp[n][W]);
+    printf("Maximum Value (Unbounded Knapsack): %d\n", dp[n][W]);
 
     return 0;
 }
