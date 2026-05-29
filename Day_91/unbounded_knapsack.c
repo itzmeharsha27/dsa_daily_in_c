c707a2fdfc801ecfa85040cd9a795f4127aa6f7d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-29 19:51:42 +0530

    added dp table

--- Day_91/unbounded_knapsack.c
+++ Day_91/unbounded_knapsack.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+#define MAX 100
+
 int main()
 {
     int wt[] = {2,3,4};
     int val[] = {4,5,6};
     int n = 3;
     int W = 5;
+
+    int dp[MAX][MAX];
 
     return 0;
 }
