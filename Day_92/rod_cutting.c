aff9759572b6befd2e836c1af0ba95099d0d9a9c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-30 18:16:42 +0530

    clean final rod cutting version

--- Day_92/rod_cutting.c
+++ Day_92/rod_cutting.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Rod Cutting using Unbounded Knapsack
+// Rod Cutting Problem (Unbounded Knapsack Pattern)
 
 #define MAX 100
 
@@ -12,25 +12,34 @@ int max(int a,int b)
 int main()
 {
     int price[] = {2,5,7,8,10};
-    int n = 5;
+    int n = 5; // rod length
 
     int dp[MAX][MAX] = {0};
 
-    // DP table
+    // Build DP table
     for(int i=0;i<=n;i++)
     {
         for(int len=0;len<=n;len++)
         {
             if(i==0 || len==0)
+            {
                 dp[i][len] = 0;
+            }
             else if(i <= len)
-                dp[i][len] = max(price[i-1] + dp[i][len-i], dp[i-1][len]);
+            {
+                dp[i][len] = max(
+                    price[i-1] + dp[i][len-i],  // cut piece
+                    dp[i-1][len]                // skip
+                );
+            }
             else
+            {
                 dp[i][len] = dp[i-1][len];
+            }
         }
     }
 
-    printf("Maximum Profit from Rod Cutting: %d\n", dp[n][n]);
+    printf("Max Revenue from Rod: %d\n", dp[n][n]);
 
     return 0;
 }
