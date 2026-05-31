82d004e3eba2b4e806447eb58dbe3ea462bb9e79
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-31 09:27:56 +0530

    final coin change program

--- Day_93/coin_change.c
+++ Day_93/coin_change.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Coin Change (Minimum Coins using DP)
+
 #define MAX 100
 #define INF 100000
 
@@ -10,12 +12,13 @@ int min(int a,int b)
 
 int main()
 {
-    int coins[] = {2};
-    int n = 1;
-    int amount = 3;
+    int coins[] = {1,2,5};
+    int n = 3;
+    int amount = 11;
 
     int dp[MAX][MAX];
 
+    // Initialize
     for(int i=0;i<=n;i++)
         for(int j=0;j<=amount;j++)
             dp[i][j] = INF;
@@ -23,21 +26,25 @@ int main()
     for(int i=0;i<=n;i++)
         dp[i][0] = 0;
 
+    // DP computation
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=amount;j++)
         {
             if(coins[i-1] <= j)
-                dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
+                dp[i][j] = min(
+                    1 + dp[i][j-coins[i-1]], // take coin
+                    dp[i-1][j]               // skip
+                );
             else
                 dp[i][j] = dp[i-1][j];
         }
     }
 
     if(dp[n][amount] == INF)
-        printf("Not Possible\n");
+        printf("Minimum Coins: Not Possible\n");
     else
-        printf("Min Coins: %d\n", dp[n][amount]);
+        printf("Minimum Coins Required: %d\n", dp[n][amount]);
 
     return 0;
 }
