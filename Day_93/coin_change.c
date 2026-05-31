7c6f5430d94b9e4424fd1a86ac426e6bd323562c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-31 09:24:48 +0530

    added dp table

--- Day_93/coin_change.c
+++ Day_93/coin_change.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+#define MAX 100
+#define INF 100000
+
 int main()
 {
     int coins[] = {1,2,5};
     int n = 3;
     int amount = 11;
+
+    int dp[MAX][MAX];
 
     return 0;
 }
