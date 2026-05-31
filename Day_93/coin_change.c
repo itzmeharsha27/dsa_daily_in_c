e078b20d387659a9cf1c55d414087a0476890f0b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-05-31 09:26:25 +0530

    implemented dp logic

--- Day_93/coin_change.c
+++ Day_93/coin_change.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Coin Change (Minimum Coins)
+
 #define MAX 100
 #define INF 100000
 
@@ -28,8 +30,9 @@ int main()
         for(int j=1;j<=amount;j++)
         {
             if(coins[i-1] <= j)
-            {
-            }
+                dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
+            else
+                dp[i][j] = dp[i-1][j];
         }
     }
 
