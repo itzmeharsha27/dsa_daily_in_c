9520428041dd64b0b2609cddd5222f5bfafcf7c3
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-01 22:06:41 +0530

    added dp loops

--- Day_94/coin_change_ways.c
+++ Day_94/coin_change_ways.c
@@ -13,5 +13,12 @@ int main()
     for(int i=0;i<=n;i++)
         dp[i][0] = 1;
 
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=amount;j++)
+        {
+        }
+    }
+
     return 0;
 }
