01a54a8196f48b445f5bcc71935d8f559e595308
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-02 17:54:22 +0530

    clean final LCS version

--- Day_95/lcs.c
+++ Day_95/lcs.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-// LCS using DP
+// Longest Common Subsequence (Classic DP Problem)
 
 #define MAX 100
 
@@ -20,19 +20,23 @@ int main()
     int n = strlen(s1);
     int m = strlen(s2);
 
-    // DP computation
+    // Build DP table
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=m;j++)
         {
             if(s1[i-1] == s2[j-1])
+            {
                 dp[i][j] = 1 + dp[i-1][j-1];
+            }
             else
+            {
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
         }
     }
 
-    printf("Longest Common Subsequence Length: %d\n", dp[n][m]);
+    printf("LCS Length Between \"%s\" and \"%s\" = %d\n", s1, s2, dp[n][m]);
 
     return 0;
 }
