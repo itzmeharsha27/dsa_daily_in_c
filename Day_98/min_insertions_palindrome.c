161ffa1ae0401891702d33e6f309e728e66b1c17
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-05 23:10:42 +0530

    computed LPS

--- Day_98/min_insertions_palindrome.c
+++ Day_98/min_insertions_palindrome.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+// Min Insertions to Palindrome
+
 #define MAX 100
+
+int max(int a,int b)
+{
+    return a > b ? a : b;
+}
 
 int main()
 {
@@ -15,6 +22,15 @@ int main()
 
     for(int len=2; len<=n; len++)
     {
+        for(int i=0;i<=n-len;i++)
+        {
+            int j = i + len - 1;
+
+            if(s[i] == s[j])
+                dp[i][j] = 2 + dp[i+1][j-1];
+            else
+                dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+        }
     }
 
     return 0;
