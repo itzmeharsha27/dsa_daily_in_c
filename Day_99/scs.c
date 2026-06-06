83ad5028d4d89d7f0c0174921677f73c9cc25f1a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-06 22:02:04 +0530

    clean final SCS version

--- Day_99/scs.c
+++ Day_99/scs.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-// SCS using LCS
+// Shortest Common Supersequence (via LCS)
 
 #define MAX 100
 
@@ -20,7 +20,7 @@ int main()
     int n = strlen(s1);
     int m = strlen(s2);
 
-    // LCS DP
+    // Compute LCS
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=m;j++)
@@ -32,7 +32,9 @@ int main()
         }
     }
 
-    printf("Shortest Common Supersequence Length: %d\n", n + m - dp[n][m]);
+    int scs_len = n + m - dp[n][m];
+
+    printf("SCS Length for \"%s\" & \"%s\" = %d\n", s1, s2, scs_len);
 
     return 0;
 }
