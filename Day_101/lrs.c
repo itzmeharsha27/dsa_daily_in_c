52d930751e31c25e6b99dee604bea91579a3c97a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-08 17:46:36 +0530

    implemented dp logic

--- Day_101/lrs.c
+++ Day_101/lrs.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+// Longest Repeating Subsequence
+
 #define MAX 100
+
+int max(int a,int b)
+{
+    return a > b ? a : b;
+}
 
 int main()
 {
@@ -15,8 +22,9 @@ int main()
         for(int j=1;j<=n;j++)
         {
             if(s[i-1] == s[j-1] && i != j)
-            {
-            }
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
         }
     }
 
