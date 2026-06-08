7bef81530849bdeda9dae0d9d3451af549c79ab1
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-08 17:44:57 +0530

    added string length

--- Day_101/lrs.c
+++ Day_101/lrs.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX 100
 
@@ -7,6 +8,7 @@ int main()
     char s[] = "aab";
 
     int dp[MAX][MAX] = {0};
+    int n = strlen(s);
 
     return 0;
 }
