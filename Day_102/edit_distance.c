02a5b45b6fd0eb3e1d22dd7c0f837a1c25110a4c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-09 19:51:47 +0530

    added string lengths

--- Day_102/edit_distance.c
+++ Day_102/edit_distance.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX 100
 
@@ -8,6 +9,9 @@ int main()
     char s2[] = "ros";
 
     int dp[MAX][MAX] = {0};
+
+    int n = strlen(s1);
+    int m = strlen(s2);
 
     return 0;
 }
