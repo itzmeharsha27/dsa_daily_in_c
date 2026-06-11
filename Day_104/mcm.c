cd71cb46e153e0abaf68a5a5c931765dd23da0e9
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-11 21:12:07 +0530

    clean final mcm version

--- Day_104/mcm.c
+++ Day_104/mcm.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// MCM using DP
+// Matrix Chain Multiplication (Interval DP)
 
 #define MAX 100
 #define INF 1000000
@@ -8,11 +8,11 @@
 int main()
 {
     int arr[] = {40,20,30,10,30};
-    int n = 5;
+    int n = 5; // number of dimensions
 
     int dp[MAX][MAX] = {0};
 
-    // DP computation
+    // Build DP table
     for(int i=1;i<n;i++)
         dp[i][i] = 0;
 
@@ -25,14 +25,16 @@ int main()
 
             for(int k=i;k<j;k++)
             {
-                int cost = dp[i][k] + dp[k+1][j] + arr[i-1]*arr[k]*arr[j];
+                int cost = dp[i][k] + dp[k+1][j]
+                         + arr[i-1]*arr[k]*arr[j];
+
                 if(cost < dp[i][j])
                     dp[i][j] = cost;
             }
         }
     }
 
-    printf("Minimum Multiplication Cost: %d\n", dp[1][n-1]);
+    printf("Minimum Cost to Multiply Matrices = %d\n", dp[1][n-1]);
 
     return 0;
 }
