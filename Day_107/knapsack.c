5153b2fabc69d31d828c4855ae50afa0aa972619
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-14 10:45:55 +0530

    implemented knapsack logic

--- Day_107/knapsack.c
+++ Day_107/knapsack.c
@@ -21,8 +21,9 @@ int main()
         for(int j=1;j<=W;j++)
         {
             if(wt[i-1] <= j)
-            {
-            }
+                dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+            else
+                dp[i][j] = dp[i-1][j];
         }
     }
 
