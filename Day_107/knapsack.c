132de889c61efccd5bfa46727e862496112d2d3f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-14 10:47:31 +0530

    final clean knapsack implementation

--- Day_107/knapsack.c
+++ Day_107/knapsack.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// 0/1 Knapsack (Tabulation DP)
+
 #define MAX 100
 
 int max(int a,int b)
@@ -16,20 +18,30 @@ int main()
 
     int dp[MAX][MAX] = {0};
 
+    // Build DP table
     for(int i=0;i<=n;i++)
     {
         for(int j=0;j<=W;j++)
         {
             if(i==0 || j==0)
+            {
                 dp[i][j] = 0;
+            }
             else if(wt[i-1] <= j)
-                dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+            {
+                dp[i][j] = max(
+                    val[i-1] + dp[i-1][j-wt[i-1]],
+                    dp[i-1][j]
+                );
+            }
             else
+            {
                 dp[i][j] = dp[i-1][j];
+            }
         }
     }
 
-    printf("Max Value: %d\n", dp[n][W]);
+    printf("Maximum Value in Knapsack = %d\n", dp[n][W]);
 
     return 0;
 }
