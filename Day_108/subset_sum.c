e5288a588c7d4f72a117a7a920fc4402f9eeaa74
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-15 23:24:33 +0530

    final subset sum program

--- Day_108/subset_sum.c
+++ Day_108/subset_sum.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
+// Subset Sum using DP
+
 #define MAX 100
 
 int main()
 {
-    int arr[] = {1,2,3,7};
-    int n = 4;
-    int sum = 6;
+    int arr[] = {3,34,4,12,5,2};
+    int n = 6;
+    int sum = 9;
 
     int dp[MAX][MAX] = {0};
 
+    // Base cases
     for(int i=0;i<=n;i++)
         dp[i][0] = 1;
 
     for(int j=1;j<=sum;j++)
         dp[0][j] = 0;
 
+    // DP computation
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=sum;j++)
@@ -27,7 +31,7 @@ int main()
         }
     }
 
-    printf("Subset Exists: %d\n", dp[n][sum]);
+    printf("Subset Sum Possible: %d\n", dp[n][sum]);
 
     return 0;
 }
