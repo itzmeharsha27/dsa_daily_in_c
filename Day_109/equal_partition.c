7cf388aa57e42aa4d4db23478dfa31880c702578
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-16 23:27:20 +0530

    implemented dp logic

--- Day_109/equal_partition.c
+++ Day_109/equal_partition.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Equal Partition using Subset Sum
+
 #define MAX 100
 
 int main()
@@ -23,6 +25,20 @@ int main()
 
     for(int i=0;i<=n;i++)
         dp[i][0] = 1;
+
+    for(int j=1;j<=sum;j++)
+        dp[0][j] = 0;
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=sum;j++)
+        {
+            if(arr[i-1] <= j)
+                dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
 
     return 0;
 }
