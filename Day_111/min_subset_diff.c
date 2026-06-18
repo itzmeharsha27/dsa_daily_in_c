bbef380bef273b02c1c726b810ba7ac394899256
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-18 18:39:58 +0530

    implemented subset dp

--- Day_111/min_subset_diff.c
+++ Day_111/min_subset_diff.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Minimum Subset Sum Difference
+
 #define MAX 100
 
 int main()
@@ -17,6 +19,20 @@ int main()
 
     for(int i=0;i<=n;i++)
         dp[i][0] = 1;
+
+    for(int j=1;j<=sum;j++)
+        dp[0][j] = 0;
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=sum;j++)
+        {
+            if(arr[i-1] <= j)
+                dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
 
     return 0;
 }
