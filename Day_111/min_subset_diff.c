9a5c49afce6a6042e2cd4504b89d875cd8cb9aac
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-18 18:45:23 +0530

    clean final min subset difference version

--- Day_111/min_subset_diff.c
+++ Day_111/min_subset_diff.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+// Minimum Subset Sum Difference (DP)
+
 #define MAX 100
 
 int main()
 {
-    int arr[] = {1,2,7};
-    int n = 3;
+    int arr[] = {1,6,11,5};
+    int n = 4;
 
     int dp[MAX][MAX] = {0};
 
@@ -15,25 +17,32 @@ int main()
 
     int sum = total / 2;
 
+    // Initialize DP
     for(int i=0;i<=n;i++)
         dp[i][0] = 1;
 
     for(int j=1;j<=sum;j++)
         dp[0][j] = 0;
 
+    // Build DP table
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=sum;j++)
         {
             if(arr[i-1] <= j)
-                dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+            {
+                dp[i][j] = dp[i-1][j-arr[i-1]] 
+                         || dp[i-1][j];
+            }
             else
+            {
                 dp[i][j] = dp[i-1][j];
+            }
         }
     }
 
     int best = 0;
-    for(int j=sum;j>=0;j--)
+    for(int j=sum; j>=0; j--)
     {
         if(dp[n][j])
         {
@@ -42,7 +51,9 @@ int main()
         }
     }
 
-    printf("Min Difference: %d\n", total - 2*best);
+    int minDiff = total - 2 * best;
+
+    printf("Minimum Subset Sum Difference = %d\n", minDiff);
 
     return 0;
 }
