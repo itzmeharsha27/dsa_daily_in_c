0126b7159ba8170694f8b5bca4acfd6df24f3e93
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-19 19:41:26 +0530

    created node and initialized list

--- Day_112/graph_representation.c
+++ Day_112/graph_representation.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Graph Representation
-
 #define MAX 100
 
 struct Node {
@@ -10,11 +8,22 @@ struct Node {
     struct Node* next;
 };
 
+struct Node* createNode(int v)
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
 int main()
 {
     int V = 4;
 
     struct Node* adjList[MAX];
+
+    for(int i=0;i<V;i++)
+        adjList[i] = NULL;
 
     return 0;
 }
