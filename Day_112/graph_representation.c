737556baec5dd7d0866080f412c4eddf5104c11d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-19 19:41:15 +0530

    started adjacency list

--- Day_112/graph_representation.c
+++ Day_112/graph_representation.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// Graph Representation
 
 #define MAX 100
+
+struct Node {
+    int data;
+    struct Node* next;
+};
 
 int main()
 {
     int V = 4;
 
-    int adj[MAX][MAX] = {0};
-
-    adj[0][1] = adj[1][0] = 1;
-    adj[0][2] = adj[2][0] = 1;
-    adj[1][3] = adj[3][1] = 1;
-
-    for(int i=0;i<V;i++)
-    {
-        for(int j=0;j<V;j++)
-        {
-            printf("%d ", adj[i][j]);
-        }
-        printf("\n");
-    }
+    struct Node* adjList[MAX];
 
     return 0;
 }
