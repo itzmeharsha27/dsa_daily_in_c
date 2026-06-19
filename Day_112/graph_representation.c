7618c0aff62a69a482276860b097d37d9306c50b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-19 19:41:35 +0530

    added edges to adjacency list

--- Day_112/graph_representation.c
+++ Day_112/graph_representation.c
@@ -16,6 +16,17 @@ struct Node* createNode(int v)
     return newNode;
 }
 
+void addEdge(struct Node* adjList[], int u, int v)
+{
+    struct Node* newNode = createNode(v);
+    newNode->next = adjList[u];
+    adjList[u] = newNode;
+
+    newNode = createNode(u);
+    newNode->next = adjList[v];
+    adjList[v] = newNode;
+}
+
 int main()
 {
     int V = 4;
@@ -24,6 +35,10 @@ int main()
 
     for(int i=0;i<V;i++)
         adjList[i] = NULL;
+
+    addEdge(adjList, 0, 1);
+    addEdge(adjList, 0, 2);
+    addEdge(adjList, 1, 3);
 
     return 0;
 }
