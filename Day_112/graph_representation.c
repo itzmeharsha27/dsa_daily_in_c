dd7df2de47eb51e42f14b4654384bc2f96965f32
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-19 19:41:02 +0530

    printed adjacency matrix

--- Day_112/graph_representation.c
+++ Day_112/graph_representation.c
@@ -12,5 +12,14 @@ int main()
     adj[0][2] = adj[2][0] = 1;
     adj[1][3] = adj[3][1] = 1;
 
+    for(int i=0;i<V;i++)
+    {
+        for(int j=0;j<V;j++)
+        {
+            printf("%d ", adj[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
