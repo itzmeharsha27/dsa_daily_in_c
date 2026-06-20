60029b430b25e747a673d321be60fd05b6e536f4
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-20 21:30:49 +0530

    added graph edges

--- Day_113/bfs.c
+++ Day_113/bfs.c
@@ -5,5 +5,10 @@ int main()
     int V = 5;
     int adj[100][100] = {0};
 
+    adj[0][1] = adj[1][0] = 1;
+    adj[0][2] = adj[2][0] = 1;
+    adj[1][3] = adj[3][1] = 1;
+    adj[2][4] = adj[4][2] = 1;
+
     return 0;
 }
