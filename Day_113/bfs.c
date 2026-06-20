ec34d096aeb1eb56b3d119a33a5b0bf667b3ac54
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-20 21:32:45 +0530

    final BFS traversal implementation

--- Day_113/bfs.c
+++ Day_113/bfs.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+// BFS Traversal (Queue Based)
+
 int main()
 {
     int V = 5;
+
     int adj[100][100] = {0};
 
+    // Graph edges
     adj[0][1] = adj[1][0] = 1;
     adj[0][2] = adj[2][0] = 1;
     adj[1][3] = adj[3][1] = 1;
@@ -12,11 +16,12 @@ int main()
 
     int queue[100];
     int front = 0, rear = 0;
+
     int visited[100] = {0};
 
     int start = 0;
 
-    printf("BFS: ");
+    printf("BFS Traversal: ");
 
     queue[rear++] = start;
     visited[start] = 1;
