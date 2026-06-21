9e23da16b40b5f6b02e0396d83339b9d242360f3
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-21 17:00:37 +0530

    added visited array

--- Day_114/dfs.c
+++ Day_114/dfs.c
@@ -2,13 +2,7 @@
 
 int main()
 {
-    int V = 5;
-    int adj[100][100]  = {0};
-
-    adj[0][1] = adj[1][0] = 1;
-    adj[0][2] = adj[2][0] = 1;
-    adj[1][3] = adj[3][1] = 1;
-    adj[2][4] = adj[4][2] = 1;
+    int visited[100] = {0};
 
     return 0;
 }
