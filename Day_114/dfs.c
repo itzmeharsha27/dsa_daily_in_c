69a7b7cf6573d0f767ceb768a6f312dd67068946
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-21 17:02:44 +0530

    added neighbor loop

--- Day_114/dfs.c
+++ Day_114/dfs.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// DFS Traversal
+
 int adj[100][100];
 int visited[100];
 int V = 5;
@@ -8,6 +10,10 @@ void dfs(int node)
 {
     visited[node] = 1;
     printf("%d ", node);
+
+    for(int i=0;i<V;i++)
+    {
+    }
 }
 
 int main()
