b4a8429307011698d00cded1b3f80b1a58228fe6
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-24 21:58:38 +0530

    added minDistance function

--- Day_121/day121_dijkstra.c
+++ Day_121/day121_dijkstra.c
@@ -7,6 +7,20 @@ int adj[100][100];
 int dist[100];
 int visited[100];
 
+int minDistance()
+{
+    int min = INF, idx = -1;
+    for(int i=0;i<V;i++)
+    {
+        if(!visited[i] && dist[i] < min)
+        {
+            min = dist[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
 int main()
 {
     int src = 0;
