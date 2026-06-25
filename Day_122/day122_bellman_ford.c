0fa21d4c40dbaaceca99f4cc1113d605aef08c42
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-25 23:42:38 +0530

    added sample graph

--- Day_122/day122_bellman_ford.c
+++ Day_122/day122_bellman_ford.c
@@ -13,7 +13,17 @@ int main()
     int V = 5;
     int E = 8;
 
-    struct Edge edges[MAX];
+    struct Edge edges[MAX] = {
+        {0,1,-1},
+        {0,2,4},
+        {1,2,3},
+        {1,3,2},
+        {1,4,2},
+        {3,2,5},
+        {3,1,1},
+        {4,3,-3}
+    };
+
     int dist[MAX];
 
     int src = 0;
