8e3f229f39042974f45686f94c00e64c356b0eef
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-27 22:34:06 +0530

    added main mst loop

--- Day_124/prims_algorithm.c
+++ Day_124/prims_algorithm.c
@@ -8,18 +8,36 @@ int minKey(int key[], int mst[])
     int min=INF,index=-1;
 
     for(int i=0;i<V;i++)
-    {
         if(!mst[i] && key[i]<min)
-        {
-            min=key[i];
-            index=i;
-        }
-    }
+            min=key[i],index=i;
 
     return index;
 }
 
 int main()
 {
+    int graph[V][V]={
+        {0,2,0,6,0},
+        {2,0,3,8,5},
+        {0,3,0,0,7},
+        {6,8,0,0,9},
+        {0,5,7,9,0}
+    };
+
+    int parent[V],key[V],mst[V];
+
+    for(int i=0;i<V;i++)
+    {
+        key[i]=INF;
+        mst[i]=0;
+    }
+
+    key[0]=0;
+    parent[0]=-1;
+
+    for(int count=0;count<V-1;count++)
+    {
+    }
+
     return 0;
 }
