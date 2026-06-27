6136b8de201073bddac805bcd70e551024cdeecf
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-27 22:30:22 +0530

    added sample weighted graph

--- Day_124/prims_algorithm.c
+++ Day_124/prims_algorithm.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 #define V 5
+#define INF 99999
 
 int main()
 {
+    int graph[V][V] = {
+        {0,2,0,6,0},
+        {2,0,3,8,5},
+        {0,3,0,0,7},
+        {6,8,0,0,9},
+        {0,5,7,9,0}
+    };
+
     return 0;
 }
