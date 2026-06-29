99281a9b7d9b69a0ec915ad1ca87c09a9be9ff40
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-29 07:24:31 +0530

    final disjoint set union implementation

--- Day_126/disjoint_set_union.c
+++ Day_126/disjoint_set_union.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Disjoint Set Union (Union-Find)
+
 #define MAX 100
 
 int parent[MAX];
@@ -9,7 +11,7 @@ int find(int x)
     if(parent[x] == x)
         return x;
 
-    return parent[x] = find(parent[x]);
+    return parent[x] = find(parent[x]); // Path Compression
 }
 
 void unite(int x, int y)
@@ -21,14 +23,19 @@ int main()
 {
     int n = 5;
 
+    // Initialize each element as its own parent
     for(int i=0;i<n;i++)
-        parent[i]=i;
+        parent[i] = i;
 
     unite(0,1);
     unite(2,3);
     unite(1,2);
 
+    printf("Representative of 0 = %d\n", find(0));
     printf("Representative of 3 = %d\n", find(3));
+
+    if(find(0) == find(3))
+        printf("0 and 3 belong to the same set\n");
 
     return 0;
 }
