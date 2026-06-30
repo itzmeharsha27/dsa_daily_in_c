69056c46a5db46c860e473ef4f9d67be9be157e3
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-06-30 09:06:33 +0530

    added create node function

--- Day_127/trie.c
+++ Day_127/trie.c
@@ -9,6 +9,14 @@ struct TrieNode
     int isEnd;
 };
 
+struct TrieNode* createNode()
+{
+    struct TrieNode *node = (struct TrieNode*)malloc(sizeof(struct TrieNode));
+    node->isEnd = 0;
+
+    return node;
+}
+
 int main()
 {
     return 0;
