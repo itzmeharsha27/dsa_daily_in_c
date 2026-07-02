2d82420637eedcd888d9e67d8a82d667e6648b49
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-02 21:30:58 +0530

    handled duplicate first element

--- Day_129/3sum.c
+++ Day_129/3sum.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// 3Sum using Sorting + Two Pointers
+
 int arr[] = {-1,0,1,2,-1,-4};
 int n = 6;
 
@@ -24,6 +26,9 @@ int main()
 
     for(int i=0;i<n-2;i++)
     {
+        if(i>0 && arr[i]==arr[i-1])
+            continue;
+
         int l=i+1,r=n-1;
 
         while(l<r)
