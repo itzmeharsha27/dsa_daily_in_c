e7390b2cd5e898e0b5d15074e3a1747fc4c85049
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-02 21:29:28 +0530

    added two pointer structure

--- Day_129/3sum.c
+++ Day_129/3sum.c
@@ -5,25 +5,28 @@ int n = 6;
 
 void swap(int *a, int *b)
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
+    int t=*a;
+    *a=*b;
+    *b=t;
 }
 
 void sort()
 {
     for(int i=0;i<n-1;i++)
-    {
         for(int j=0;j<n-i-1;j++)
-        {
-            if(arr[j] > arr[j+1])
-                swap(&arr[j], &arr[j+1]);
-        }
-    }
+            if(arr[j]>arr[j+1])
+                swap(&arr[j],&arr[j+1]);
 }
 
 int main()
 {
     sort();
+
+    for(int i=0;i<n-2;i++)
+    {
+        int l=i+1;
+        int r=n-1;
+    }
+
     return 0;
 }
