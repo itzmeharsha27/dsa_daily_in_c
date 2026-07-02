c025c940e45d4f71b5f53dbc466dc557328c71d1
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-02 21:28:54 +0530

    added bubble sort

--- Day_129/3sum.c
+++ Day_129/3sum.c
@@ -10,6 +10,18 @@ void swap(int *a, int *b)
     *b = t;
 }
 
+void sort()
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j] > arr[j+1])
+                swap(&arr[j], &arr[j+1]);
+        }
+    }
+}
+
 int main()
 {
     return 0;
