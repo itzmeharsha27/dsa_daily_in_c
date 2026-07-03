c1e1b8527ee5b99372b967a57c7b24707800c281
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-03 09:02:12 +0530

    final container with most water program

--- Day_130/container_with_most_water.c
+++ Day_130/container_with_most_water.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Container With Most Water using Two Pointers
+
 int min(int a, int b)
 {
     return (a < b) ? a : b;
@@ -7,28 +9,33 @@ int min(int a, int b)
 
 int main()
 {
-    int height[] = {2,3,4,5,18,17,6};
-    int n = 7;
+    // Sample input
+    int height[] = {1,8,6,2,5,4,8,3,7};
+    int n = sizeof(height) / sizeof(height[0]);
 
     int left = 0;
     int right = n - 1;
     int maxArea = 0;
 
+    // Traverse from both ends
     while(left < right)
     {
         int width = right - left;
-        int area = width * min(height[left], height[right]);
+        int currentHeight = min(height[left], height[right]);
+        int area = width * currentHeight;
 
+        // Update maximum area
         if(area > maxArea)
             maxArea = area;
 
+        // Move the pointer with smaller height
         if(height[left] < height[right])
             left++;
         else
             right--;
     }
 
-    printf("Maximum Area = %d\n", maxArea);
+    printf("Maximum Water Area = %d\n", maxArea);
 
     return 0;
 }
