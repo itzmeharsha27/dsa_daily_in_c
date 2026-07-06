089c45741f292a795529307600ec4dbf810fa247
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-06 09:54:20 +0530

    added row traversal

--- Day_133/maximal_rectangle.c
+++ Day_133/maximal_rectangle.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
+#define ROWS 4
 #define COLS 5
 
 int histogram[COLS] = {0};
 
 int main()
 {
-    int matrix[4][5] = {
+    int matrix[ROWS][COLS] = {
         {1,0,1,0,0},
         {1,0,1,1,1},
         {1,1,1,1,1},
         {1,0,0,1,0}
     };
+
+    for(int i=0;i<ROWS;i++)
+    {
+    }
 
     return 0;
 }
