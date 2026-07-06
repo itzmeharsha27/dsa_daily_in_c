a92fff022434fd10efe44000ada946cea7ef5207
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-06 09:54:41 +0530

    added histogram function

--- Day_133/maximal_rectangle.c
+++ Day_133/maximal_rectangle.c
@@ -5,6 +5,11 @@
 
 int histogram[COLS] = {0};
 
+int largestHistogram(int arr[], int n)
+{
+    return 0;
+}
+
 int main()
 {
     int matrix[ROWS][COLS] = {
@@ -13,17 +18,6 @@ int main()
         {1,1,1,1,1},
         {1,0,0,1,0}
     };
-
-    for(int i=0;i<ROWS;i++)
-    {
-        for(int j=0;j<COLS;j++)
-        {
-            if(matrix[i][j]==1)
-                histogram[j]++;
-            else
-                histogram[j]=0;
-        }
-    }
 
     return 0;
 }
