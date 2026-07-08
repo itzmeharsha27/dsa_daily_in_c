7cec95ae2a033998ffbed34901c39bd6c828dc56
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-08 20:32:36 +0530

    added sample input

--- Day_135/insert_interval.c
+++ Day_135/insert_interval.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+struct Interval
+{
+    int start;
+    int end;
+};
+
 int main()
 {
+    struct Interval arr[] = {{1,3},{6,9}};
+    struct Interval newInterval = {2,5};
+    int n = 2;
+
     return 0;
 }
