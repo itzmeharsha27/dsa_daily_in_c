c4b3b16053e5f70773c80587e1c2a51278a83130
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-09 22:12:56 +0530

    sorted intervals

--- Day_136/non_overlapping_intervals.c
+++ Day_136/non_overlapping_intervals.c
@@ -8,24 +8,25 @@ struct Interval
 
 void swap(struct Interval *a, struct Interval *b)
 {
-    struct Interval t = *a;
-    *a = *b;
-    *b = t;
+    struct Interval t=*a;
+    *a=*b;
+    *b=t;
 }
 
 void sort(struct Interval arr[], int n)
 {
     for(int i=0;i<n-1;i++)
-    {
         for(int j=0;j<n-i-1;j++)
-        {
-            if(arr[j].end > arr[j+1].end)
-                swap(&arr[j], &arr[j+1]);
-        }
-    }
+            if(arr[j].end>arr[j+1].end)
+                swap(&arr[j],&arr[j+1]);
 }
 
 int main()
 {
+    struct Interval arr[]={{1,2},{2,3},{3,4},{1,3}};
+    int n=4;
+
+    sort(arr,n);
+
     return 0;
 }
