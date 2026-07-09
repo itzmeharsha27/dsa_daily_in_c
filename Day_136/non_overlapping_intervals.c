d86496896f41e71df242d058680435a2c71ad307
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-09 22:12:49 +0530

    added sorting by end time

--- Day_136/non_overlapping_intervals.c
+++ Day_136/non_overlapping_intervals.c
@@ -13,6 +13,18 @@ void swap(struct Interval *a, struct Interval *b)
     *b = t;
 }
 
+void sort(struct Interval arr[], int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j].end > arr[j+1].end)
+                swap(&arr[j], &arr[j+1]);
+        }
+    }
+}
+
 int main()
 {
     return 0;
