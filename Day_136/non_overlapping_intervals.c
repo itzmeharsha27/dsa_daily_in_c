d5f3802188626008e3ee1f734d7b3ec6863f763c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-09 22:14:13 +0530

    final non overlapping intervals program

--- Day_136/non_overlapping_intervals.c
+++ Day_136/non_overlapping_intervals.c
@@ -1,33 +1,56 @@
 #include <stdio.h>
 
+// Non-overlapping Intervals using Greedy
+
 struct Interval
 {
     int start;
     int end;
 };
 
-void swap(struct Interval*, struct Interval*);
-void sort(struct Interval[], int);
+void swap(struct Interval *a, struct Interval *b)
+{
+    struct Interval t=*a;
+    *a=*b;
+    *b=t;
+}
+
+void sort(struct Interval arr[], int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j].end > arr[j+1].end)
+                swap(&arr[j], &arr[j+1]);
+        }
+    }
+}
 
 int main()
 {
-    struct Interval arr[]={{1,2},{1,2},{1,2}};
-    int n=3;
+    struct Interval arr[]={{1,2},{2,3},{3,4},{1,3}};
+    int n=sizeof(arr)/sizeof(arr[0]);
 
     sort(arr,n);
 
     int remove=0;
     int prevEnd=arr[0].end;
 
+    // Greedily keep intervals with earliest end time
     for(int i=1;i<n;i++)
     {
         if(arr[i].start < prevEnd)
+        {
             remove++;
+        }
         else
+        {
             prevEnd=arr[i].end;
+        }
     }
 
-    printf("Intervals to Remove = %d\n", remove);
+    printf("Minimum Intervals to Remove = %d\n", remove);
 
     return 0;
 }
