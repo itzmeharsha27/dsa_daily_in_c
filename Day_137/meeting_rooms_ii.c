635594077e549eaa9d27654b706f6dbfe1ce3e8f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-10 19:00:51 +0530

    final meeting rooms ii program

--- Day_137/meeting_rooms_ii.c
+++ Day_137/meeting_rooms_ii.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Meeting Rooms II using Sorting and Two Pointers
+
 void swap(int *a,int *b)
 {
     int t=*a;
@@ -10,30 +12,38 @@ void swap(int *a,int *b)
 void sort(int arr[],int n)
 {
     for(int i=0;i<n-1;i++)
+    {
         for(int j=0;j<n-i-1;j++)
-            if(arr[j]>arr[j+1])
-                swap(&arr[j],&arr[j+1]);
+        {
+            if(arr[j] > arr[j+1])
+                swap(&arr[j], &arr[j+1]);
+        }
+    }
 }
 
 int main()
 {
     int start[] = {0,5,15};
     int end[] = {30,10,20};
-    int n = 3;
+    int n = sizeof(start)/sizeof(start[0]);
 
     sort(start,n);
     sort(end,n);
 
     int i=0,j=0;
-    int rooms=0,maxRooms=0;
+    int rooms=0;
+    int maxRooms=0;
 
+    // Compare meeting start and end times
     while(i<n && j<n)
     {
         if(start[i] < end[j])
         {
             rooms++;
+
             if(rooms > maxRooms)
                 maxRooms = rooms;
+
             i++;
         }
         else
@@ -43,7 +53,7 @@ int main()
         }
     }
 
-    printf("Minimum Meeting Rooms = %d\n", maxRooms);
+    printf("Minimum Meeting Rooms Required = %d\n", maxRooms);
 
     return 0;
 }
