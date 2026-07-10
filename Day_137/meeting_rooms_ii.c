b89625474cc56cdc5a0088e633f24736c5c9c2b2
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-10 18:59:02 +0530

    added sorting function

--- Day_137/meeting_rooms_ii.c
+++ Day_137/meeting_rooms_ii.c
@@ -7,6 +7,14 @@ void swap(int *a,int *b)
     *b=t;
 }
 
+void sort(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+        for(int j=0;j<n-i-1;j++)
+            if(arr[j]>arr[j+1])
+                swap(&arr[j],&arr[j+1]);
+}
+
 int main()
 {
     int start[] = {0,5,15};
