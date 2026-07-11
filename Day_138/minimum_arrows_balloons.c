d45ed4d32b90997ddc14c9dd60ef49cf70ca7a3a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-11 19:33:34 +0530

    initialized greedy variables

--- Day_138/minimum_arrows_balloons.c
+++ Day_138/minimum_arrows_balloons.c
@@ -6,20 +6,8 @@ struct Balloon
     int end;
 };
 
-void swap(struct Balloon *a, struct Balloon *b)
-{
-    struct Balloon t=*a;
-    *a=*b;
-    *b=t;
-}
-
-void sort(struct Balloon arr[], int n)
-{
-    for(int i=0;i<n-1;i++)
-        for(int j=0;j<n-i-1;j++)
-            if(arr[j].end>arr[j+1].end)
-                swap(&arr[j],&arr[j+1]);
-}
+void swap(struct Balloon*,struct Balloon*);
+void sort(struct Balloon[],int);
 
 int main()
 {
@@ -27,6 +15,9 @@ int main()
     int n=4;
 
     sort(arr,n);
+
+    int arrows=1;
+    int lastEnd=arr[0].end;
 
     return 0;
 }
