2002668fa15d54141ef1e9ce64515176c9071ba8
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-11 19:35:00 +0530

    final minimum arrows to burst balloons program

--- Day_138/minimum_arrows_balloons.c
+++ Day_138/minimum_arrows_balloons.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
 
+// Minimum Number of Arrows to Burst Balloons using Greedy
+
 struct Balloon
 {
     int start;
     int end;
 };
 
-void swap(struct Balloon*,struct Balloon*);
-void sort(struct Balloon[],int);
+void swap(struct Balloon *a, struct Balloon *b)
+{
+    struct Balloon t=*a;
+    *a=*b;
+    *b=t;
+}
+
+void sort(struct Balloon arr[], int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j].end>arr[j+1].end)
+                swap(&arr[j],&arr[j+1]);
+        }
+    }
+}
 
 int main()
 {
-    struct Balloon arr[]={{1,2},{2,3},{3,4},{4,5}};
-    int n=4;
+    struct Balloon arr[]={{10,16},{2,8},{1,6},{7,12}};
+    int n=sizeof(arr)/sizeof(arr[0]);
 
     sort(arr,n);
 
     int arrows=1;
     int lastEnd=arr[0].end;
 
+    // Greedily shoot arrows at the earliest ending balloon
     for(int i=1;i<n;i++)
     {
         if(arr[i].start>lastEnd)
@@ -28,7 +47,7 @@ int main()
         }
     }
 
-    printf("Minimum Arrows = %d\n",arrows);
+    printf("Minimum Number of Arrows = %d\n",arrows);
 
     return 0;
 }
