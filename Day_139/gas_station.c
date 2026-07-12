b258e13160e37fc47b00bd9ce9fc0c16864b2eb8
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-12 20:09:02 +0530

    final gas station program

--- Day_139/gas_station.c
+++ Day_139/gas_station.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
+// Gas Station using Greedy Algorithm
+
 int main()
 {
     int gas[] = {1,2,3,4,5};
     int cost[] = {3,4,5,1,2};
-    int n = 5;
+    int n = sizeof(gas) / sizeof(gas[0]);
 
     int totalGas = 0;
     int totalCost = 0;
     int start = 0;
     int tank = 0;
 
+    // Traverse all stations
     for(int i=0;i<n;i++)
     {
         totalGas += gas[i];
@@ -18,6 +21,7 @@ int main()
 
         tank += gas[i] - cost[i];
 
+        // Reset starting station if tank becomes negative
         if(tank < 0)
         {
             start = i + 1;
@@ -26,9 +30,9 @@ int main()
     }
 
     if(totalGas >= totalCost)
-        printf("Starting Statio n = %d\n", start);
+        printf("Starting Gas Station Index = %d\n", start);
     else
-        printf("-1\n");
+        printf("Starting Gas Station Index = -1\n");
 
     return 0;
 }
