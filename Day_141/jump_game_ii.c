76ba41baae4c87c648d72cd3cb73b47a64d1faf7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-14 21:03:11 +0530

    final jump game ii program

--- Day_141/jump_game_ii.c
+++ Day_141/jump_game_ii.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Jump Game II using Greedy Algorithm
+
 int max(int a,int b)
 {
     return (a>b)?a:b;
@@ -8,16 +10,19 @@ int max(int a,int b)
 int main()
 {
     int nums[] = {2,3,1,1,4};
-    int n = 5;
+    int n = sizeof(nums) / sizeof(nums[0]);
 
     int jumps = 0;
     int currentEnd = 0;
     int farthest = 0;
 
+    // Traverse until the second last index
     for(int i=0;i<n-1;i++)
     {
+        // Update the farthest position reachable
         farthest = max(farthest, i + nums[i]);
 
+        // Need another jump
         if(i == currentEnd)
         {
             jumps++;
@@ -25,7 +30,7 @@ int main()
         }
     }
 
-    printf("Minimum Jumps = %d\n", jumps);
+    printf("Minimum Number of Jumps = %d\n", jumps);
 
     return 0;
 }
