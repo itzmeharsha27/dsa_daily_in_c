2ae91114706d1afb7b6b7be15a9a4d5bb187acc5
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-16 22:14:44 +0530

    final partition labels program

--- Day_143/partition_labels.c
+++ Day_143/partition_labels.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+// Partition Labels using Greedy Algorithm
+
 int max(int a,int b)
 {
     return (a>b)?a:b;
@@ -12,13 +14,16 @@ int main()
     int last[26];
     int n=strlen(str);
 
+    // Store last occurrence of every character
     for(int i=0;i<n;i++)
         last[str[i]-'a']=i;
 
-    int start=0,end=0;
+    int start=0;
+    int end=0;
 
     printf("Partition Sizes: ");
 
+    // Form partitions greedily
     for(int i=0;i<n;i++)
     {
         end=max(end,last[str[i]-'a']);
