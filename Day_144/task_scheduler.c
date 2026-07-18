f07654dc2183b6ae05ee2bbcc073a59f2b60efd7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-18 19:01:23 +0530

    added queue size variable

--- Day_144/task_scheduler.c
+++ Day_144/task_scheduler.c
@@ -3,7 +3,6 @@
 void swap(int a[2],int b[2])
 {
     int t;
-
     t=a[0]; a[0]=b[0]; b[0]=t;
     t=a[1]; a[1]=b[1]; b[1]=t;
 }
@@ -14,16 +13,13 @@ int main()
     int n=6;
 
     int result[10][2];
+    int size=0;
 
     for(int i=0;i<n-1;i++)
-    {
         for(int j=i+1;j<n;j++)
-        {
             if(people[i][0]<people[j][0] ||
-              (people[i][0]==people[j][0] && people[i][1]>people[j][1]))
+              (people[i][0]==people[j][0]&&people[i][1]>people[j][1]))
                 swap(people[i],people[j]);
-        }
-    }
 
     return 0;
 }
