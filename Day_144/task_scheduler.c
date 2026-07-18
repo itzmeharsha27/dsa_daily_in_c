905307d129ee943f0058829bfb8472e524d575ea
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-18 19:00:45 +0530

    added result array

--- Day_144/task_scheduler.c
+++ Day_144/task_scheduler.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
+void swap(int a[2],int b[2])
+{
+    int t;
+
+    t=a[0];
+    a[0]=b[0];
+    b[0]=t;
+
+    t=a[1];
+    a[1]=b[1];
+    b[1]=t;
+}
+
 int main()
 {
-    char tasks[] = {'A','A','A','B','B','B'};
-    int coolDown = 2;
-    int size = 6;
+    int people[][2]={{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+    int n=6;
 
-    int freq[26] = {0};
+    int result[10][2];
 
     return 0;
 }
