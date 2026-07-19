7454e44581b57de24f3672e4947433e79d86df39
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-19 07:38:09 +0530

    final remove k digits program

--- Day_145/remove_k_digits.c
+++ Day_145/remove_k_digits.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
-// Remove K Digits using Greedy Stack
+// Remove K Digits using Greedy Stack Algorithm
 
 int main()
 {
     char num[] = "1432219";
     int k = 3;
+    int n = strlen(num);
 
     char stack[100];
     int top = -1;
-    int n = strlen(num);
 
+    // Traverse each digit
     for(int i=0;i<n;i++)
     {
-        while(top>=0 && k>0 && stack[top]>num[i])
+        // Remove larger previous digits
+        while(top >= 0 && k > 0 && stack[top] > num[i])
         {
             top--;
             k--;
         }
 
-        stack[++top]=num[i];
+        stack[++top] = num[i];
     }
 
-    while(k>0)
+    // Remove remaining digits if needed
+    while(k > 0)
     {
         top--;
         k--;
     }
 
-    stack[top+1]='\0';
+    stack[top + 1] = '\0';
 
-    printf("Smallest Number = %s\n",stack);
+    printf("Smallest Number = %s\n", stack);
 
     return 0;
 }
