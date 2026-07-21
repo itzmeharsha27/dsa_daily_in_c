93cf5fb1d852c2a1f3c30245c698cffafa49c2f7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-21 22:20:05 +0530

    final merge triplets program

--- Day_147/merge_triplets.c
+++ Day_147/merge_triplets.c
@@ -1,31 +1,39 @@
 #include <stdio.h>
 
-// Greedy Solution for Merge Triplets
+// Merge Triplets to Form Target Triplet using Greedy Algorithm
 
 int main()
 {
     int triplets[][3]={{2,5,3},{1,8,4},{1,7,5}};
     int target[]={2,7,5};
-    int n=3;
+    int n=sizeof(triplets)/sizeof(triplets[0]);
 
-    int foundA=0,foundB=0,foundC=0;
+    int foundA=0;
+    int foundB=0;
+    int foundC=0;
 
+    // Check every valid triplet
     for(int i=0;i<n;i++)
     {
         if(triplets[i][0]<=target[0] &&
            triplets[i][1]<=target[1] &&
            triplets[i][2]<=target[2])
         {
-            if(triplets[i][0]==target[0]) foundA=1;
-            if(triplets[i][1]==target[1]) foundB=1;
-            if(triplets[i][2]==target[2]) foundC=1;
+            if(triplets[i][0]==target[0])
+                foundA=1;
+
+            if(triplets[i][1]==target[1])
+                foundB=1;
+
+            if(triplets[i][2]==target[2])
+                foundC=1;
         }
     }
 
-    if(foundA&&foundB&&foundC)
-        printf("Possible\n");
+    if(foundA && foundB && foundC)
+        printf("Possible to form target triplet\n");
     else
-        printf("Not Possible\n");
+        printf("Not Possible to form target triplet\n");
 
     return 0;
 }
