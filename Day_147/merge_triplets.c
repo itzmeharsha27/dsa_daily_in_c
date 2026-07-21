82aaa0e8fe4a51f012983675e9d0b511d03a3811
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-21 22:18:27 +0530

    checked first target value

--- Day_147/merge_triplets.c
+++ Day_147/merge_triplets.c
@@ -12,6 +12,11 @@ int main()
 
     for(int i=0;i<n;i++)
     {
+        if(triplets[i][0]<=target[0] &&
+           triplets[i][1]<=target[1] &&
+           triplets[i][2]<=target[2])
+        {
+        }
     }
 
     return 0;
