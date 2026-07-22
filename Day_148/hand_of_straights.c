4a756257a7ad5477b978e887c97a36b23deffb83
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-22 22:29:14 +0530

    added visited array

--- Day_148/hand_of_straights.c
+++ Day_148/hand_of_straights.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 int main()
-{#include <stdio.h>
-
-int main()
 {
     int hand[] = {1,2,3,6,2,3,4,7,8};
     int groupSize = 3;
     int n = 9;
 
-    return 0;
-}
+    int used[100] = {0};
+
     return 0;
 }
