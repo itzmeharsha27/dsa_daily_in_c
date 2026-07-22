ae882d112bab0d909b6e59771780ecbe50241bc9
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-22 22:28:00 +0530

    added sample input

--- Day_148/hand_of_straights.c
+++ Day_148/hand_of_straights.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main()
+{#include <stdio.h>
+
+int main()
 {
+    int hand[] = {1,2,3,6,2,3,4,7,8};
+    int groupSize = 3;
+    int n = 9;
+
+    return 0;
+}
     return 0;
 }
