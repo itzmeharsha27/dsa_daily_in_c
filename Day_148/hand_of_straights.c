c06a039173730e406787d497253bf1ab4a4b5485
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-22 22:29:26 +0530

    added swap function

--- Day_148/hand_of_straights.c
+++ Day_148/hand_of_straights.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
+void swap(int *a,int *b)
+{
+    int t=*a;
+    *a=*b;
+    *b=t;
+}
+
 int main()
 {
     int hand[] = {1,2,3,6,2,3,4,7,8};
     int groupSize = 3;
     int n = 9;
 
-    int used[100] = {0};
+    int used[100]={0};
 
     return 0;
 }
