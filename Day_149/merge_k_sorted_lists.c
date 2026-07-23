02840487d0997f7a9b1c616f5ea19a809f44a769
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-23 20:28:44 +0530

    added print function

--- Day_149/merge_k_sorted_lists.c
+++ Day_149/merge_k_sorted_lists.c
@@ -16,53 +16,17 @@ struct Node* createNode(int value)
     return newNode;
 }
 
+void printList(struct Node *head)
+{
+    while(head)
+    {
+        printf("%d ",head->data);
+        head=head->next;
+    }
+}
+
 int main()
 {
-    struct Node *list1=createNode(1);
-    list1->next=createNode(4);
-    list1->next->next=createNode(5);
-
-    struct Node *list2=createNode(1);
-    list2->next=createNode(3);
-    list2->next->next=createNode(4);
-
-    struct Node *list3=createNode(2);
-    list3->next=createNode(6);
-
-    struct Node *lists[3]={list1,list2,list3};
-
-    struct Node *head=NULL,*tail=NULL;
-
-    while(1)
-    {
-        int minIndex=-1;
-        int minValue=INT_MAX;
-
-        for(int i=0;i<3;i++)
-        {
-            if(lists[i]!=NULL && lists[i]->data<minValue)
-            {
-                minValue=lists[i]->data;
-                minIndex=i;
-            }
-        }
-
-        if(minIndex==-1)
-            break;
-
-        if(head==NULL)
-        {
-            head=lists[minIndex];
-            tail=head;
-        }
-        else
-        {
-            tail->next=lists[minIndex];
-            tail=tail->next;
-        }
-
-        lists[minIndex]=lists[minIndex]->next;
-    }
-
+    /* previous code remains same */
     return 0;
 }
