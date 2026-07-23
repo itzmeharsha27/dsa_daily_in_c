c8ec2a17a00a38e6ec8bd54d9930810e83e7fba6
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-23 20:27:20 +0530

    added node creation function

--- Day_149/merge_k_sorted_lists.c
+++ Day_149/merge_k_sorted_lists.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Node
 {
     int data;
     struct Node *next;
 };
+
+struct Node* createNode(int value)
+{
+    struct Node *newNode=(struct Node*)malloc(sizeof(struct Node));
+    newNode->data=value;
+    newNode->next=NULL;
+    return newNode;
+}
 
 int main()
 {
