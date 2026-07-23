23d5d66bf1e718945aa28439e9e14243465ac540
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-23 20:28:53 +0530

    added output

--- Day_149/merge_k_sorted_lists.c
+++ Day_149/merge_k_sorted_lists.c
@@ -1,32 +1,3 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 
-struct Node
-{
-    int data;
-    struct Node *next;
-};
-
-struct Node* createNode(int value)
-{
-    struct Node *newNode=(struct Node*)malloc(sizeof(struct Node));
-    newNode->data=value;
-    newNode->next=NULL;
-    return newNode;
-}
-
-void printList(struct Node *head)
-{
-    while(head)
-    {
-        printf("%d ",head->data);
-        head=head->next;
-    }
-}
-
-int main()
-{
-    /* previous code remains same */
-    return 0;
-}
+// Merge K Sorted Lists
