e276b6695ea0633fa571bf56b4daff4f81ed4b4e
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-24 18:05:08 +0530

    inserted sample cache values

--- Day_150/lru_cache.c
+++ Day_150/lru_cache.c
@@ -13,5 +13,13 @@ int main()
 
     struct Cache cache[10];
 
+    cache[size].key = 1;
+    cache[size].value = 1;
+    size++;
+
+    cache[size].key = 2;
+    cache[size].value = 2;
+    size++;
+
     return 0;
 }
