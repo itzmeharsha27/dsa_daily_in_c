a44177e0b589acb07740d02523f9209ee3240456
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-25 09:27:26 +0530

    improved readability

--- Day_151/kth_largest_element.c
+++ Day_151/kth_largest_element.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Greedy Sorting Approach
+// Kth Largest Element using Sorting
 
 void swap(int *a,int *b)
 {
@@ -13,14 +13,18 @@ int main()
 {
     int nums[] = {3,2,1,5,6,4};
     int k = 2;
-    int n = 6;
+    int n = sizeof(nums)/sizeof(nums[0]);
 
     for(int i=0;i<n-1;i++)
+    {
         for(int j=i+1;j<n;j++)
+        {
             if(nums[i]<nums[j])
                 swap(&nums[i],&nums[j]);
+        }
+    }
 
-    printf("Kth Largest Element = %d\n",nums[k-1]);
+    printf("Answer = %d\n",nums[k-1]);
 
     return 0;
 }
