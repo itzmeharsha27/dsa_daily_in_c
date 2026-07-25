0ab55cba807efae00ad94cb91e4a94d1c275a36a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-25 09:26:04 +0530

    added swap function

--- Day_151/kth_largest_element.c
+++ Day_151/kth_largest_element.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+void swap(int *a,int *b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
 int main()
 {
+    int nums[] = {3,2,1,5,6,4};
+    int k = 2;
+    int n = 6;
+
     return 0;
 }
