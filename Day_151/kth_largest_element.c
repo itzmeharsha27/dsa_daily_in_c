0ff18514624d923535004a0f7274cd14e99b6c5e
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-25 09:26:22 +0530

    implemented descending sort

--- Day_151/kth_largest_element.c
+++ Day_151/kth_largest_element.c
@@ -15,6 +15,11 @@ int main()
 
     for(int i=0;i<n-1;i++)
     {
+        for(int j=i+1;j<n;j++)
+        {
+            if(nums[i]<nums[j])
+                swap(&nums[i],&nums[j]);
+        }
     }
 
     return 0;
