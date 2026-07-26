bdb66f97d768a87031bb6efef2617269e4d7c530
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-26 13:04:03 +0530

    counted element frequencies

--- Day_152/top_k_frequent_elements.c
+++ Day_152/top_k_frequent_elements.c
@@ -13,6 +13,29 @@ int main()
     int n = 6;
 
     struct Item arr[100];
+    int size = 0;
+
+    for(int i=0;i<n;i++)
+    {
+        int found=0;
+
+        for(int j=0;j<size;j++)
+        {
+            if(arr[j].value==nums[i])
+            {
+                arr[j].freq++;
+                found=1;
+                break;
+            }
+        }
+
+        if(!found)
+        {
+            arr[size].value=nums[i];
+            arr[size].freq=1;
+            size++;
+        }
+    }
 
     return 0;
 }
