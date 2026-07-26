9b718c19df899ba0b82cfdfad82c305f7be7c320
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-26 13:03:48 +0530

    added item structure

--- Day_152/top_k_frequent_elements.c
+++ Day_152/top_k_frequent_elements.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+struct Item
+{
+    int value;
+    int freq;
+};
+
 int main()
 {
     int nums[] = {1,1,1,2,2,3};
     int k = 2;
     int n = 6;
+
+    struct Item arr[100];
 
     return 0;
 }
