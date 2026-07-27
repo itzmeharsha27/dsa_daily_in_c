9759c550c93c37294c0d1f864a765302e4f49e0d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-27 10:27:53 +0530

    organized solution

--- Day_153/find_median_data_stream.c
+++ Day_153/find_median_data_stream.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Median Finder using Sorting
+
 void swap(int *a,int *b)
 {
     int t=*a;
@@ -9,17 +11,18 @@ void swap(int *a,int *b)
 
 int main()
 {
-    int arr[]={5,2,8,4};
-    int size=4;
+    int arr[]={1,2,3};
+    int size=3;
 
     for(int i=0;i<size-1;i++)
         for(int j=i+1;j<size;j++)
             if(arr[i]>arr[j])
                 swap(&arr[i],&arr[j]);
 
-    float median=(arr[size/2-1]+arr[size/2])/2.0;
-
-    printf("Median = %.1f\n",median);
+    if(size%2==0)
+        printf("Median = %.1f\n",(arr[size/2-1]+arr[size/2])/2.0);
+    else
+        printf("Median = %d\n",arr[size/2]);
 
     return 0;
 }
