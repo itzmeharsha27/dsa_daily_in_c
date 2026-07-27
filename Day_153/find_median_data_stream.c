46af62cb828ebb4e6fc986431a6b2679b9e49fb4
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-27 10:25:11 +0530

    calculated median

--- Day_153/find_median_data_stream.c
+++ Day_153/find_median_data_stream.c
@@ -17,13 +17,16 @@ int main()
     arr[size++]=3;
 
     for(int i=0;i<size-1;i++)
-    {
         for(int j=i+1;j<size;j++)
-        {
             if(arr[i]>arr[j])
                 swap(&arr[i],&arr[j]);
-        }
-    }
+
+    float median;
+
+    if(size%2==0)
+        median=(arr[size/2-1]+arr[size/2])/2.0;
+    else
+        median=arr[size/2];
 
     return 0;
 }
