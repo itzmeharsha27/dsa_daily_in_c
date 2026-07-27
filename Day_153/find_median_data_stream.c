401c415e18ad83042dab821d5aee90ac5e960eb9
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-27 10:25:03 +0530

    sorted stream values

--- Day_153/find_median_data_stream.c
+++ Day_153/find_median_data_stream.c
@@ -16,5 +16,14 @@ int main()
     arr[size++]=2;
     arr[size++]=3;
 
+    for(int i=0;i<size-1;i++)
+    {
+        for(int j=i+1;j<size;j++)
+        {
+            if(arr[i]>arr[j])
+                swap(&arr[i],&arr[j]);
+        }
+    }
+
     return 0;
 }
