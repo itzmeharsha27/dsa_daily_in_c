352975207a8cd318a186b016451ecb94c31cb56d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-27 10:23:42 +0530

    added swap function

--- Day_153/find_median_data_stream.c
+++ Day_153/find_median_data_stream.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+void swap(int *a,int *b)
+{
+    int t=*a;
+    *a=*b;
+    *b=t;
+}
+
 int main()
 {
     int arr[100];
-    int size = 0;
+    int size=0;
 
     return 0;
 }
