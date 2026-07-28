d2d9caad3d0f108f20715a2136fc7efd08c4f740
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-28 09:28:56 +0530

    added output heading

--- Day_154/sliding_window_maximum.c
+++ Day_154/sliding_window_maximum.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Sliding Window Maximum
+
 int main()
 {
     int nums[]={1,3,-1,-3,5,3,6,7};
@@ -8,10 +10,22 @@ int main()
 
     int maxValue;
 
+    printf("Maximums: ");
+
     for(int i=0;i<=n-k;i++)
     {
         maxValue=nums[i];
+
+        for(int j=i;j<i+k;j++)
+        {
+            if(nums[j]>maxValue)
+                maxValue=nums[j];
+        }
+
+        printf("%d ",maxValue);
     }
+
+    printf("\n");
 
     return 0;
 }
