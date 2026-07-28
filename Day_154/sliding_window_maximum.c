40fa8e891012d9f8c70acfd0186b73d25065de9c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-28 09:29:53 +0530

    improved readability

--- Day_154/sliding_window_maximum.c
+++ Day_154/sliding_window_maximum.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-// Sliding Window Maximum
+// Sliding Window Maximum using Nested Loops
 
 int main()
 {
     int nums[]={1,3,-1,-3,5,3,6,7};
     int k=3;
-    int n=8;
+    int n=sizeof(nums)/sizeof(nums[0]);
 
     for(int i=0;i<=n-k;i++)
     {
         int maxValue=nums[i];
 
-        for(int j=i;j<i+k;j++)
+        for(int j=i+1;j<i+k;j++)
         {
             if(nums[j]>maxValue)
                 maxValue=nums[j];
