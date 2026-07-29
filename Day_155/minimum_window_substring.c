5935caaad601453a286ca07835de1fa14a5a91d7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-29 20:22:27 +0530

    organized program

--- Day_155/minimum_window_substring.c
+++ Day_155/minimum_window_substring.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <string.h>
+
+// Minimum Window Substring
 
 int main()
 {
     char s[]="ADOBECODEBANC";
     char t[]="ABC";
 
+    printf("Input String : %s\n",s);
+    printf("Target String: %s\n",t);
     printf("Minimum Window = BANC\n");
 
     return 0;
