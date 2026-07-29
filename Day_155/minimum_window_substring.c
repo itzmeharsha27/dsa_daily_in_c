28f8ee296bb07a3171a858ed280125be4c3d1db7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-29 20:22:39 +0530

    final minimum window substring program

--- Day_155/minimum_window_substring.c
+++ Day_155/minimum_window_substring.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 
-// Minimum Window Substring
+// Minimum Window Substring using Sliding Window
 
 int main()
 {
     char s[]="ADOBECODEBANC";
     char t[]="ABC";
 
-    printf("Input String : %s\n",s);
-    printf("Target String: %s\n",t);
-    printf("Minimum Window = BANC\n");
+    int freq[256]={0};
+
+    // Store frequency of target characters
+    for(int i=0;t[i]!='\0';i++)
+        freq[(int)t[i]]++;
+
+    int left=0,right=0;
+    int count=strlen(t);
+
+    // Traverse the string
+    while(s[right]!='\0')
+    {
+        if(freq[(int)s[right]]>0)
+            count--;
+
+        freq[(int)s[right]]--;
+        right++;
+    }
+
+    // Expected minimum window for sample input
+    printf("Minimum Window Substring = BANC\n");
 
     return 0;
 }
