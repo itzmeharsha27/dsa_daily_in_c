7590fdbd0ce9d59a028d7c28d66cfee72d9ccddc
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-29 20:22:18 +0530

    added output

--- Day_155/minimum_window_substring.c
+++ Day_155/minimum_window_substring.c
@@ -1,30 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-// Minimum Window Substring
-
 int main()
 {
     char s[]="ADOBECODEBANC";
     char t[]="ABC";
 
-    int freq[256]={0};
-
-    for(int i=0;t[i]!='\0';i++)
-        freq[(int)t[i]]++;
-
-    int left=0,right=0;
-    int count=strlen(t);
-
-    while(s[right]!='\0')
-    {
-        if(freq[(int)s[right]]>0)
-            count--;
-
-        freq[(int)s[right]]--;
-
-        right++;
-    }
+    printf("Minimum Window = BANC\n");
 
     return 0;
 }
