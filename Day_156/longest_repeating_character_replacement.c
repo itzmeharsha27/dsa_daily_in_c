d570ba3f1cc9104277540b797f8ce041ac559b05
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-30 19:49:17 +0530

    tracked maximum frequency

--- Day_156/longest_repeating_character_replacement.c
+++ Day_156/longest_repeating_character_replacement.c
@@ -8,10 +8,15 @@ int main()
 
     int freq[26]={0};
     int left=0,right=0;
+    int maxFreq=0;
 
     while(right<strlen(s))
     {
         freq[s[right]-'A']++;
+
+        if(freq[s[right]-'A']>maxFreq)
+            maxFreq=freq[s[right]-'A'];
+
         right++;
     }
 
