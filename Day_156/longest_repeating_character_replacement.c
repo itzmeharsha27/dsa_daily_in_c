6b201194da4984b4a0d3eda8e9cd975b0890ebce
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-30 19:49:59 +0530

    tracked maximum window length

--- Day_156/longest_repeating_character_replacement.c
+++ Day_156/longest_repeating_character_replacement.c
@@ -8,7 +8,7 @@ int main()
 
     int freq[26]={0};
     int left=0,right=0;
-    int maxFreq=0;
+    int maxFreq=0,maxLen=0;
 
     while(right<strlen(s))
     {
@@ -16,6 +16,9 @@ int main()
 
         if(freq[s[right]-'A']>maxFreq)
             maxFreq=freq[s[right]-'A'];
+
+        if(right-left+1>maxLen)
+            maxLen=right-left+1;
 
         right++;
     }
