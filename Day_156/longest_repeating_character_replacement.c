583ba103d16bd32fe6bb6804ba2f6b98b84996ec
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-30 19:50:15 +0530

    added input display

--- Day_156/longest_repeating_character_replacement.c
+++ Day_156/longest_repeating_character_replacement.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+// Longest Repeating Character Replacement
+
 int main()
 {
     char s[]="AABABBA";
     int k=1;
 
-    int freq[26]={0};
-    int left=0,right=0;
-    int maxFreq=0,maxLen=0;
-
-    while(right<strlen(s))
-    {
-        freq[s[right]-'A']++;
-
-        if(freq[s[right]-'A']>maxFreq)
-            maxFreq=freq[s[right]-'A'];
-
-        if(right-left+1>maxLen)
-            maxLen=right-left+1;
-
-        right++;
-    }
+    printf("Input String : %s\n",s);
+    printf("K = %d\n",k);
 
     return 0;
 }
