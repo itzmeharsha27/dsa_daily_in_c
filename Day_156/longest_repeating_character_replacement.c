53231300f1c671a9e8ad4f2caf86ec1cdbfda1a1
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-30 19:50:54 +0530

    final longest repeating character replacement program

--- Day_156/longest_repeating_character_replacement.c
+++ Day_156/longest_repeating_character_replacement.c
@@ -8,9 +8,30 @@ int main()
     char s[]="AABABBA";
     int k=1;
 
-    printf("String : %s\n",s);
-    printf("Replacement Limit : %d\n",k);
-    printf("Longest Length = 4\n");
+    int freq[26]={0};
+    int left=0,right=0;
+    int maxFreq=0,maxLen=0;
+
+    while(right<strlen(s))
+    {
+        freq[s[right]-'A']++;
+
+        if(freq[s[right]-'A']>maxFreq)
+            maxFreq=freq[s[right]-'A'];
+
+        if((right-left+1)-maxFreq>k)
+        {
+            freq[s[left]-'A']--;
+            left++;
+        }
+
+        if(right-left+1>maxLen)
+            maxLen=right-left+1;
+
+        right++;
+    }
+
+    printf("Longest Repeating Character Replacement Length = %d\n",maxLen);
 
     return 0;
 }
