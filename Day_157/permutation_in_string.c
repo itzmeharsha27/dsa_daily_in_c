a5c88f0df9400ceae44a1ea84d20c93b020d088e
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-07-31 18:06:25 +0530

    added frequency comparison function

--- Day_157/permutation_in_string.c
+++ Day_157/permutation_in_string.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+int check(int a[],int b[])
+{
+    for(int i=0;i<26;i++)
+        if(a[i]!=b[i])
+            return 0;
+    return 1;
+}
+
 int main()
 {
     char s1[]="ab";
