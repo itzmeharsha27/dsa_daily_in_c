4128dcdd0eaf24da527fe2826bac58520611230d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-01 15:58:50 +0530

    final longest substring without repeating characters program

--- Day_158/longest_substring_without_repeating_characters.c
+++ Day_158/longest_substring_without_repeating_characters.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
-// Sliding Window Approach
+// Longest Substring Without Repeating Characters using Sliding Window
 
 int main()
 {
     char s[]="abcabcbb";
 
     int freq[256]={0};
-    int left=0,right=0,maxLen=0;
+    int left=0,right=0;
+    int maxLen=0;
 
+    // Traverse the string
     while(right<strlen(s))
     {
         freq[(int)s[right]]++;
 
+        // Remove duplicate characters
         while(freq[(int)s[right]]>1)
         {
             freq[(int)s[left]]--;
             left++;
         }
 
+        // Update longest length
         if(right-left+1>maxLen)
             maxLen=right-left+1;
 
         right++;
     }
 
-    printf("Longest Length = %d\n",maxLen);
+    printf("Longest Substring Length = %d\n",maxLen);
 
     return 0;
 }
