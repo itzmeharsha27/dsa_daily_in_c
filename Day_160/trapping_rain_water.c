87537d384b6922cf63cada6df08c893c81f4f221
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-03 18:10:44 +0530

    added traversal loop

--- Day_160/trapping_rain_water.c
+++ Day_160/trapping_rain_water.c
@@ -5,11 +5,13 @@ int main()
     int height[]={0,1,0,2,1,0,1,3,2,1,2,1};
     int n=12;
 
-    int left=0;
-    int right=n-1;
-    int leftMax=0;
-    int rightMax=0;
+    int left=0,right=n-1;
+    int leftMax=0,rightMax=0;
     int water=0;
+
+    while(left<right)
+    {
+    }
 
     return 0;
 }
