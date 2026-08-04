d55f3f9722f46406d0fdff781d2a90a53618e8c1
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-04 18:04:32 +0530

    sorted input array

--- Day_161/3sum.c
+++ Day_161/3sum.c
@@ -12,5 +12,10 @@ int main()
     int nums[]={-1,0,1,2,-1,-4};
     int n=6;
 
+    for(int i=0;i<n-1;i++)
+        for(int j=i+1;j<n;j++)
+            if(nums[i]>nums[j])
+                swap(&nums[i],&nums[j]);
+
     return 0;
 }
