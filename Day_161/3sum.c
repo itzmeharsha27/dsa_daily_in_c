4930b98a38867ad2d67a76bc1c372b60b9eb1af5
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-04 18:05:27 +0530

    calculated triplet sum

--- Day_161/3sum.c
+++ Day_161/3sum.c
@@ -21,6 +21,21 @@ int main()
     {
         int left=i+1;
         int right=n-1;
+
+        while(left<right)
+        {
+            int sum=nums[i]+nums[left]+nums[right];
+
+            if(sum==0)
+            {
+                printf("%d %d %d\n",nums[i],nums[left],nums[right]);
+                break;
+            }
+            else if(sum<0)
+                left++;
+            else
+                right--;
+        }
     }
 
     return 0;
