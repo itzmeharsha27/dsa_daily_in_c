c21dbf0e3af8024d5ab9880bfc068c7505d3be1f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-06 12:15:12 +0530

    final valid parentheses python program

--- Day_163/valid_parentheses.py
+++ Day_163/valid_parentheses.py
@@ -1,3 +1,5 @@
+# Valid Parentheses using Stack
+
 def main():
     s = "()[]{}"
 
@@ -11,17 +13,22 @@ def main():
 
     valid = True
 
+    # Traverse the string
     for ch in s:
+
         if ch in "([{":
             stack.append(ch)
+
         else:
             if not stack or stack.pop() != pairs[ch]:
                 valid = False
                 break
 
+    # Stack should be empty
     if stack:
         valid = False
 
-    print(valid)
+    print("Valid Parentheses:", valid)
+
 
 main()
