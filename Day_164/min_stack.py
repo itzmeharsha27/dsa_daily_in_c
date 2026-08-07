1b6b04aa97927dbfc6e6395fdca453cc461d48c0
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-07 19:38:58 +0530

    final min stack python program

--- Day_164/min_stack.py
+++ Day_164/min_stack.py
@@ -1,11 +1,24 @@
 # Min Stack using Two Stacks
 
 def main():
-    stack = [-2, 0, -3]
-    min_stack = [-2, -2, -3]
+    stack = []
+    min_stack = []
+
+    # Push -2
+    stack.append(-2)
+    min_stack.append(-2)
+
+    # Push 0
+    stack.append(0)
+    min_stack.append(min(min_stack[-1], 0))
+
+    # Push -3
+    stack.append(-3)
+    min_stack.append(min(min_stack[-1], -3))
 
     print("Minimum:", min_stack[-1])
 
+    # Pop
     stack.pop()
     min_stack.pop()
 
