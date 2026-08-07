8e79004e6aa8ce51d2a7b66a8aeeaa5976223efc
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-07 19:38:24 +0530

    added push operation

--- Day_164/min_stack.py
+++ Day_164/min_stack.py
@@ -2,4 +2,13 @@ def main():
     stack = []
     min_stack = []
 
+    stack.append(-2)
+    min_stack.append(-2)
+
+    stack.append(0)
+    min_stack.append(-2)
+
+    stack.append(-3)
+    min_stack.append(-3)
+
 main()
