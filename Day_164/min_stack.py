72e94e9fdc7834e7644d9b2cfc9a3492ca0b55cc
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-07 19:38:49 +0530

    organized solution

--- Day_164/min_stack.py
+++ Day_164/min_stack.py
@@ -1,11 +1,15 @@
+# Min Stack using Two Stacks
+
 def main():
     stack = [-2, 0, -3]
     min_stack = [-2, -2, -3]
 
+    print("Minimum:", min_stack[-1])
+
     stack.pop()
     min_stack.pop()
 
-    print(stack[-1])
-    print(min_stack[-1])
+    print("Top:", stack[-1])
+    print("Minimum:", min_stack[-1])
 
 main()
