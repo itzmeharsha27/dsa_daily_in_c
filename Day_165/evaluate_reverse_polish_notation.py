62e01c9cb147e97a304aa8899a1b27f50709ac30
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-08 21:11:40 +0530

    implemented stack evaluation

--- Day_165/evaluate_reverse_polish_notation.py
+++ Day_165/evaluate_reverse_polish_notation.py
@@ -2,4 +2,20 @@ def main():
     tokens = ["2", "1", "+", "3", "*"]
     stack = []
 
+    for token in tokens:
+        if token not in "+-*/":
+            stack.append(int(token))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+
+            if token == "+":
+                stack.append(a + b)
+            elif token == "-":
+                stack.append(a - b)
+            elif token == "*":
+                stack.append(a * b)
+            else:
+                stack.append(int(a / b))
+
 main()
