789bd386ac27d6ee199fda60225b0da1ccea6fb6
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-08 21:12:19 +0530

    final reverse polish notation program

--- Day_165/evaluate_reverse_polish_notation.py
+++ Day_165/evaluate_reverse_polish_notation.py
@@ -1,23 +1,36 @@
+# Evaluate Reverse Polish Notation using Stack
+
 def main():
     tokens = ["2", "1", "+", "3", "*"]
+
     stack = []
 
+    # Process every token
     for token in tokens:
+
+        # If token is a number, push it
         if token not in "+-*/":
             stack.append(int(token))
+
         else:
+            # Pop the two operands
             b = stack.pop()
             a = stack.pop()
 
             if token == "+":
                 stack.append(a + b)
+
             elif token == "-":
                 stack.append(a - b)
+
             elif token == "*":
                 stack.append(a * b)
+
             elif token == "/":
+                # Truncate toward zero
                 stack.append(int(a / b))
 
-    print("Result =", stack[-1])
+    print("Reverse Polish Notation Result =", stack[-1])
+
 
 main()
