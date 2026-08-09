e2b5bef331f503eb885852d43a5fcfa3c9526c50
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-09 12:42:08 +0530

    final daily temperatures program

--- Day_166/daily_temperatures.py
+++ Day_166/daily_temperatures.py
@@ -1,17 +1,29 @@
+# Daily Temperatures using Monotonic Stack
+
 def main():
     temperatures = [73,74,75,71,69,72,76,73]
 
     n = len(temperatures)
+
+    # Result array
     answer = [0] * n
+
+    # Stack stores indices
     stack = []
 
+    # Traverse temperatures
     for i in range(n):
+
+        # Find a warmer temperature
         while stack and temperatures[i] > temperatures[stack[-1]]:
             previous = stack.pop()
+
             answer[previous] = i - previous
 
+        # Store current index
         stack.append(i)
 
-    print("Daily Temperatures:", answer)
+    print("Daily Temperatures =", answer)
+
 
 main()
