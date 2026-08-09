122cd25d4778f37da8f7b7405833210549b3bd4b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-09 12:41:53 +0530

    implemented monotonic stack

--- Day_166/daily_temperatures.py
+++ Day_166/daily_temperatures.py
@@ -3,5 +3,13 @@ def main():
 
     n = len(temperatures)
     answer = [0] * n
+    stack = []
+
+    for i in range(n):
+        while stack and temperatures[i] > temperatures[stack[-1]]:
+            previous = stack.pop()
+            answer[previous] = i - previous
+
+        stack.append(i)
 
 main()
