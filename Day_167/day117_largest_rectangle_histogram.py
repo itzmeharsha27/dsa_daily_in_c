9b4d26b6ff9a9689e51c7ae28517970b9c5fc187
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-10 19:27:14 +0530

    implemented monotonic stack

--- Day_167/day117_largest_rectangle_histogram.py
+++ Day_167/day117_largest_rectangle_histogram.py
@@ -5,4 +5,17 @@ def main():
     stack = []
     max_area = 0
 
+    for i in range(n):
+        while stack and heights[i] < heights[stack[-1]]:
+            height = heights[stack.pop()]
+
+            if stack:
+                width = i - stack[-1] - 1
+            else:
+                width = i
+
+            max_area = max(max_area, height * width)
+
+        stack.append(i)
+
 main()
