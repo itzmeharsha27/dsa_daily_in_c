39632c4211e1bd43e4b68b16fcd2e6332e08354f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-11 09:17:07 +0530

    final car fleet python program

--- Day_168/car_fleet.py
+++ Day_168/car_fleet.py
@@ -1,3 +1,5 @@
+# Car Fleet using Sorting and Stack
+
 def main():
     position = [10,8,0,5,3]
     speed = [2,4,1,1,3]
@@ -5,19 +7,24 @@ def main():
 
     cars = []
 
+    # Create position-speed pairs
     for i in range(len(position)):
         cars.append((position[i], speed[i]))
 
+    # Process cars from closest to target
     cars.sort(reverse=True)
 
     stack = []
 
+    # Calculate arrival time for each car
     for pos, spd in cars:
         time = (target - pos) / spd
 
+        # A slower fleet ahead cannot be caught
         if not stack or time > stack[-1]:
             stack.append(time)
 
     print("Number of Car Fleets =", len(stack))
+
 
 main()
