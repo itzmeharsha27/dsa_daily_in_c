a4ab4cfe45af97461a774e819b5cfcef39cc7370
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-12 19:39:05 +0530

    final binary search python program

--- Day_169/binary_search.py
+++ Day_169/binary_search.py
@@ -1,4 +1,4 @@
-# Binary Search
+# Binary Search using Divide and Conquer
 
 def main():
     nums = [-1,0,3,5,9,12]
@@ -8,21 +8,25 @@ def main():
     right = len(nums) - 1
     result = -1
 
+    # Search while valid range exists
     while left <= right:
+
+        # Find middle index
         mid = (left + right) // 2
 
         if nums[mid] == target:
             result = mid
             break
 
+        # Search right half
         elif nums[mid] < target:
             left = mid + 1
 
+        # Search left half
         else:
             right = mid - 1
 
-    print("Target =", target)
-    print("Index =", result)
+    print("Binary Search Index =", result)
 
 
 main()
