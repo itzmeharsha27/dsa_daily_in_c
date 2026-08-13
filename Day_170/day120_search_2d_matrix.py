eb70cb43630abec7d6416c35eed8fca696aa32f9
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-13 21:42:05 +0530

    implemented matrix binary search

--- Day_170/day120_search_2d_matrix.py
+++ Day_170/day120_search_2d_matrix.py
@@ -12,11 +12,20 @@ def main():
 
     left = 0
     right = rows * cols - 1
+    found = False
 
     while left <= right:
         mid = (left + right) // 2
 
         row = mid // cols
         col = mid % cols
+
+        if matrix[row][col] == target:
+            found = True
+            break
+        elif matrix[row][col] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
 
 main()
