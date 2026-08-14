0161788f2b3c535aea25b96a58bd7845b5bd2c1b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-14 19:18:07 +0530

    final koko eating bananas python program

--- Day_171/day121_koko_eating_bananas.py
+++ Day_171/day121_koko_eating_bananas.py
@@ -1,30 +1,38 @@
-# Koko Eating Bananas
+# Koko Eating Bananas using Binary Search
 
 def main():
     piles = [3,6,7,11]
     h = 8
 
+    # Minimum possible speed
     left = 1
+
+    # Maximum possible speed
     right = max(piles)
+
     answer = right
 
+    # Binary search for minimum valid speed
     while left <= right:
 
         speed = (left + right) // 2
 
         hours = 0
 
+        # Calculate hours required at this speed
         for pile in piles:
             hours += (pile + speed - 1) // speed
 
+        # Speed is sufficient
         if hours <= h:
             answer = speed
             right = speed - 1
+
+        # Speed is too slow
         else:
             left = speed + 1
 
-    print("Piles:", piles)
-    print("Hours:", h)
-    print("Minimum Speed:", answer)
+    print("Minimum Eating Speed =", answer)
+
 
 main()
