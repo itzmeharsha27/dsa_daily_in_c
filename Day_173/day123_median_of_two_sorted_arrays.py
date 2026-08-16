c6fe7a75d095daae928bc659f38235d55883fcec
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-16 20:11:04 +0530

    merged sorted arrays

--- Day_173/day123_median_of_two_sorted_arrays.py
+++ Day_173/day123_median_of_two_sorted_arrays.py
@@ -7,4 +7,13 @@ def main():
     i = 0
     j = 0
 
+    while i < len(nums1) and j < len(nums2):
+
+        if nums1[i] < nums2[j]:
+            merged.append(nums1[i])
+            i += 1
+        else:
+            merged.append(nums2[j])
+            j += 1
+
 main()
