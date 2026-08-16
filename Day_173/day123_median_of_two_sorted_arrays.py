1dd9241d0294aa286964c87befab2140420e9970
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-16 20:13:24 +0530

    final median of two sorted arrays program

--- Day_173/day123_median_of_two_sorted_arrays.py
+++ Day_173/day123_median_of_two_sorted_arrays.py
@@ -1,33 +1,39 @@
 # Median of Two Sorted Arrays
 
 def main():
-    nums1 = [1,2]
-    nums2 = [3,4]
+    nums1 = [1,3]
+    nums2 = [2]
 
     merged = []
 
     i = 0
     j = 0
 
+    # Merge both sorted arrays
     while i < len(nums1) and j < len(nums2):
 
-        if nums1[i] < nums2[j]:
+        if nums1[i] <= nums2[j]:
             merged.append(nums1[i])
             i += 1
+
         else:
             merged.append(nums2[j])
             j += 1
 
+    # Add remaining elements
     merged.extend(nums1[i:])
     merged.extend(nums2[j:])
 
     n = len(merged)
 
+    # Calculate median
     if n % 2 == 1:
         median = merged[n // 2]
+
     else:
         median = (merged[n // 2 - 1] + merged[n // 2]) / 2
 
-    print("Median =", median)
+    print("Median of Two Sorted Arrays =", median)
+
 
 main()
