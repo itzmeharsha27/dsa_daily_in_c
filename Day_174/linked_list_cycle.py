1a66425f2f6228c4df9e83c0e0848febbafae738
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-17 20:47:15 +0530

    added cycle detection function

--- Day_174/linked_list_cycle.py
+++ Day_174/linked_list_cycle.py
@@ -1,2 +1,26 @@
-git add .
-git commit -m "created sample linked list"
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def has_cycle(head):
+    slow = head
+    fast = head
+
+    return False
+
+
+def main():
+    head = Node(3)
+
+    head.next = Node(2)
+    head.next.next = Node(0)
+    head.next.next.next = Node(-4)
+
+    head.next.next.next.next = head.next
+
+    print(has_cycle(head))
+
+
+main()
