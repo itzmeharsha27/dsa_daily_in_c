33e3f6ff43b66805b670f377bccb07ebdc346bd5
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-19 17:06:02 +0530

    basic structure for remove nth node

---
+++ forst.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+
+using namespace std;
+int main()
+{
+
+
+int p=10;
+int t=20;
+int r=30;
+
+int simple_interest=p*t*r;
+
+cout<<"simple interest="<<simple_interest;
+return 0;
+}
