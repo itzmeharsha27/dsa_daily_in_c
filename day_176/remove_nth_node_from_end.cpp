a4eb2aa8d4db464d5b8f635bd7e6207927dcf3f7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-19 17:07:51 +0530

    final remove nth node from end cpp program

--- day_176/remove_nth_node_from_end.cpp
+++ day_176/remove_nth_node_from_end.cpp
@@ -1,33 +1,49 @@
 #include <iostream>
 using namespace std;
 
+// Remove Nth Node From End of Linked List
+// Using Two Pointer Technique
+
 struct Node
 {
     int data;
     Node* next;
+
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
+    }
 };
 
+// Remove nth node from the end
 Node* removeNthFromEnd(Node* head, int n)
 {
-    Node* dummy = new Node{0, head};
+    // Dummy node handles removal of head
+    Node* dummy = new Node(0);
+    dummy->next = head;
 
     Node* fast = dummy;
     Node* slow = dummy;
 
+    // Move fast pointer n steps ahead
     for(int i = 0; i < n; i++)
         fast = fast->next;
 
+    // Move both pointers until fast reaches last node
     while(fast->next)
     {
         fast = fast->next;
         slow = slow->next;
     }
 
+    // slow is now before the target node
     slow->next = slow->next->next;
 
     return dummy->next;
 }
 
+// Display linked list
 void display(Node* head)
 {
     while(head)
@@ -45,18 +61,20 @@ void display(Node* head)
 
 int main()
 {
-    Node* head = new Node{1, nullptr};
+    // Create linked list
+    Node* head = new Node(1);
 
-    head->next = new Node{2, nullptr};
-    head->next->next = new Node{3, nullptr};
-    head->next->next->next = new Node{4, nullptr};
-    head->next->next->next->next = new Node{5, nullptr};
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
 
     int n = 2;
 
     cout << "Original List: ";
     display(head);
 
+    // Remove nth node from end
     head = removeNthFromEnd(head, n);
 
     cout << "After Removal: ";
