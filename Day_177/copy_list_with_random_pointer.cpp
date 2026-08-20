71b73063da1ae7108a9898d4c3b1efcafb187efd
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-20 21:42:17 +0530

    created sample random pointer list

--- Day_177/copy_list_with_random_pointer.cpp
+++ Day_177/copy_list_with_random_pointer.cpp
@@ -6,11 +6,23 @@ struct Node
     int data;
     Node* next;
     Node* random;
+
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
+        random = nullptr;
+    }
 };
 
 int main()
 {
-    Node* head = nullptr;
+    Node* head = new Node(7);
+
+    head->next = new Node(13);
+    head->next->next = new Node(11);
+    head->next->next->next = new Node(10);
+    head->next->next->next->next = new Node(1);
 
     return 0;
 }
