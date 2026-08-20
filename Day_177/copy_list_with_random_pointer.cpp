a44b15541ed43198072df75a2b635b48903a51e4
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-20 21:42:27 +0530

    created node mapping

--- Day_177/copy_list_with_random_pointer.cpp
+++ Day_177/copy_list_with_random_pointer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 struct Node
@@ -15,6 +16,24 @@ struct Node
     }
 };
 
+Node* copyRandomList(Node* head)
+{
+    if(head == nullptr)
+        return nullptr;
+
+    unordered_map<Node*, Node*> mp;
+
+    Node* current = head;
+
+    while(current)
+    {
+        mp[current] = new Node(current->data);
+        current = current->next;
+    }
+
+    return mp[head];
+}
+
 int main()
 {
     Node* head = new Node(7);
@@ -23,6 +42,8 @@ int main()
     head->next->next = new Node(11);
     head->next->next->next = new Node(10);
     head->next->next->next->next = new Node(1);
+
+    Node* copy = copyRandomList(head);
 
     return 0;
 }
