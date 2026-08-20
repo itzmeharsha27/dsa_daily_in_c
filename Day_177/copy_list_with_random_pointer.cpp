9c8ddab5e97a722ff615ceb4041b4b6c8ea7c363
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-20 21:42:53 +0530

    added copied list output

--- Day_177/copy_list_with_random_pointer.cpp
+++ Day_177/copy_list_with_random_pointer.cpp
@@ -46,6 +46,21 @@ Node* copyRandomList(Node* head)
     return mp[head];
 }
 
+void display(Node* head)
+{
+    while(head)
+    {
+        cout << head->data;
+
+        if(head->next)
+            cout << " -> ";
+
+        head = head->next;
+    }
+
+    cout << endl;
+}
+
 int main()
 {
     Node* head = new Node(7);
@@ -55,7 +70,16 @@ int main()
     head->next->next->next = new Node(10);
     head->next->next->next->next = new Node(1);
 
+    head->random = nullptr;
+    head->next->random = head;
+    head->next->next->random = head->next->next->next->next;
+    head->next->next->next->random = head->next->next;
+    head->next->next->next->next->random = head;
+
     Node* copy = copyRandomList(head);
+
+    cout << "Copied List: ";
+    display(copy);
 
     return 0;
 }
