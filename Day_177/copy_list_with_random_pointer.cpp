fe8593102630dbe24fe46ecdecf6fa3e2ff86e95
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-20 21:43:05 +0530

    organized copy random pointer solution

--- Day_177/copy_list_with_random_pointer.cpp
+++ Day_177/copy_list_with_random_pointer.cpp
@@ -2,6 +2,8 @@
 #include <unordered_map>
 using namespace std;
 
+// Copy List with Random Pointer
+
 struct Node
 {
     int data;
@@ -18,13 +20,14 @@ struct Node
 
 Node* copyRandomList(Node* head)
 {
-    if(head == nullptr)
+    if(!head)
         return nullptr;
 
     unordered_map<Node*, Node*> mp;
 
     Node* current = head;
 
+    // Create copy of every node
     while(current)
     {
         mp[current] = new Node(current->data);
@@ -33,6 +36,7 @@ Node* copyRandomList(Node* head)
 
     current = head;
 
+    // Connect next and random pointers
     while(current)
     {
         mp[current]->next = mp[current->next];
@@ -70,13 +74,15 @@ int main()
     head->next->next->next = new Node(10);
     head->next->next->next->next = new Node(1);
 
-    head->random = nullptr;
     head->next->random = head;
     head->next->next->random = head->next->next->next->next;
     head->next->next->next->random = head->next->next;
     head->next->next->next->next->random = head;
 
     Node* copy = copyRandomList(head);
+
+    cout << "Original List: ";
+    display(head);
 
     cout << "Copied List: ";
     display(copy);
