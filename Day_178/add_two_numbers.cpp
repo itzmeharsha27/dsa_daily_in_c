55ee2ee6adfb292c93c3408e4ba19733a1a0223d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-21 10:18:46 +0530

    created sample input lists

--- Day_178/add_two_numbers.cpp
+++ Day_178/add_two_numbers.cpp
@@ -5,12 +5,23 @@ struct Node
 {
     int data;
     Node* next;
+
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
+    }
 };
 
 int main()
 {
-    Node* l1 = nullptr;
-    Node* l2 = nullptr;
+    Node* l1 = new Node(2);
+    l1->next = new Node(4);
+    l1->next->next = new Node(3);
+
+    Node* l2 = new Node(5);
+    l2->next = new Node(6);
+    l2->next->next = new Node(4);
 
     return 0;
 }
