0cc96a82e64aa74ba684a2f48b1949b1fccb76ed
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-22 14:22:25 +0530

    created doubly linked list

--- Day_179/lru_cache.cpp
+++ Day_179/lru_cache.cpp
@@ -8,11 +8,27 @@ struct Node
     int value;
     Node* prev;
     Node* next;
+
+    Node(int k, int v)
+    {
+        key = k;
+        value = v;
+        prev = nullptr;
+        next = nullptr;
+    }
 };
 
 int main()
 {
+    int capacity = 2;
+
     unordered_map<int, Node*> cache;
+
+    Node* head = new Node(0, 0);
+    Node* tail = new Node(0, 0);
+
+    head->next = tail;
+    tail->prev = head;
 
     return 0;
 }
