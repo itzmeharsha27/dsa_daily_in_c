32d45469080b0c1d04d85cf4a49865022c9d812d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-22 14:22:09 +0530

    added hash map and cache node

--- Day_179/lru_cache.cpp
+++ Day_179/lru_cache.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
+
+struct Node
+{
+    int key;
+    int value;
+    Node* prev;
+    Node* next;
+};
 
 int main()
 {
+    unordered_map<int, Node*> cache;
+
     return 0;
 }
