b7b5b1c3fe40350f5adcfc2d3cf7f6605b5d4d87
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-23 11:36:52 +0530

    created sample linked list

--- Day_180/reverse_nodes_in_k_group.cpp
+++ Day_180/reverse_nodes_in_k_group.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
+    }
+};
+
 int main()
 {
+    Node* head = new Node(1);
+
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    int k = 2;
+
     return 0;
 }
