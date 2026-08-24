ce5f9c61468ea1c3454c0ed1f3f75074fbdcbc1e
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-24 18:38:06 +0530

    added twitter feed demonstration

--- Day_181/design_twitter.cpp
+++ Day_181/design_twitter.cpp
@@ -2,7 +2,10 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <queue>
 using namespace std;
+
+// Design Twitter
 
 struct Tweet
 {
@@ -13,6 +16,7 @@ struct Tweet
 class Twitter
 {
 private:
+
     unordered_map<int, vector<Tweet>> tweets;
     unordered_map<int, unordered_set<int>> following;
 
@@ -34,7 +38,46 @@ public:
     {
         following[followerId].erase(followeeId);
     }
+
+    vector<int> getNewsFeed(int userId)
+    {
+        priority_queue<pair<int, int>> maxHeap;
+
+        for(auto tweet : tweets[userId])
+            maxHeap.push({tweet.time, tweet.id});
+
+        for(int user : following[userId])
+        {
+            for(auto tweet : tweets[user])
+                maxHeap.push({tweet.time, tweet.id});
+        }
+
+        vector<int> result;
+
+        while(!maxHeap.empty() && result.size() < 10)
+        {
+            result.push_back(maxHeap.top().second);
+            maxHeap.pop();
+        }
+
+        return result;
+    }
 };
+
+void printFeed(vector<int> feed)
+{
+    cout << "[";
+
+    for(int i = 0; i < feed.size(); i++)
+    {
+        cout << feed[i];
+
+        if(i + 1 < feed.size())
+            cout << ", ";
+    }
+
+    cout << "]" << endl;
+}
 
 int main()
 {
@@ -42,8 +85,20 @@ int main()
 
     twitter.postTweet(1, 5);
 
+    cout << "Feed after Tweet 5: ";
+    printFeed(twitter.getNewsFeed(1));
+
     twitter.follow(1, 2);
+
+    twitter.postTweet(2, 6);
+
+    cout << "Feed after following User 2: ";
+    printFeed(twitter.getNewsFeed(1));
+
     twitter.unfollow(1, 2);
+
+    cout << "Feed after unfollowing User 2: ";
+    printFeed(twitter.getNewsFeed(1));
 
     return 0;
 }
