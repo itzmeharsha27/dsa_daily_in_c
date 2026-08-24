55cfefb58e933513c9df9be83ee64e8d697f6790
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-24 18:37:02 +0530

    added follow relationship storage

--- Day_181/design_twitter.cpp
+++ Day_181/design_twitter.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 using namespace std;
+
+class Twitter
+{
+private:
+    unordered_map<int, unordered_set<int>> following;
+
+public:
+
+    Twitter()
+    {
+    }
+};
 
 int main()
 {
+    Twitter twitter;
+
     return 0;
 }
