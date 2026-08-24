0b8694e984b3e1de0bd57af8e2fed03dff3cefff
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-24 18:37:16 +0530

    added tweet storage

--- Day_181/design_twitter.cpp
+++ Day_181/design_twitter.cpp
@@ -4,21 +4,37 @@
 #include <vector>
 using namespace std;
 
+struct Tweet
+{
+    int id;
+    int time;
+};
+
 class Twitter
 {
 private:
+    unordered_map<int, vector<Tweet>> tweets;
     unordered_map<int, unordered_set<int>> following;
+
+    int timestamp = 0;
 
 public:
 
     Twitter()
     {
     }
+
+    void postTweet(int userId, int tweetId)
+    {
+        tweets[userId].push_back({tweetId, timestamp++});
+    }
 };
 
 int main()
 {
     Twitter twitter;
+
+    twitter.postTweet(1, 5);
 
     return 0;
 }
