c06086d19b88bd94ba989946a08e441ad446bc4d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-25 11:48:05 +0530

    created median finder class

--- Day_182/find_median_from_data_stream.cpp
+++ Day_182/find_median_from_data_stream.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
+
+class MedianFinder
+{
+private:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+public:
+
+    void addNum(int num)
+    {
+        maxHeap.push(num);
+    }
+
+    double findMedian()
+    {
+        return 0.0;
+    }
+};
 
 int main()
 {
-    priority_queue<int> maxHeap;
-    priority_queue<int, vector<int>, greater<int>> minHeap;
+    MedianFinder finder;
+
+    finder.addNum(1);
+    finder.addNum(2);
 
     return 0;
 }
