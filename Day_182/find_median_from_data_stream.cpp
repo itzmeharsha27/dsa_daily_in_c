372173145db159272ff7b495c54a1944516226a4
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-25 11:48:46 +0530

    final median from data stream cpp program

--- Day_182/find_median_from_data_stream.cpp
+++ Day_182/find_median_from_data_stream.cpp
@@ -4,28 +4,34 @@
 using namespace std;
 
 // Find Median from Data Stream
+// Using Two Heaps
 
 class MedianFinder
 {
 private:
 
-    // Smaller half
+    // Max heap stores the smaller half
     priority_queue<int> maxHeap;
 
-    // Larger half
+    // Min heap stores the larger half
     priority_queue<int, vector<int>, greater<int>> minHeap;
 
 public:
 
+    // Add number to the data stream
     void addNum(int num)
     {
-        // Add to smaller half
+        // Decide which heap should contain the number
         if(maxHeap.empty() || num <= maxHeap.top())
+        {
             maxHeap.push(num);
+        }
         else
+        {
             minHeap.push(num);
+        }
 
-        // Balance both heaps
+        // Keep the heaps balanced
         if(maxHeap.size() > minHeap.size() + 1)
         {
             minHeap.push(maxHeap.top());
@@ -39,11 +45,16 @@ public:
         }
     }
 
+    // Find current median
     double findMedian()
     {
+        // Odd number of elements
         if(maxHeap.size() > minHeap.size())
+        {
             return maxHeap.top();
+        }
 
+        // Even number of elements
         return (maxHeap.top() + minHeap.top()) / 2.0;
     }
 };
@@ -53,13 +64,18 @@ int main()
     MedianFinder finder;
 
     finder.addNum(1);
-    finder.addNum(2);
+    cout << "Median: "
+         << finder.findMedian() << endl;
 
+    finder.addNum(2);
     cout << "Median: "
          << finder.findMedian() << endl;
 
     finder.addNum(3);
+    cout << "Median: "
+         << finder.findMedian() << endl;
 
+    finder.addNum(4);
     cout << "Median: "
          << finder.findMedian() << endl;
 
