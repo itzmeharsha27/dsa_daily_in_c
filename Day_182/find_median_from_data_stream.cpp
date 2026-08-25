5a0a33304c7c2c0329ebc417d472cd041b188fff
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-25 11:47:55 +0530

    added two heaps

--- Day_182/find_median_from_data_stream.cpp
+++ Day_182/find_median_from_data_stream.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int main()
 {
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
     return 0;
 }
