66704bafaa11d91793a5d58c3521c0979798e099
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-26 14:14:54 +0530

    added deque and result vector

--- Day_183/sliding_window_maximum.cpp
+++ Day_183/sliding_window_maximum.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 using namespace std;
 
 int main()
 {
     vector<int> nums = {1,3,-1,-3,5,3,6,7};
     int k = 3;
+
+    deque<int> dq;
+    vector<int> result;
 
     return 0;
 }
