3a728e514d69396e6eecd1bf7dae1578b8fe73e5
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-26 14:15:09 +0530

    final sliding window maximum cpp program

--- Day_183/sliding_window_maximum.cpp
+++ Day_183/sliding_window_maximum.cpp
@@ -3,6 +3,9 @@
 #include <deque>
 using namespace std;
 
+// Sliding Window Maximum
+// Using Monotonic Deque
+
 vector<int> maxSlidingWindow(vector<int>& nums, int k)
 {
     deque<int> dq;
@@ -10,19 +13,27 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k)
 
     for(int i = 0; i < nums.size(); i++)
     {
-        // Remove indices outside the window
+        // Remove elements outside current window
         if(!dq.empty() && dq.front() <= i - k)
+        {
             dq.pop_front();
+        }
 
-        // Remove smaller elements
+        // Remove smaller elements from back
+        // because they can never become maximum
         while(!dq.empty() && nums[dq.back()] <= nums[i])
+        {
             dq.pop_back();
+        }
 
+        // Store current index
         dq.push_back(i);
 
-        // Window is ready
+        // Start recording maximums
         if(i >= k - 1)
+        {
             result.push_back(nums[dq.front()]);
+        }
     }
 
     return result;
@@ -30,13 +41,24 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k)
 
 int main()
 {
-    vector<int> nums = {1,3,-1,-3,5,3,6,7};
+    vector<int> nums = {
+        1, 3, -1, -3,
+        5, 3, 6, 7
+    };
+
     int k = 3;
 
-    vector<int> result = maxSlidingWindow(nums, k);
+    vector<int> result =
+        maxSlidingWindow(nums, k);
+
+    cout << "Sliding Window Maximum: ";
 
     for(int value : result)
+    {
         cout << value << " ";
+    }
+
+    cout << endl;
 
     return 0;
 }
