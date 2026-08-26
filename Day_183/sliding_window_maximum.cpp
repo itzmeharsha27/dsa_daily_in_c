bf982d511f013514efd182bff99a7e5722d47de7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-26 14:15:01 +0530

    implemented monotonic deque solution

--- Day_183/sliding_window_maximum.cpp
+++ Day_183/sliding_window_maximum.cpp
@@ -3,13 +3,40 @@
 #include <deque>
 using namespace std;
 
+vector<int> maxSlidingWindow(vector<int>& nums, int k)
+{
+    deque<int> dq;
+    vector<int> result;
+
+    for(int i = 0; i < nums.size(); i++)
+    {
+        // Remove indices outside the window
+        if(!dq.empty() && dq.front() <= i - k)
+            dq.pop_front();
+
+        // Remove smaller elements
+        while(!dq.empty() && nums[dq.back()] <= nums[i])
+            dq.pop_back();
+
+        dq.push_back(i);
+
+        // Window is ready
+        if(i >= k - 1)
+            result.push_back(nums[dq.front()]);
+    }
+
+    return result;
+}
+
 int main()
 {
     vector<int> nums = {1,3,-1,-3,5,3,6,7};
     int k = 3;
 
-    deque<int> dq;
-    vector<int> result;
+    vector<int> result = maxSlidingWindow(nums, k);
+
+    for(int value : result)
+        cout << value << " ";
 
     return 0;
 }
