f1fc2f30d502568bd6479fade15c99db1e54b8d1
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-27 11:24:38 +0530

    added sliding window tracking

--- Day_184/minimum_window_substring.cpp
+++ Day_184/minimum_window_substring.cpp
@@ -3,15 +3,44 @@
 #include <unordered_map>
 using namespace std;
 
+string minWindow(string s, string t)
+{
+    unordered_map<char, int> required;
+
+    for(char ch : t)
+        required[ch]++;
+
+    unordered_map<char, int> window;
+
+    int left = 0;
+    int formed = 0;
+    int requiredCount = required.size();
+
+    int minLength = s.length() + 1;
+    int start = 0;
+
+    for(int right = 0; right < s.length(); right++)
+    {
+        char ch = s[right];
+
+        window[ch]++;
+
+        if(required.count(ch) &&
+           window[ch] == required[ch])
+        {
+            formed++;
+        }
+    }
+
+    return "";
+}
+
 int main()
 {
     string s = "ADOBECODEBANC";
     string t = "ABC";
 
-    unordered_map<char, int> required;
-
-    for(char ch : t)
-        required[ch]++;
+    cout << minWindow(s, t);
 
     return 0;
 }
