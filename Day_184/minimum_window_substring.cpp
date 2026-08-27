4c3b0a5de48bd04019f03951a13af8b508364658
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-27 11:25:13 +0530

    final minimum window substring cpp program

--- Day_184/minimum_window_substring.cpp
+++ Day_184/minimum_window_substring.cpp
@@ -3,8 +3,15 @@
 #include <unordered_map>
 using namespace std;
 
+// Minimum Window Substring
+// Sliding Window + Frequency Map
+
 string minWindow(string s, string t)
 {
+    if(t.empty() || s.empty())
+        return "";
+
+    // Store required frequency of characters
     unordered_map<char, int> required;
 
     for(char ch : t)
@@ -13,36 +20,47 @@ string minWindow(string s, string t)
     unordered_map<char, int> window;
 
     int left = 0;
+
+    // Number of character types completely satisfied
     int formed = 0;
+
     int requiredCount = required.size();
 
+    // Best window information
     int minLength = s.length() + 1;
     int start = 0;
 
+    // Expand the window
     for(int right = 0; right < s.length(); right++)
     {
         char ch = s[right];
 
         window[ch]++;
 
+        // Character requirement satisfied
         if(required.count(ch) &&
            window[ch] == required[ch])
         {
             formed++;
         }
 
-        while(left <= right && formed == requiredCount)
+        // Try shrinking the window
+        while(left <= right &&
+              formed == requiredCount)
         {
+            // Update smallest window
             if(right - left + 1 < minLength)
             {
                 minLength = right - left + 1;
                 start = left;
             }
 
+            // Remove left character
             char leftChar = s[left];
 
             window[leftChar]--;
 
+            // Window is no longer valid
             if(required.count(leftChar) &&
                window[leftChar] < required[leftChar])
             {
@@ -53,6 +71,7 @@ string minWindow(string s, string t)
         }
     }
 
+    // No valid window found
     if(minLength == s.length() + 1)
         return "";
 
@@ -64,8 +83,11 @@ int main()
     string s = "ADOBECODEBANC";
     string t = "ABC";
 
-    cout << "Minimum Window: "
-         << minWindow(s, t) << endl;
+    string result = minWindow(s, t);
+
+    cout << "String: " << s << endl;
+    cout << "Required: " << t << endl;
+    cout << "Minimum Window: " << result << endl;
 
     return 0;
 }
