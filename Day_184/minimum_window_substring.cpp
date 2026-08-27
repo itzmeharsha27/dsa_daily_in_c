452bd5a1abec73a46fd6602bf8374de77130c8b7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-27 11:24:26 +0530

    added required character frequency

--- Day_184/minimum_window_substring.cpp
+++ Day_184/minimum_window_substring.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
 using namespace std;
 
 int main()
 {
+    string s = "ADOBECODEBANC";
+    string t = "ABC";
+
+    unordered_map<char, int> required;
+
+    for(char ch : t)
+        required[ch]++;
+
     return 0;
 }
