13417f64c97da02e7761af8b5528cb789dd2d0d2
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-28 16:47:57 +0530

    added output and duplicate handling

--- Day_185/longest_substring_without_repeating.cpp
+++ Day_185/longest_substring_without_repeating.cpp
@@ -3,11 +3,42 @@
 #include <unordered_map>
 using namespace std;
 
+// Longest Substring Without Repeating Characters
+
+int lengthOfLongestSubstring(string s)
+{
+    unordered_map<char, int> freq;
+
+    int left = 0;
+    int maxLength = 0;
+
+    for(int right = 0; right < s.length(); right++)
+    {
+        freq[s[right]]++;
+
+        // Remove duplicate characters
+        while(freq[s[right]] > 1)
+        {
+            freq[s[left]]--;
+            left++;
+        }
+
+        maxLength = max(
+            maxLength,
+            right - left + 1
+        );
+    }
+
+    return maxLength;
+}
+
 int main()
 {
     string s = "abcabcbb";
 
-    unordered_map<char, int> freq;
+    cout << "String: " << s << endl;
+    cout << "Longest Length: "
+         << lengthOfLongestSubstring(s) << endl;
 
     return 0;
 }
