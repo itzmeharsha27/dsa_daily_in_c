86822725900eaff6cc7503a78e234c9c24347598
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-28 16:47:26 +0530

    added character frequency map

--- Day_185/longest_substring_without_repeating.cpp
+++ Day_185/longest_substring_without_repeating.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 using namespace std;
 
 int main()
 {
     string s = "abcabcbb";
+
+    unordered_map<char, int> freq;
 
     return 0;
 }
