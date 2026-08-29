199a4937a6e6e1b1911ea83b349c45fd5621097f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-29 20:44:31 +0530

    added frequency tracking

--- Day_186/longest_repeating_character_replacement.cpp
+++ Day_186/longest_repeating_character_replacement.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
+
+int characterReplacement(string s, int k)
+{
+    vector<int> freq(26, 0);
+
+    int left = 0;
+    int maxFreq = 0;
+    int answer = 0;
+
+    return answer;
+}
 
 int main()
 {
     string s = "AABABBA";
     int k = 1;
+
+    cout << characterReplacement(s, k);
 
     return 0;
 }
