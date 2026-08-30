1993aa7125c08bc3d002dca13a076a08b7748761
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-30 13:02:56 +0530

    added frequency arrays

--- Day_187/permutation_in_string.cpp
+++ Day_187/permutation_in_string.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int main()
 {
     string s1 = "ab";
     string s2 = "eidbaooo";
+
+    vector<int> count1(26, 0);
+    vector<int> count2(26, 0);
 
     return 0;
 }
