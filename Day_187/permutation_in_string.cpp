4df52113964eb5af55ba22b0e290e0b89a009614
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-30 13:03:21 +0530

    created initial sliding window

--- Day_187/permutation_in_string.cpp
+++ Day_187/permutation_in_string.cpp
@@ -3,13 +3,29 @@
 #include <vector>
 using namespace std;
 
+bool checkInclusion(string s1, string s2)
+{
+    if(s1.length() > s2.length())
+        return false;
+
+    vector<int> count1(26, 0);
+    vector<int> count2(26, 0);
+
+    for(char ch : s1)
+        count1[ch - 'a']++;
+
+    for(int i = 0; i < s1.length(); i++)
+        count2[s2[i] - 'a']++;
+
+    return count1 == count2;
+}
+
 int main()
 {
     string s1 = "ab";
     string s2 = "eidbaooo";
 
-    vector<int> count1(26, 0);
-    vector<int> count2(26, 0);
+    cout << checkInclusion(s1, s2);
 
     return 0;
 }
