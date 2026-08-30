77bf1952002cc4ee803eb3ea751e6a4242805353
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-30 13:03:42 +0530

    added output and comments

--- Day_187/permutation_in_string.cpp
+++ Day_187/permutation_in_string.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 using namespace std;
 
+// Permutation in String
+// Sliding Window + Frequency Array
+
 bool checkInclusion(string s1, string s2)
 {
     if(s1.length() > s2.length())
@@ -11,18 +14,23 @@ bool checkInclusion(string s1, string s2)
     vector<int> count1(26, 0);
     vector<int> count2(26, 0);
 
+    // Frequency of characters in s1
     for(char ch : s1)
         count1[ch - 'a']++;
 
     int windowSize = s1.length();
 
+    // First window
     for(int i = 0; i < windowSize; i++)
         count2[s2[i] - 'a']++;
 
     if(count1 == count2)
         return true;
 
-    for(int right = windowSize; right < s2.length(); right++)
+    // Slide the window
+    for(int right = windowSize;
+        right < s2.length();
+        right++)
     {
         count2[s2[right] - 'a']++;
 
@@ -39,6 +47,9 @@ int main()
 {
     string s1 = "ab";
     string s2 = "eidbaooo";
+
+    cout << "s1: " << s1 << endl;
+    cout << "s2: " << s2 << endl;
 
     cout << "Permutation Exists: "
          << checkInclusion(s1, s2)
