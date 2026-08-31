538200085b612a335db7815144eb8978e017ddce
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-31 14:45:57 +0530

    added pattern frequency

--- Day_188/find_all_anagrams.cpp
+++ Day_188/find_all_anagrams.cpp
@@ -3,13 +3,28 @@
 #include <vector>
 using namespace std;
 
+vector<int> findAnagrams(string s, string p)
+{
+    vector<int> result;
+
+    if(p.length() > s.length())
+        return result;
+
+    vector<int> patternFreq(26, 0);
+    vector<int> windowFreq(26, 0);
+
+    for(char ch : p)
+        patternFreq[ch - 'a']++;
+
+    return result;
+}
+
 int main()
 {
     string s = "cbaebabacd";
     string p = "abc";
 
-    vector<int> patternFreq(26, 0);
-    vector<int> windowFreq(26, 0);
+    vector<int> result = findAnagrams(s, p);
 
     return 0;
 }
