e7be0f5fe72bf74dbd8a6964444334ea303ebbe7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-08-31 14:45:49 +0530

    added frequency arrays

--- Day_188/find_all_anagrams.cpp
+++ Day_188/find_all_anagrams.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int main()
 {
     string s = "cbaebabacd";
     string p = "abc";
+
+    vector<int> patternFreq(26, 0);
+    vector<int> windowFreq(26, 0);
 
     return 0;
 }
