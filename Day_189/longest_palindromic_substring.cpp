003c357dece1d3449c4e918948f359bad1ff8570
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-01 12:51:02 +0530

    implemented odd and even palindrome search

--- Day_189/longest_palindromic_substring.cpp
+++ Day_189/longest_palindromic_substring.cpp
@@ -15,11 +15,30 @@ string expand(string s, int left, int right)
     return s.substr(left + 1, right - left - 1);
 }
 
+string longestPalindrome(string s)
+{
+    string answer = "";
+
+    for(int i = 0; i < s.length(); i++)
+    {
+        string odd = expand(s, i, i);
+        string even = expand(s, i, i + 1);
+
+        if(odd.length() > answer.length())
+            answer = odd;
+
+        if(even.length() > answer.length())
+            answer = even;
+    }
+
+    return answer;
+}
+
 int main()
 {
     string s = "babad";
 
-    cout << expand(s, 2, 2);
+    cout << longestPalindrome(s);
 
     return 0;
 }
