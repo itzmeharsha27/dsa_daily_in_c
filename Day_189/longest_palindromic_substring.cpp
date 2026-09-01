792b42e80d4820d59867e81cfd1c2d2be8c56222
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-01 12:50:24 +0530

    added palindrome function

--- Day_189/longest_palindromic_substring.cpp
+++ Day_189/longest_palindromic_substring.cpp
@@ -2,9 +2,18 @@
 #include <string>
 using namespace std;
 
+string longestPalindrome(string s)
+{
+    string answer = "";
+
+    return answer;
+}
+
 int main()
 {
     string s = "babad";
+
+    cout << longestPalindrome(s);
 
     return 0;
 }
