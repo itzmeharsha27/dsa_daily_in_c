abd2becc9527528533b204bef7983eb3538ffdf2
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-02 09:31:25 +0530

    added palindrome counting function

--- Day_190/palindromic_substrings.cpp
+++ Day_190/palindromic_substrings.cpp
@@ -2,9 +2,18 @@
 #include <string>
 using namespace std;
 
+int countPalindromes(string s)
+{
+    int count = 0;
+
+    return count;
+}
+
 int main()
 {
     string s = "aaa";
+
+    cout << countPalindromes(s);
 
     return 0;
 }
