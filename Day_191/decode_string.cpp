6f34575b2ff1198f990eb832de37028802da4e1f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-03 11:11:03 +0530

    Create decode string solution

--- Day_191/decode_string.cpp
+++ Day_191/decode_string.cpp
@@ -1,46 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    string s = "3[a2[c]]";
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> nums;
+        stack<string> strs;
 
-    stack<int> nums;
-    stack<string> strs;
+        int num = 0;
+        string current = "";
 
-    int num = 0;
-    string current = "";
+        for (char ch : s) {
+            if (isdigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+            else if (ch == '[') {
+                nums.push(num);
+                strs.push(current);
 
-    for (char ch : s) {
-        if (isdigit(ch)) {
-            num = num * 10 + (ch - '0');
+                num = 0;
+                current = "";
+            }
+            else if (isalpha(ch)) {
+                current += ch;
+            }
+            else if (ch == ']') {
+                int repeat = nums.top();
+                nums.pop();
+
+                string previous = strs.top();
+                strs.pop();
+
+                string temp = "";
+
+                for (int i = 0; i < repeat; i++)
+                    temp += current;
+
+                current = previous + temp;
+            }
         }
-        else if (ch == '[') {
-            nums.push(num);
-            strs.push(current);
 
-            num = 0;
-            current = "";
-        }
-        else if (isalpha(ch)) {
-            current += ch;
-        }
-        else if (ch == ']') {
-            int repeat = nums.top();
-            nums.pop();
-
-            string previous = strs.top();
-            strs.pop();
-
-            string temp = "";
-
-            for (int i = 0; i < repeat; i++)
-                temp += current;
-
-            current = previous + temp;
-        }
+        return current;
     }
+};
 
-    cout << current << endl;
+int main() {
+    Solution s;
+
+    cout << s.decodeString("3[a2[c]]") << endl;
 
     return 0;
 }
