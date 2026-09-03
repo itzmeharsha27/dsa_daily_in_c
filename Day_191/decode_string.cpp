b8efd6d03bf3df37c358be591c6ce42e14201ee3
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-03 11:07:22 +0530

    Add stacks for decoding

--- Day_191/decode_string.cpp
+++ Day_191/decode_string.cpp
@@ -4,10 +4,29 @@ using namespace std;
 int main() {
     string s = "3[a]2[bc]";
 
+    stack<int> nums;
+    stack<string> strs;
+
+    int num = 0;
+    string current = "";
+
     for (char ch : s) {
-        if (isdigit(ch))
-            cout << "Number: " << ch << endl;
+        if (isdigit(ch)) {
+            num = num * 10 + (ch - '0');
+        }
+        else if (ch == '[') {
+            nums.push(num);
+            strs.push(current);
+
+            num = 0;
+            current = "";
+        }
+        else if (isalpha(ch)) {
+            current += ch;
+        }
     }
+
+    cout << current << endl;
 
     return 0;
 }
