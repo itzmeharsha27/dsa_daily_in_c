14ae9585e5949bf7fb8f64cdb76f49d90c28435c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-03 11:07:11 +0530

    Detect numbers in string

--- Day_191/decode_string.cpp
+++ Day_191/decode_string.cpp
@@ -5,7 +5,8 @@ int main() {
     string s = "3[a]2[bc]";
 
     for (char ch : s) {
-        cout << ch << " ";
+        if (isdigit(ch))
+            cout << "Number: " << ch << endl;
     }
 
     return 0;
