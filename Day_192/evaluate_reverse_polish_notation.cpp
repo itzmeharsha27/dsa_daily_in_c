e2002aac2e6b6746083d68c4da873c68f883ffc9
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-04 10:13:02 +0530

    Add RPN setup

---
+++ Day_192/evaluate_reverse_polish_notation.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<string> tokens = {"2", "1", "+", "3", "*"};
+
+    for (string token : tokens)
+        cout << token << " ";
+
+    return 0;
+}
