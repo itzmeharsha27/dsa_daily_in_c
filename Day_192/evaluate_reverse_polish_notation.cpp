de473f2cf03b871f35fa0f5438b7ebd09a22c1b7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-04 10:13:22 +0530

    Push numbers into stack

--- Day_192/evaluate_reverse_polish_notation.cpp
+++ Day_192/evaluate_reverse_polish_notation.cpp
@@ -4,12 +4,18 @@ using namespace std;
 int main() {
     vector<string> tokens = {"2", "1", "+", "3", "*"};
 
+    stack<int> st;
+
     for (string token : tokens) {
-        if (token == "+" || token == "-" ||
-            token == "*" || token == "/")
-            cout << "Operator: " << token << endl;
-        else
-            cout << "Number: " << token << endl;
+        if (token != "+" && token != "-" &&
+            token != "*" && token != "/") {
+            st.push(stoi(token));
+        }
+    }
+
+    while (!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
     }
 
     return 0;
