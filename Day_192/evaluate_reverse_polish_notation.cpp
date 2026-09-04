c2198d6a0cf951e0fb6a645d6c4949b06fa5aff6
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-04 10:13:32 +0530

    Handle addition operation

--- Day_192/evaluate_reverse_polish_notation.cpp
+++ Day_192/evaluate_reverse_polish_notation.cpp
@@ -2,21 +2,23 @@
 using namespace std;
 
 int main() {
-    vector<string> tokens = {"2", "1", "+", "3", "*"};
+    vector<string> tokens = {"2", "1", "+"};
 
     stack<int> st;
 
     for (string token : tokens) {
-        if (token != "+" && token != "-" &&
-            token != "*" && token != "/") {
+        if (token == "+") {
+            int b = st.top(); st.pop();
+            int a = st.top(); st.pop();
+
+            st.push(a + b);
+        }
+        else {
             st.push(stoi(token));
         }
     }
 
-    while (!st.empty()) {
-        cout << st.top() << " ";
-        st.pop();
-    }
+    cout << st.top();
 
     return 0;
 }
