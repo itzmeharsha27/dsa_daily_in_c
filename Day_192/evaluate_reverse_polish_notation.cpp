557dd026ae54434ee227772cac9ce7ac69fbc562
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-04 10:13:55 +0530

    Create RPN solution

--- Day_192/evaluate_reverse_polish_notation.cpp
+++ Day_192/evaluate_reverse_polish_notation.cpp
@@ -1,29 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+
+        for (string token : tokens) {
+            if (token == "+" || token == "-" ||
+                token == "*" || token == "/") {
+
+                int b = st.top(); st.pop();
+                int a = st.top(); st.pop();
+
+                if (token == "+") st.push(a + b);
+                else if (token == "-") st.push(a - b);
+                else if (token == "*") st.push(a * b);
+                else st.push(a / b);
+            }
+            else {
+                st.push(stoi(token));
+            }
+        }
+
+        return st.top();
+    }
+};
+
 int main() {
+    Solution s;
+
     vector<string> tokens = {"2", "1", "+", "3", "*"};
 
-    stack<int> st;
-
-    for (string token : tokens) {
-        if (token == "+" || token == "-" ||
-            token == "*" || token == "/") {
-
-            int b = st.top(); st.pop();
-            int a = st.top(); st.pop();
-
-            if (token == "+") st.push(a + b);
-            else if (token == "-") st.push(a - b);
-            else if (token == "*") st.push(a * b);
-            else st.push(a / b);
-        }
-        else {
-            st.push(stoi(token));
-        }
-    }
-
-    cout << st.top();
+    cout << s.evalRPN(tokens);
 
     return 0;
 }
