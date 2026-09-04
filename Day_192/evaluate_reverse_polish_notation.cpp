102a5c10fe3dc5da116f60de81fd90ebf684940c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-04 10:13:45 +0530

    Handle all RPN operations

--- Day_192/evaluate_reverse_polish_notation.cpp
+++ Day_192/evaluate_reverse_polish_notation.cpp
@@ -2,16 +2,21 @@
 using namespace std;
 
 int main() {
-    vector<string> tokens = {"2", "1", "+"};
+    vector<string> tokens = {"2", "1", "+", "3", "*"};
 
     stack<int> st;
 
     for (string token : tokens) {
-        if (token == "+") {
+        if (token == "+" || token == "-" ||
+            token == "*" || token == "/") {
+
             int b = st.top(); st.pop();
             int a = st.top(); st.pop();
 
-            st.push(a + b);
+            if (token == "+") st.push(a + b);
+            else if (token == "-") st.push(a - b);
+            else if (token == "*") st.push(a * b);
+            else st.push(a / b);
         }
         else {
             st.push(stoi(token));
