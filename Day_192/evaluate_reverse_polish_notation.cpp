37c87f83b233a8e5e3d819f8e185dd88aa2cca41
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-04 10:14:16 +0530

    Finalize RPN solution

--- Day_192/evaluate_reverse_polish_notation.cpp
+++ Day_192/evaluate_reverse_polish_notation.cpp
@@ -10,13 +10,20 @@ public:
             if (token == "+" || token == "-" ||
                 token == "*" || token == "/") {
 
-                int b = st.top(); st.pop();
-                int a = st.top(); st.pop();
+                int b = st.top();
+                st.pop();
 
-                if (token == "+") st.push(a + b);
-                else if (token == "-") st.push(a - b);
-                else if (token == "*") st.push(a * b);
-                else st.push(a / b);
+                int a = st.top();
+                st.pop();
+
+                if (token == "+")
+                    st.push(a + b);
+                else if (token == "-")
+                    st.push(a - b);
+                else if (token == "*")
+                    st.push(a * b);
+                else
+                    st.push(a / b);
             }
             else {
                 st.push(stoi(token));
@@ -30,12 +37,9 @@ public:
 int main() {
     Solution s;
 
-    vector<string> tokens = {
-        "10", "6", "9", "3", "+", "-11", "*",
-        "/", "*", "17", "+", "5", "+"
-    };
+    vector<string> tokens = {"2", "1", "+", "3", "*"};
 
-    cout << s.evalRPN(tokens);
+    cout << s.evalRPN(tokens) << endl;
 
     return 0;
 }
