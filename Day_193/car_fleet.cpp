3bb74599cedd80072882c08f45da5a61c59482dd
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-05 12:01:54 +0530

    Add car fleet setup

---
+++ Day_193/car_fleet.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int target = 12;
+
+    vector<int> position = {10, 8, 0, 5, 3};
+    vector<int> speed = {2, 4, 1, 1, 3};
+
+    cout << target << endl;
+
+    return 0;
+}
