558a76b4d5c02ad68edae7adcdf16acb42f4b7bc
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-05 12:02:03 +0530

    Calculate arrival times

--- Day_193/car_fleet.cpp
+++ Day_193/car_fleet.cpp
@@ -7,7 +7,10 @@ int main() {
     vector<int> position = {10, 8, 0, 5, 3};
     vector<int> speed = {2, 4, 1, 1, 3};
 
-    cout << target << endl;
+    for (int i = 0; i < position.size(); i++) {
+        double time = (double)(target - position[i]) / speed[i];
+        cout << time << " ";
+    }
 
     return 0;
 }
