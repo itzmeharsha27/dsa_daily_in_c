cdfa8a6e11e4cb2231514fc081ba3edf562d47e1
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-05 12:03:19 +0530

    Create car fleet solution

--- Day_193/car_fleet.cpp
+++ Day_193/car_fleet.cpp
@@ -1,32 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int target = 12;
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
 
+        vector<pair<int, double>> cars;
+
+        for (int i = 0; i < position.size(); i++) {
+            double time = (double)(target - position[i]) / speed[i];
+            cars.push_back({position[i], time});
+        }
+
+        sort(cars.rbegin(), cars.rend());
+
+        int fleets = 0;
+        double lastTime = 0;
+
+        for (auto car : cars) {
+            if (car.second > lastTime) {
+                fleets++;
+                lastTime = car.second;
+            }
+        }
+
+        return fleets;
+    }
+};
+
+int main() {
+    Solution s;
+
+    int target = 12;
     vector<int> position = {10, 8, 0, 5, 3};
     vector<int> speed = {2, 4, 1, 1, 3};
 
-    vector<pair<int, double>> cars;
-
-    for (int i = 0; i < position.size(); i++) {
-        double time = (double)(target - position[i]) / speed[i];
-        cars.push_back({position[i], time});
-    }
-
-    sort(cars.rbegin(), cars.rend());
-
-    int fleets = 0;
-    double lastTime = 0;
-
-    for (auto car : cars) {
-        if (car.second > lastTime) {
-            fleets++;
-            lastTime = car.second;
-        }
-    }
-
-    cout << fleets;
+    cout << s.carFleet(target, position, speed);
 
     return 0;
 }
