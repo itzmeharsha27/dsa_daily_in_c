a357734c49495801ba6377ebc73236996d51a017
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-05 12:02:42 +0530

    Store car arrival times

--- Day_193/car_fleet.cpp
+++ Day_193/car_fleet.cpp
@@ -7,10 +7,12 @@ int main() {
     vector<int> position = {10, 8, 0, 5, 3};
     vector<int> speed = {2, 4, 1, 1, 3};
 
-    vector<pair<int, int>> cars;
+    vector<pair<int, double>> cars;
 
-    for (int i = 0; i < position.size(); i++)
-        cars.push_back({position[i], speed[i]});
+    for (int i = 0; i < position.size(); i++) {
+        double time = (double)(target - position[i]) / speed[i];
+        cars.push_back({position[i], time});
+    }
 
     sort(cars.begin(), cars.end());
 
