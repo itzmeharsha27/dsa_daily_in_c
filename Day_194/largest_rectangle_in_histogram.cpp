6cef68cff91af9684f613bdcfdd8f18b0565b27d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-06 10:51:36 +0530

    Add histogram setup

---
+++ Day_194/largest_rectangle_in_histogram.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> heights = {2, 1, 5, 6, 2, 3};
+
+    for (int h : heights)
+        cout << h << " ";
+
+    return 0;
+}
