98d539a996a2b968fa4d4fa9acf8349d38295f1b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-06 10:53:22 +0530

    Calculate maximum histogram area

--- Day_194/largest_rectangle_in_histogram.cpp
+++ Day_194/largest_rectangle_in_histogram.cpp
@@ -5,16 +5,31 @@ int main() {
     vector<int> heights = {2, 1, 5, 6, 2, 3};
 
     stack<int> st;
+    int maxArea = 0;
+
+    heights.push_back(0);
 
     for (int i = 0; i < heights.size(); i++) {
 
-        while (!st.empty() && heights[i] < heights[st.top()])
+        while (!st.empty() && heights[i] < heights[st.top()]) {
+
+            int height = heights[st.top()];
             st.pop();
+
+            int width;
+
+            if (st.empty())
+                width = i;
+            else
+                width = i - st.top() - 1;
+
+            maxArea = max(maxArea, height * width);
+        }
 
         st.push(i);
     }
 
-    cout << "Monotonic stack created";
+    cout << maxArea;
 
     return 0;
 }
