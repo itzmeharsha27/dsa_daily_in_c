138541276f5541b0221f5d76672c590a67b72b8b
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-07 17:38:15 +0530

    Add binary search comparisons

--- Day_195/inary_search.cpp
+++ Day_195/inary_search.cpp
@@ -3,13 +3,25 @@ using namespace std;
 
 int main() {
     vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
 
     int left = 0;
     int right = nums.size() - 1;
 
-    int mid = left + (right - left) / 2;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
 
-    cout << nums[mid];
+        if (nums[mid] == target) {
+            cout << "Found at index " << mid;
+            break;
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
 
     return 0;
 }
