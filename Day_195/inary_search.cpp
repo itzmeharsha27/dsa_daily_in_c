30789831bac296ce887518fec2f3428ab001af2d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-07 17:38:25 +0530

    Handle target not found

--- Day_195/inary_search.cpp
+++ Day_195/inary_search.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 int main() {
     vector<int> nums = {-1, 0, 3, 5, 9, 12};
-    int target = 9;
+    int target = 2;
 
     int left = 0;
     int right = nums.size() - 1;
+    int answer = -1;
 
     while (left <= right) {
         int mid = left + (right - left) / 2;
 
         if (nums[mid] == target) {
-            cout << "Found at index " << mid;
+            answer = mid;
             break;
         }
         else if (nums[mid] < target) {
@@ -22,6 +23,8 @@ int main() {
             right = mid - 1;
         }
     }
+
+    cout << answer;
 
     return 0;
 }
