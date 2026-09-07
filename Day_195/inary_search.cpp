3035744e84e1b816dc963f7f4071aa585060b35f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-07 17:37:09 +0530

    Add binary search setup

---
+++ Day_195/inary_search.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+
+    cout << "Target: " << target << endl;
+
+    return 0;
+}
