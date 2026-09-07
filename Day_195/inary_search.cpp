71c23c2e2dc82992d1f962fbd941c96e1cc5150e
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-07 17:38:43 +0530

    Create binary search function

--- Day_195/inary_search.cpp
+++ Day_195/inary_search.cpp
@@ -1,30 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    vector<int> nums = {-1, 0, 3, 5, 9, 12};
-    int target = 2;
+int binarySearch(vector<int>& nums, int target) {
 
     int left = 0;
     int right = nums.size() - 1;
-    int answer = -1;
 
     while (left <= right) {
+
         int mid = left + (right - left) / 2;
 
-        if (nums[mid] == target) {
-            answer = mid;
-            break;
-        }
-        else if (nums[mid] < target) {
+        if (nums[mid] == target)
+            return mid;
+
+        if (nums[mid] < target)
             left = mid + 1;
-        }
-        else {
+        else
             right = mid - 1;
-        }
     }
 
-    cout << answer;
+    return -1;
+}
+
+int main() {
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+
+    cout << binarySearch(nums, 9);
 
     return 0;
 }
