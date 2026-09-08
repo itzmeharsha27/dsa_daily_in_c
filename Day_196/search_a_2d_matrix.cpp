bfe863cb48b35f21ae5f80bfb78975c3f66be904
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-08 18:57:42 +0530

    Combine matrix search steps

--- Day_196/search_a_2d_matrix.cpp
+++ Day_196/search_a_2d_matrix.cpp
@@ -9,25 +9,35 @@ int main() {
     };
 
     int target = 16;
-    int row = 1;
+    int row = -1;
 
-    int left = 0;
-    int right = matrix[row].size() - 1;
+    for (int i = 0; i < matrix.size(); i++) {
+        if (target >= matrix[i][0] &&
+            target <= matrix[i].back()) {
+            row = i;
+            break;
+        }
+    }
 
     bool found = false;
 
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
+    if (row != -1) {
+        int left = 0;
+        int right = matrix[row].size() - 1;
 
-        if (matrix[row][mid] == target) {
-            found = true;
-            break;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (matrix[row][mid] == target) {
+                found = true;
+                break;
+            }
+
+            if (matrix[row][mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
-
-        if (matrix[row][mid] < target)
-            left = mid + 1;
-        else
-            right = mid - 1;
     }
 
     cout << found;
