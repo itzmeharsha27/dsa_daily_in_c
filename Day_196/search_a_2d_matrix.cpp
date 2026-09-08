30796f7718d9bb466cddea2c1195a26d81981680
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-08 18:53:05 +0530

    Add 2D matrix search setup

---
+++ Day_196/search_a_2d_matrix.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 3, 5, 7},
+        {10, 11, 16, 20},
+        {23, 30, 34, 60}
+    };
+
+    int target = 3;
+
+    cout << target;
+
+    return 0;
+}
