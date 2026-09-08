3749e6e8dde532d3bf4ab015cdafcb74649866c0
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-08 18:57:52 +0530

    Create matrix search solution

--- Day_196/search_a_2d_matrix.cpp
+++ Day_196/search_a_2d_matrix.cpp
@@ -1,46 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    vector<vector<int>> matrix = {
-        {1, 3, 5, 7},
-        {10, 11, 16, 20},
-        {23, 30, 34, 60}
-    };
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
-    int target = 16;
-    int row = -1;
+        int row = -1;
 
-    for (int i = 0; i < matrix.size(); i++) {
-        if (target >= matrix[i][0] &&
-            target <= matrix[i].back()) {
-            row = i;
-            break;
+        for (int i = 0; i < matrix.size(); i++) {
+            if (target >= matrix[i][0] &&
+                target <= matrix[i].back()) {
+                row = i;
+                break;
+            }
         }
-    }
 
-    bool found = false;
+        if (row == -1)
+            return false;
 
-    if (row != -1) {
         int left = 0;
         int right = matrix[row].size() - 1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            if (matrix[row][mid] == target) {
-                found = true;
-                break;
-            }
+            if (matrix[row][mid] == target)
+                return true;
 
             if (matrix[row][mid] < target)
                 left = mid + 1;
             else
                 right = mid - 1;
         }
-    }
 
-    cout << found;
+        return false;
+    }
+};
+
+int main() {
+    Solution s;
+
+    vector<vector<int>> matrix = {
+        {1, 3, 5, 7},
+        {10, 11, 16, 20},
+        {23, 30, 34, 60}
+    };
+
+    cout << s.searchMatrix(matrix, 16);
 
     return 0;
 }
