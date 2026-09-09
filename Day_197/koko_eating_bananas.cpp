a45ebda90dd752babb374badf5fec15c1748de02
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-09 19:03:26 +0530

    Add Koko eating bananas setup

---
+++ Day_197/koko_eating_bananas.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> piles = {3, 6, 7, 11};
+    int h = 8;
+
+    cout << "Piles: ";
+
+    for (int x : piles)
+        cout << x << " ";
+
+    cout << "\nHours: " << h;
+
+    return 0;
+}
