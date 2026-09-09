c226dc87d4c7cb405f7d5f2ef6c66b3405eb6280
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-09 19:04:11 +0530

    Add speed feasibility function

--- Day_197/koko_eating_bananas.cpp
+++ Day_197/koko_eating_bananas.cpp
@@ -5,18 +5,25 @@ int main() {
     vector<int> piles = {3, 6, 7, 11};
     int h = 8;
 
-    for (int speed = 1; speed <= 11; speed++) {
+    int left = 1;
+    int right = *max_element(piles.begin(), piles.end());
+
+    while (left <= right) {
+
+        int speed = left + (right - left) / 2;
 
         int hours = 0;
 
         for (int bananas : piles)
             hours += ceil((double)bananas / speed);
 
-        if (hours <= h) {
-            cout << "Minimum speed: " << speed;
-            break;
-        }
+        if (hours <= h)
+            right = speed - 1;
+        else
+            left = speed + 1;
     }
+
+    cout << left;
 
     return 0;
 }
