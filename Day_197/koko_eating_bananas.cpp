a9d013ad405ec3033e1b63b6e0cb51c91dc483c8
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-09 19:04:28 +0530

    Create Koko solution

--- Day_197/koko_eating_bananas.cpp
+++ Day_197/koko_eating_bananas.cpp
@@ -1,29 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    vector<int> piles = {3, 6, 7, 11};
-    int h = 8;
+class Solution {
+public:
+    bool canFinish(vector<int>& piles, int h, int speed) {
 
-    int left = 1;
-    int right = *max_element(piles.begin(), piles.end());
-
-    while (left <= right) {
-
-        int speed = left + (right - left) / 2;
-
-        int hours = 0;
+        long long hours = 0;
 
         for (int bananas : piles)
-            hours += ceil((double)bananas / speed);
+            hours += (bananas + speed - 1) / speed;
 
-        if (hours <= h)
-            right = speed - 1;
-        else
-            left = speed + 1;
+        return hours <= h;
     }
 
-    cout << left;
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+
+        while (left <= right) {
+
+            int speed = left + (right - left) / 2;
+
+            if (canFinish(piles, h, speed))
+                right = speed - 1;
+            else
+                left = speed + 1;
+        }
+
+        return left;
+    }
+};
+
+int main() {
+    Solution s;
+
+    vector<int> piles = {3, 6, 7, 11};
+
+    cout << s.minEatingSpeed(piles, 8);
 
     return 0;
 }
