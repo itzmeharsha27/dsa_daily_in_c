4cd33afb2b11c3cab0012ac7390892e898c05cea
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-10 17:42:14 +0530

    Add linear search

--- Day_198/search_in_rotated_sorted_array.cpp
+++ Day_198/search_in_rotated_sorted_array.cpp
@@ -1,1 +1,18 @@
-search_in_rotated_sorted_array.cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == target) {
+            cout << i;
+            return 0;
+        }
+    }
+
+    cout << -1;
+
+    return 0;
+}
