baff91c4ed36427d676ddcf7d0e6c40f51a1e028
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-10 17:43:00 +0530

    Search within sorted half

--- Day_198/search_in_rotated_sorted_array.cpp
+++ Day_198/search_in_rotated_sorted_array.cpp
@@ -3,22 +3,37 @@ using namespace std;
 
 int main() {
     vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 5;
 
     int left = 0;
     int right = nums.size() - 1;
 
     while (left <= right) {
+
         int mid = left + (right - left) / 2;
 
-        if (nums[left] <= nums[mid]) {
-            cout << "Left half is sorted\n";
-            break;
+        if (nums[mid] == target) {
+            cout << mid;
+            return 0;
         }
-        else {
-            cout << "Right half is sorted\n";
-            break;
+
+        if (nums[left] <= nums[mid]) {
+
+            if (nums[left] <= target && target < nums[mid])
+                right = mid - 1;
+            else
+                left = mid + 1;
+
+        } else {
+
+            if (nums[mid] < target && target <= nums[right])
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
     }
+
+    cout << -1;
 
     return 0;
 }
