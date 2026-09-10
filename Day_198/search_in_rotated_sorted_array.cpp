5c0d259c2c659a0e278781c3b1c6379747076c6f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-10 17:43:24 +0530

    Create rotated search solution

--- Day_198/search_in_rotated_sorted_array.cpp
+++ Day_198/search_in_rotated_sorted_array.cpp
@@ -1,41 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int searchRotated(vector<int>& nums, int target) {
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
 
-    int left = 0;
-    int right = nums.size() - 1;
+        int left = 0;
+        int right = nums.size() - 1;
 
-    while (left <= right) {
+        while (left <= right) {
 
-        int mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2;
 
-        if (nums[mid] == target)
-            return mid;
+            if (nums[mid] == target)
+                return mid;
 
-        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= nums[mid]) {
 
-            if (nums[left] <= target && target < nums[mid])
-                right = mid - 1;
-            else
-                left = mid + 1;
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
 
-        } else {
+            } else {
 
-            if (nums[mid] < target && target <= nums[right])
-                left = mid + 1;
-            else
-                right = mid - 1;
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
         }
-    }
 
-    return -1;
-}
+        return -1;
+    }
+};
 
 int main() {
+    Solution s;
+
     vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
 
-    cout << searchRotated(nums, 0);
+    cout << s.search(nums, 0);
 
     return 0;
 }
