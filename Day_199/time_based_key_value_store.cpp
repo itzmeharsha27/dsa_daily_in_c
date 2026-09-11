5d000f3bd4992033efa7ef8b6e61da7d2ab9d055
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-11 18:49:08 +0530

    Add get operation

--- Day_199/time_based_key_value_store.cpp
+++ Day_199/time_based_key_value_store.cpp
@@ -8,6 +8,23 @@ public:
     void set(string key, string value, int timestamp) {
         data[key].push_back({timestamp, value});
     }
+
+    string get(string key, int timestamp) {
+
+        if (!data.count(key))
+            return "";
+
+        string answer = "";
+
+        for (auto p : data[key]) {
+            if (p.first <= timestamp)
+                answer = p.second;
+            else
+                break;
+        }
+
+        return answer;
+    }
 };
 
 int main() {
@@ -16,7 +33,7 @@ int main() {
     obj.set("foo", "bar", 1);
     obj.set("foo", "bar2", 4);
 
-    cout << "Values stored";
+    cout << obj.get("foo", 3);
 
     return 0;
 }
