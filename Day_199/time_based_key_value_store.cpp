f0b614b23958ffda51ade00f53e0c1adcf225f3c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-11 18:48:31 +0530

    Store multiple timestamp values

--- Day_199/time_based_key_value_store.cpp
+++ Day_199/time_based_key_value_store.cpp
@@ -5,8 +5,10 @@ int main() {
     map<string, vector<pair<int, string>>> data;
 
     data["foo"].push_back({1, "bar"});
+    data["foo"].push_back({4, "bar2"});
 
-    cout << data["foo"][0].second;
+    for (auto p : data["foo"])
+        cout << p.first << " " << p.second << endl;
 
     return 0;
 }
