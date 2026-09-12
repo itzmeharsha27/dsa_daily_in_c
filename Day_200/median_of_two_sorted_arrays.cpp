fece939f0b01f856a3e033c7a44c2188cb78ca3c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-12 14:04:19 +0530

    Finalize median of sorted arrays solution

--- Day_200/median_of_two_sorted_arrays.cpp
+++ Day_200/median_of_two_sorted_arrays.cpp
@@ -1,47 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double findMedian(vector<int>& A, vector<int>& B) {
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1,
+                                  vector<int>& nums2) {
 
-    if (A.size() > B.size())
-        return findMedian(B, A);
+        if (nums1.size() > nums2.size())
+            return findMedianSortedArrays(nums2, nums1);
 
-    int m = A.size();
-    int n = B.size();
+        int m = nums1.size();
+        int n = nums2.size();
 
-    int left = 0;
-    int right = m;
+        int left = 0;
+        int right = m;
 
-    while (left <= right) {
+        while (left <= right) {
 
-        int cutA = left + (right - left) / 2;
-        int cutB = (m + n + 1) / 2 - cutA;
+            int cut1 = left + (right - left) / 2;
+            int cut2 = (m + n + 1) / 2 - cut1;
 
-        int leftA = (cutA == 0) ? INT_MIN : A[cutA - 1];
-        int rightA = (cutA == m) ? INT_MAX : A[cutA];
+            int left1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+            int right1 = (cut1 == m) ? INT_MAX : nums1[cut1];
 
-        int leftB = (cutB == 0) ? INT_MIN : B[cutB - 1];
-        int rightB = (cutB == n) ? INT_MAX : B[cutB];
+            int left2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+            int right2 = (cut2 == n) ? INT_MAX : nums2[cut2];
 
-        if (leftA <= rightB && leftB <= rightA) {
-            cout << "Correct partition found";
-            return 0;
+            if (left1 <= right2 && left2 <= right1) {
+
+                if ((m + n) % 2 == 1)
+                    return max(left1, left2);
+
+                return (max(left1, left2) +
+                        min(right1, right2)) / 2.0;
+            }
+
+            if (left1 > right2)
+                right = cut1 - 1;
+            else
+                left = cut1 + 1;
         }
 
-        if (leftA > rightB)
-            right = cutA - 1;
-        else
-            left = cutA + 1;
+        return 0.0;
     }
-
-    return 0;
-}
+};
 
 int main() {
-    vector<int> A = {1, 3};
-    vector<int> B = {2};
+    Solution s;
 
-    findMedian(A, B);
+    vector<int> nums1 = {1, 3};
+    vector<int> nums2 = {2};
+
+    cout << s.findMedianSortedArrays(nums1, nums2) << endl;
 
     return 0;
 }
