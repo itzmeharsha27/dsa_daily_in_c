e8247ab97555a856450827f0ab081b33aa22c1bc
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-13 17:36:00 +0530

    Add kth smallest matrix setup

---
+++ Day_201/kth_smallest_element_in_sorted_matrix.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 5, 9},
+        {10, 11, 13},
+        {12, 13, 15}
+    };
+
+    int k = 8;
+
+    cout << "K = " << k << endl;
+
+    return 0;
+}
