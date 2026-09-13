9742987a5bc496277d7ec8fa080c371771b9854a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-13 17:38:02 +0530

    Finalize kth smallest matrix solution

--- Day_201/kth_smallest_element_in_sorted_matrix.cpp
+++ Day_201/kth_smallest_element_in_sorted_matrix.cpp
@@ -3,25 +3,28 @@ using namespace std;
 
 class Solution {
 public:
-    int countLessEqual(vector<vector<int>>& matrix, int value) {
-        int count = 0;
-
-        for (auto& row : matrix)
-            count += upper_bound(row.begin(), row.end(), value) - row.begin();
-
-        return count;
-    }
-
     int kthSmallest(vector<vector<int>>& matrix, int k) {
 
+        int n = matrix.size();
+
         int left = matrix[0][0];
-        int right = matrix.back().back();
+        int right = matrix[n - 1][n - 1];
 
         while (left < right) {
 
             int mid = left + (right - left) / 2;
 
-            if (countLessEqual(matrix, mid) < k)
+            int count = 0;
+
+            for (auto& row : matrix) {
+                count += upper_bound(
+                    row.begin(),
+                    row.end(),
+                    mid
+                ) - row.begin();
+            }
+
+            if (count < k)
                 left = mid + 1;
             else
                 right = mid;
@@ -40,7 +43,9 @@ int main() {
         {12, 13, 15}
     };
 
-    cout << s.kthSmallest(matrix, 8);
+    int k = 8;
+
+    cout << s.kthSmallest(matrix, k) << endl;
 
     return 0;
 }
