fa9e90ae84915013557c3d8624dfa4dea3cb3bbb
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-13 17:37:32 +0530

    Add binary search on answer

--- Day_201/kth_smallest_element_in_sorted_matrix.cpp
+++ Day_201/kth_smallest_element_in_sorted_matrix.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int countLessEqual(vector<int>& row, int value) {
-    return upper_bound(row.begin(), row.end(), value) - row.begin();
+int countLessEqual(vector<vector<int>>& matrix, int value) {
+    int count = 0;
+
+    for (auto& row : matrix)
+        count += upper_bound(row.begin(), row.end(), value) - row.begin();
+
+    return count;
 }
 
 int main() {
@@ -12,13 +17,21 @@ int main() {
         {12, 13, 15}
     };
 
-    int value = 12;
-    int count = 0;
+    int k = 8;
 
-    for (auto& row : matrix)
-        count += countLessEqual(row, value);
+    int left = matrix[0][0];
+    int right = matrix.back().back();
 
-    cout << count;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (countLessEqual(matrix, mid) < k)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+
+    cout << left;
 
     return 0;
 }
