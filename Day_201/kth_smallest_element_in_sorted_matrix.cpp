e6503746e4cea43e999588bba26ef9c7fbb7e861
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-13 17:37:46 +0530

    Create kth smallest solution

--- Day_201/kth_smallest_element_in_sorted_matrix.cpp
+++ Day_201/kth_smallest_element_in_sorted_matrix.cpp
@@ -1,37 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int countLessEqual(vector<vector<int>>& matrix, int value) {
-    int count = 0;
+class Solution {
+public:
+    int countLessEqual(vector<vector<int>>& matrix, int value) {
+        int count = 0;
 
-    for (auto& row : matrix)
-        count += upper_bound(row.begin(), row.end(), value) - row.begin();
+        for (auto& row : matrix)
+            count += upper_bound(row.begin(), row.end(), value) - row.begin();
 
-    return count;
-}
+        return count;
+    }
+
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+
+        int left = matrix[0][0];
+        int right = matrix.back().back();
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (countLessEqual(matrix, mid) < k)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+
+        return left;
+    }
+};
 
 int main() {
+    Solution s;
+
     vector<vector<int>> matrix = {
         {1, 5, 9},
         {10, 11, 13},
         {12, 13, 15}
     };
 
-    int k = 8;
-
-    int left = matrix[0][0];
-    int right = matrix.back().back();
-
-    while (left < right) {
-        int mid = left + (right - left) / 2;
-
-        if (countLessEqual(matrix, mid) < k)
-            left = mid + 1;
-        else
-            right = mid;
-    }
-
-    cout << left;
+    cout << s.kthSmallest(matrix, 8);
 
     return 0;
 }
