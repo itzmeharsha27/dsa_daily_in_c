f69dc05208df80db5786df452ffc37013542721f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-14 23:00:51 +0530

    Add linear peak search

--- Day_202/day157_find_peak_element.cpp
+++ Day_202/day157_find_peak_element.cpp
@@ -4,8 +4,17 @@ using namespace std;
 int main() {
     vector<int> nums = {1, 2, 3, 1};
 
-    for (int x : nums)
-        cout << x << " ";
+    for (int i = 0; i < nums.size(); i++) {
+
+        bool leftOK = (i == 0 || nums[i] > nums[i - 1]);
+        bool rightOK = (i == nums.size() - 1 ||
+                        nums[i] > nums[i + 1]);
+
+        if (leftOK && rightOK) {
+            cout << i;
+            return 0;
+        }
+    }
 
     return 0;
 }
