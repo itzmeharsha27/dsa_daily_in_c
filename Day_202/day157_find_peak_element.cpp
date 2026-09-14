b3ec9bdb1312a69f98904a6253d2ef3fb8cc112e
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-14 23:01:00 +0530

    Introduce binary search for peak

--- Day_202/day157_find_peak_element.cpp
+++ Day_202/day157_find_peak_element.cpp
@@ -4,16 +4,15 @@ using namespace std;
 int main() {
     vector<int> nums = {1, 2, 3, 1};
 
-    for (int i = 0; i < nums.size(); i++) {
+    int left = 0;
+    int right = nums.size() - 1;
 
-        bool leftOK = (i == 0 || nums[i] > nums[i - 1]);
-        bool rightOK = (i == nums.size() - 1 ||
-                        nums[i] > nums[i + 1]);
+    while (left < right) {
 
-        if (leftOK && rightOK) {
-            cout << i;
-            return 0;
-        }
+        int mid = left + (right - left) / 2;
+
+        cout << "Mid: " << mid << endl;
+        break;
     }
 
     return 0;
