5859453c36f14804a587ac622cbd9c4bea726868
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-15 21:17:11 +0530

    Add binary search

--- Day_203/find_first_and_last_position.cpp
+++ Day_203/find_first_and_last_position.cpp
@@ -5,7 +5,23 @@ int main() {
     vector<int> nums = {5, 7, 7, 8, 8, 10};
     int target = 8;
 
-    cout << "Target: " << target;
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            cout << "Found at: " << mid;
+            break;
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
 
     return 0;
 }
