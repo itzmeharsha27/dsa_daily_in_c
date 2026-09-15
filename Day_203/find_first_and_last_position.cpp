63041f23a5949a7f4c5a8732e1d0be1882648154
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-15 21:16:54 +0530

    Add first and last position setup

---
+++ Day_203/find_first_and_last_position.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> nums = {5, 7, 7, 8, 8, 10};
+    int target = 8;
+
+    cout << "Target: " << target;
+
+    return 0;
+}
