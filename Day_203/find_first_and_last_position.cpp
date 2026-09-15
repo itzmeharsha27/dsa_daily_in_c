da53c38b3d6a2b2c9e3337e65ca1a80e573a63a0
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-15 21:17:47 +0530

    Combine first and last search

--- Day_203/find_first_and_last_position.cpp
+++ Day_203/find_first_and_last_position.cpp
@@ -5,9 +5,29 @@ int main() {
     vector<int> nums = {5, 7, 7, 8, 8, 10};
     int target = 8;
 
+    int first = -1;
+    int last = -1;
+
     int left = 0;
     int right = nums.size() - 1;
-    int last = -1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            first = mid;
+            right = mid - 1;
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    left = 0;
+    right = nums.size() - 1;
 
     while (left <= right) {
         int mid = left + (right - left) / 2;
@@ -24,7 +44,7 @@ int main() {
         }
     }
 
-    cout << last;
+    cout << first << " " << last;
 
     return 0;
 }
