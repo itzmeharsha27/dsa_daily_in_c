c130e92fdf63edaf9c639c2127cea8930f2ba13f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-15 21:18:14 +0530

    Create search range solution

--- Day_203/find_first_and_last_position.cpp
+++ Day_203/find_first_and_last_position.cpp
@@ -1,57 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findFirst(vector<int>& nums, int target) {
-    int left = 0;
-    int right = nums.size() - 1;
-    int answer = -1;
+class Solution {
+public:
+    int findFirst(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int answer = -1;
 
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-        if (nums[mid] == target) {
-            answer = mid;
-            right = mid - 1;
+            if (nums[mid] == target) {
+                answer = mid;
+                right = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
         }
-        else if (nums[mid] < target) {
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
-        }
+
+        return answer;
     }
 
-    return answer;
-}
+    int findLast(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int answer = -1;
 
-int findLast(vector<int>& nums, int target) {
-    int left = 0;
-    int right = nums.size() - 1;
-    int answer = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                answer = mid;
+                left = mid + 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
 
-        if (nums[mid] == target) {
-            answer = mid;
-            left = mid + 1;
-        }
-        else if (nums[mid] < target) {
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
-        }
+        return answer;
     }
 
-    return answer;
-}
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {findFirst(nums, target), findLast(nums, target)};
+    }
+};
 
 int main() {
+    Solution s;
+
     vector<int> nums = {5, 7, 7, 8, 8, 10};
 
-    cout << findFirst(nums, 8) << " ";
-    cout << findLast(nums, 8);
+    vector<int> ans = s.searchRange(nums, 8);
+
+    cout << ans[0] << " " << ans[1];
 
     return 0;
 }
