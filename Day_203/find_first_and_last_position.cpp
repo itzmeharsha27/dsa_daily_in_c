c6e1ff8422712eaf088c52527d4a0078822ce7df
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-15 21:18:24 +0530

    Test search range edge cases

--- Day_203/find_first_and_last_position.cpp
+++ Day_203/find_first_and_last_position.cpp
@@ -3,65 +3,66 @@ using namespace std;
 
 class Solution {
 public:
-    int findFirst(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-        int answer = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                answer = mid;
-                right = mid - 1;
-            }
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
-            }
-        }
-
-        return answer;
-    }
-
-    int findLast(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-        int answer = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                answer = mid;
-                left = mid + 1;
-            }
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
-            }
-        }
-
-        return answer;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {findFirst(nums, target), findLast(nums, target)};
+
+        int first = -1;
+        int last = -1;
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] >= target) {
+                if (nums[mid] == target)
+                    first = mid;
+
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        left = 0;
+        right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] <= target) {
+                if (nums[mid] == target)
+                    last = mid;
+
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return {first, last};
     }
 };
 
 int main() {
     Solution s;
 
-    vector<int> nums = {5, 7, 7, 8, 8, 10};
+    vector<int> a = {5, 7, 7, 8, 8, 10};
+    vector<int> b = {1};
+    vector<int> c = {2, 2, 2, 2};
 
-    vector<int> ans = s.searchRange(nums, 8);
+    for (int x : s.searchRange(a, 8))
+        cout << x << " ";
+    cout << endl;
 
-    cout << ans[0] << " " << ans[1];
+    for (int x : s.searchRange(b, 1))
+        cout << x << " ";
+    cout << endl;
+
+    for (int x : s.searchRange(c, 3))
+        cout << x << " ";
 
     return 0;
 }
