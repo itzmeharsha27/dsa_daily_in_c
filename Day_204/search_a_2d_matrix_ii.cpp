784c96e9242b2f452ffedbb1637f2a667a694967
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-16 18:36:18 +0530

    Add 2D matrix II setup

---
+++ Day_204/search_a_2d_matrix_ii.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1,  4,  7, 11, 15},
+        {2,  5,  8, 12, 19},
+        {3,  6,  9, 16, 22},
+        {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+
+    int target = 5;
+
+    cout << "Target: " << target << endl;
+
+    return 0;
+}
