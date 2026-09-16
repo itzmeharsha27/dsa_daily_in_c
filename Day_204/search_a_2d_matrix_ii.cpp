d7c83dd5035a3494fd54f5afe05962fcc0ae8e21
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-16 18:36:51 +0530

    Add matrix movement logic

--- Day_204/search_a_2d_matrix_ii.cpp
+++ Day_204/search_a_2d_matrix_ii.cpp
@@ -1,2 +1,34 @@
-git add .
-git commit -m "Add linear matrix search"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1,  4,  7, 11, 15},
+        {2,  5,  8, 12, 19},
+        {3,  6,  9, 16, 22},
+        {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+
+    int target = 5;
+
+    int row = 0;
+    int col = matrix[0].size() - 1;
+
+    while (row < matrix.size() && col >= 0) {
+
+        if (matrix[row][col] == target) {
+            cout << "Found";
+            return 0;
+        }
+
+        if (matrix[row][col] > target)
+            col--;
+        else
+            row++;
+    }
+
+    cout << "Not Found";
+
+    return 0;
+}
