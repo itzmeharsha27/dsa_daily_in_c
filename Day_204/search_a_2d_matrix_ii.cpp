3d6965deaea71ee526b225d6f29b75054f0ededa
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-16 18:37:06 +0530

    Create matrix II solution

--- Day_204/search_a_2d_matrix_ii.cpp
+++ Day_204/search_a_2d_matrix_ii.cpp
@@ -1,26 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool searchMatrix(vector<vector<int>>& matrix, int target) {
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
-    int row = 0;
-    int col = matrix[0].size() - 1;
+        int row = 0;
+        int col = matrix[0].size() - 1;
 
-    while (row < matrix.size() && col >= 0) {
+        while (row < matrix.size() && col >= 0) {
 
-        if (matrix[row][col] == target)
-            return true;
+            if (matrix[row][col] == target)
+                return true;
 
-        if (matrix[row][col] > target)
-            col--;
-        else
-            row++;
+            if (matrix[row][col] > target)
+                col--;
+            else
+                row++;
+        }
+
+        return false;
     }
-
-    return false;
-}
+};
 
 int main() {
+    Solution s;
+
     vector<vector<int>> matrix = {
         {1,  4,  7, 11, 15},
         {2,  5,  8, 12, 19},
@@ -29,7 +34,7 @@ int main() {
         {18, 21, 23, 26, 30}
     };
 
-    cout << boolalpha << searchMatrix(matrix, 5);
+    cout << boolalpha << s.searchMatrix(matrix, 5);
 
     return 0;
 }
