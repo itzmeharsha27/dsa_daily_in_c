fcc9fd069da4302ef0c9a5caf02264e3116d3971
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-17 18:35:52 +0530

    Add brute force capacity search

--- Day_205/capacity_to_ship_packages_within_d_days.cpp
+++ Day_205/capacity_to_ship_packages_within_d_days.cpp
@@ -5,12 +5,26 @@ int main() {
     vector<int> weights = {1,2,3,4,5,6,7,8,9,10};
     int days = 5;
 
-    int total = 0;
+    int left = *max_element(weights.begin(), weights.end());
+    int right = accumulate(weights.begin(), weights.end(), 0);
 
-    for (int w : weights)
-        total += w;
+    for (int capacity = left; capacity <= right; capacity++) {
+        int current = 0;
+        int usedDays = 1;
 
-    cout << "Total weight: " << total << endl;
+        for (int w : weights) {
+            if (current + w > capacity) {
+                usedDays++;
+                current = 0;
+            }
+            current += w;
+        }
+
+        if (usedDays <= days) {
+            cout << "Minimum capacity: " << capacity << endl;
+            break;
+        }
+    }
 
     return 0;
 }
