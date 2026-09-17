131e8810700f2d3a6e27346d3d35a2e535bd77b5
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-17 18:36:03 +0530

    Optimize capacity search with binary search

--- Day_205/capacity_to_ship_packages_within_d_days.cpp
+++ Day_205/capacity_to_ship_packages_within_d_days.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool canShip(vector<int>& weights, int days, int capacity) {
+    int current = 0;
+    int usedDays = 1;
+
+    for (int w : weights) {
+        if (current + w > capacity) {
+            usedDays++;
+            current = 0;
+        }
+
+        current += w;
+    }
+
+    return usedDays <= days;
+}
+
 int main() {
     vector<int> weights = {1,2,3,4,5,6,7,8,9,10};
     int days = 5;
@@ -8,23 +24,16 @@ int main() {
     int left = *max_element(weights.begin(), weights.end());
     int right = accumulate(weights.begin(), weights.end(), 0);
 
-    for (int capacity = left; capacity <= right; capacity++) {
-        int current = 0;
-        int usedDays = 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
 
-        for (int w : weights) {
-            if (current + w > capacity) {
-                usedDays++;
-                current = 0;
-            }
-            current += w;
-        }
-
-        if (usedDays <= days) {
-            cout << "Minimum capacity: " << capacity << endl;
-            break;
-        }
+        if (canShip(weights, days, mid))
+            right = mid - 1;
+        else
+            left = mid + 1;
     }
+
+    cout << "Minimum capacity: " << left << endl;
 
     return 0;
 }
