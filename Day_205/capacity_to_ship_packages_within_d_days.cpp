ed615771859be3b64abadd934d9d9c29843f285a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-17 18:35:15 +0530

    Simulate package shipping by capacity

--- Day_205/capacity_to_ship_packages_within_d_days.cpp
+++ Day_205/capacity_to_ship_packages_within_d_days.cpp
@@ -3,11 +3,21 @@ using namespace std;
 
 int main() {
     vector<int> weights = {1,2,3,4,5,6,7,8,9,10};
-    int days = 5;
+    int capacity = 15;
 
-    int maximum = *max_element(weights.begin(), weights.end());
+    int current = 0;
+    int usedDays = 1;
 
-    cout << "Maximum package weight: " << maximum << endl;
+    for (int w : weights) {
+        if (current + w > capacity) {
+            usedDays++;
+            current = 0;
+        }
+
+        current += w;
+    }
+
+    cout << "Days required: " << usedDays << endl;
 
     return 0;
 }
