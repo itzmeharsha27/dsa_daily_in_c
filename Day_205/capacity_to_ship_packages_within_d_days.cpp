c4c7212adff4c46f3b0c4ef994faab1e83f6b7af
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-17 18:36:19 +0530

    Implement shipping solution class

--- Day_205/capacity_to_ship_packages_within_d_days.cpp
+++ Day_205/capacity_to_ship_packages_within_d_days.cpp
@@ -1,39 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool canShip(vector<int>& weights, int days, int capacity) {
-    int current = 0;
-    int usedDays = 1;
+class Solution {
+public:
+    bool canShip(vector<int>& weights, int days, int capacity) {
+        int current = 0;
+        int usedDays = 1;
 
-    for (int w : weights) {
-        if (current + w > capacity) {
-            usedDays++;
-            current = 0;
+        for (int w : weights) {
+            if (current + w > capacity) {
+                usedDays++;
+                current = 0;
+            }
+
+            current += w;
         }
 
-        current += w;
+        return usedDays <= days;
     }
 
-    return usedDays <= days;
-}
+    int shipWithinDays(vector<int>& weights, int days) {
+        int left = *max_element(weights.begin(), weights.end());
+        int right = accumulate(weights.begin(), weights.end(), 0);
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (canShip(weights, days, mid))
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+};
 
 int main() {
+    Solution s;
+
     vector<int> weights = {1,2,3,4,5,6,7,8,9,10};
-    int days = 5;
 
-    int left = *max_element(weights.begin(), weights.end());
-    int right = accumulate(weights.begin(), weights.end(), 0);
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-
-        if (canShip(weights, days, mid))
-            right = mid - 1;
-        else
-            left = mid + 1;
-    }
-
-    cout << "Minimum capacity: " << left << endl;
+    cout << s.shipWithinDays(weights, 5) << endl;
 
     return 0;
 }
