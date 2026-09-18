0f7ad520d8ab8153d33bfa31a7689c27a4dabefc
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-18 18:08:47 +0530

    Implement solution class

--- Day_206/split_array_largest_sum.cpp
+++ Day_206/split_array_largest_sum.cpp
@@ -1,39 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool canSplit(vector<int>& nums, int k, int limit) {
-    int sum = 0;
-    int parts = 1;
+class Solution {
+public:
+    bool canSplit(vector<int>& nums, int k, int limit) {
+        int sum = 0;
+        int parts = 1;
 
-    for (int x : nums) {
-        if (sum + x > limit) {
-            parts++;
-            sum = 0;
+        for (int x : nums) {
+            if (sum + x > limit) {
+                parts++;
+                sum = 0;
+            }
+
+            sum += x;
         }
 
-        sum += x;
+        return parts <= k;
     }
 
-    return parts <= k;
-}
+    int splitArray(vector<int>& nums, int k) {
+        int left = *max_element(nums.begin(), nums.end());
+        int right = accumulate(nums.begin(), nums.end(), 0);
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (canSplit(nums, k, mid))
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+};
 
 int main() {
+    Solution s;
+
     vector<int> nums = {7,2,5,10,8};
-    int k = 2;
 
-    int left = *max_element(nums.begin(), nums.end());
-    int right = accumulate(nums.begin(), nums.end(), 0);
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-
-        if (canSplit(nums, k, mid))
-            right = mid - 1;
-        else
-            left = mid + 1;
-    }
-
-    cout << left << endl;
+    cout << s.splitArray(nums, 2) << endl;
 
     return 0;
 }
