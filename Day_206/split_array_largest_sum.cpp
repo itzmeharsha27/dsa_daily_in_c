4ad94a89faa4972d882793f028fce0863c4ead20
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-18 18:07:33 +0530

    Add split array problem setup

---
+++ Day_206/split_array_largest_sum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> nums = {7,2,5,10,8};
+    int k = 2;
+
+    cout << "Array: ";
+    for (int x : nums)
+        cout << x << " ";
+
+    cout << "\nK = " << k << endl;
+
+    return 0;
+}
