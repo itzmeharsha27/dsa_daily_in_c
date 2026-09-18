02c4e90422a0a2eb8c9265baa612e572a7e9d358
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-18 18:07:53 +0530

    Count required subarrays

--- Day_206/split_array_largest_sum.cpp
+++ Day_206/split_array_largest_sum.cpp
@@ -3,18 +3,21 @@ using namespace std;
 
 int main() {
     vector<int> nums = {7,2,5,10,8};
-    int k = 2;
+    int capacity = 18;
 
-    int total = 0;
-    int maximum = 0;
+    int sum = 0;
+    int parts = 1;
 
     for (int x : nums) {
-        total += x;
-        maximum = max(maximum, x);
+        if (sum + x > capacity) {
+            parts++;
+            sum = 0;
+        }
+
+        sum += x;
     }
 
-    cout << "Maximum element: " << maximum << endl;
-    cout << "Total sum: " << total << endl;
+    cout << "Required parts: " << parts << endl;
 
     return 0;
 }
