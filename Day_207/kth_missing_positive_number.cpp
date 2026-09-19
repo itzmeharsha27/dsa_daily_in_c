ed708ae26ad0276419786157b112b006cf6a83d4
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-19 20:48:47 +0530

    Find missing positive numbers

--- Day_207/kth_missing_positive_number.cpp
+++ Day_207/kth_missing_positive_number.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main() {
     vector<int> arr = {2,3,4,7,11};
-    int k = 5;
 
-    cout << "Array: ";
-    for (int x : arr)
-        cout << x << " ";
+    int previous = 0;
 
-    cout << "\nK = " << k << endl;
+    for (int x : arr) {
+        for (int n = previous + 1; n < x; n++)
+            cout << n << " ";
+
+        previous = x;
+    }
 
     return 0;
 }
