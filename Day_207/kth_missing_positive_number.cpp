b9e6ce7da63c113a54f4860ddce5ca6c781d138f
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-19 20:49:50 +0530

    Implement solution class

--- Day_207/kth_missing_positive_number.cpp
+++ Day_207/kth_missing_positive_number.cpp
@@ -1,27 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    vector<int> arr = {2,3,4,7,11};
-    int k = 5;
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int left = 0;
+        int right = arr.size() - 1;
 
-    int left = 0;
-    int right = arr.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
+            int missing = arr[mid] - (mid + 1);
 
-        int missing = arr[mid] - (mid + 1);
+            if (missing < k)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
 
-        if (missing < k)
-            left = mid + 1;
-        else
-            right = mid - 1;
+        return left + k;
     }
+};
 
-    int answer = left + k;
+int main() {
+    Solution s;
 
-    cout << answer << endl;
+    vector<int> arr = {2,3,4,7,11};
+
+    cout << s.findKthPositive(arr, 5) << endl;
 
     return 0;
 }
