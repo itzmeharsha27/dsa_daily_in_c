96720196b4e9fb5050c792fab9d920221e90c674
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-19 20:48:57 +0530

    Count missing numbers to find answer

--- Day_207/kth_missing_positive_number.cpp
+++ Day_207/kth_missing_positive_number.cpp
@@ -3,14 +3,28 @@ using namespace std;
 
 int main() {
     vector<int> arr = {2,3,4,7,11};
+    int k = 5;
 
-    int previous = 0;
+    int missing = 0;
 
-    for (int x : arr) {
-        for (int n = previous + 1; n < x; n++)
-            cout << n << " ";
+    for (int n = 1; ; n++) {
+        bool found = false;
 
-        previous = x;
+        for (int x : arr) {
+            if (x == n) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            missing++;
+
+            if (missing == k) {
+                cout << n << endl;
+                break;
+            }
+        }
     }
 
     return 0;
