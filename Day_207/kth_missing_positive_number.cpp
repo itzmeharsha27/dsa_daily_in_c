d475c8f42388e4ea3d1463aff52c052b15d82216
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-19 20:48:36 +0530

    Add kth missing number setup

---
+++ Day_207/kth_missing_positive_number.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {2,3,4,7,11};
+    int k = 5;
+
+    cout << "Array: ";
+    for (int x : arr)
+        cout << x << " ";
+
+    cout << "\nK = " << k << endl;
+
+    return 0;
+}
