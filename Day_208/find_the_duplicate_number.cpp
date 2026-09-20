3010f5721fe6359a68e884fc3f9194990a9edace
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-20 17:50:59 +0530

    Implement duplicate number solution class

--- Day_208/find_the_duplicate_number.cpp
+++ Day_208/find_the_duplicate_number.cpp
@@ -1,25 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
+
 int main() {
+    Solution s;
+
     vector<int> nums = {1,3,4,2,2};
 
-    int slow = nums[0];
-    int fast = nums[0];
-
-    do {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-    } while (slow != fast);
-
-    slow = nums[0];
-
-    while (slow != fast) {
-        slow = nums[slow];
-        fast = nums[fast];
-    }
-
-    cout << "Duplicate: " << slow << endl;
+    cout << s.findDuplicate(nums) << endl;
 
     return 0;
 }
