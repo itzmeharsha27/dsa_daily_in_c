89d27b1d560d8515413c105812c8ce27483a45f7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-20 17:48:55 +0530

    Add duplicate number problem setup

---
+++ Day_208/find_the_duplicate_number.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> nums = {1,3,4,2,2};
+
+    cout << "Array: ";
+    for (int x : nums)
+        cout << x << " ";
+
+    return 0;
+}
