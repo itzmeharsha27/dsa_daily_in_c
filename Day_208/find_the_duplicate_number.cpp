2b17b2235a2b93e2f399d07eabee325c93c2859e
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-20 17:49:07 +0530

    Find duplicate using pair comparison

--- Day_208/find_the_duplicate_number.cpp
+++ Day_208/find_the_duplicate_number.cpp
@@ -4,9 +4,14 @@ using namespace std;
 int main() {
     vector<int> nums = {1,3,4,2,2};
 
-    cout << "Array: ";
-    for (int x : nums)
-        cout << x << " ";
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i + 1; j < nums.size(); j++) {
+            if (nums[i] == nums[j]) {
+                cout << "Duplicate: " << nums[i] << endl;
+                return 0;
+            }
+        }
+    }
 
     return 0;
 }
