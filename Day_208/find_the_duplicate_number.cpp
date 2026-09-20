53fde357a5d0009fb701d6a90a989bd127b0128d
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-20 17:49:39 +0530

    Find duplicate using frequency array

--- Day_208/find_the_duplicate_number.cpp
+++ Day_208/find_the_duplicate_number.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main() {
     vector<int> nums = {1,3,4,2,2};
 
-    for (int i = 0; i < nums.size(); i++) {
-        for (int j = i + 1; j < nums.size(); j++) {
-            if (nums[i] == nums[j]) {
-                cout << "Duplicate: " << nums[i] << endl;
-                return 0;
-            }
+    vector<int> freq(nums.size(), 0);
+
+    for (int x : nums) {
+        freq[x]++;
+
+        if (freq[x] > 1) {
+            cout << "Duplicate: " << x << endl;
+            break;
         }
     }
 
