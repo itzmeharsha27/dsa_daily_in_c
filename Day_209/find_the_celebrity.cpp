c668a8e94a6f964e1cd5f69cf64a5e06905f38e1
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-21 20:56:34 +0530

    Implement celebrity solution class

--- Day_209/find_the_celebrity.cpp
+++ Day_209/find_the_celebrity.cpp
@@ -1,38 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> graph = {
-    {1, 1, 0},
-    {0, 1, 0},
-    {1, 1, 1}
+class Solution {
+public:
+    vector<vector<int>> graph = {
+        {1, 1, 0},
+        {0, 1, 0},
+        {1, 1, 1}
+    };
+
+    bool knows(int a, int b) {
+        return graph[a][b];
+    }
+
+    int findCelebrity(int n) {
+        int candidate = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (knows(candidate, i))
+                candidate = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i == candidate)
+                continue;
+
+            if (knows(candidate, i) || !knows(i, candidate))
+                return -1;
+        }
+
+        return candidate;
+    }
 };
 
-bool knows(int a, int b) {
-    return graph[a][b];
-}
-
-bool isCelebrity(int candidate, int n) {
-    for (int i = 0; i < n; i++) {
-        if (i == candidate)
-            continue;
-
-        if (knows(candidate, i) || !knows(i, candidate))
-            return false;
-    }
-
-    return true;
-}
-
 int main() {
-    int n = 3;
-    int candidate = 0;
+    Solution s;
 
-    for (int i = 1; i < n; i++) {
-        if (knows(candidate, i))
-            candidate = i;
-    }
-
-    cout << (isCelebrity(candidate, n) ? candidate : -1);
+    cout << s.findCelebrity(3);
 
     return 0;
 }
