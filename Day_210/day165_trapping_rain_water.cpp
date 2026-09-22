1766a627acf31b658680e98230ea0fb6cec98374
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-22 18:22:25 +0530

    Add two pointer approach

--- Day_210/day165_trapping_rain_water.cpp
+++ Day_210/day165_trapping_rain_water.cpp
@@ -4,25 +4,22 @@ using namespace std;
 int main() {
     vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
 
-    int n = height.size();
+    int left = 0;
+    int right = height.size() - 1;
 
-    vector<int> leftMax(n);
-    vector<int> rightMax(n);
-
-    leftMax[0] = height[0];
-
-    for (int i = 1; i < n; i++)
-        leftMax[i] = max(leftMax[i - 1], height[i]);
-
-    rightMax[n - 1] = height[n - 1];
-
-    for (int i = n - 2; i >= 0; i--)
-        rightMax[i] = max(rightMax[i + 1], height[i]);
-
+    int leftMax = 0;
+    int rightMax = 0;
     int total = 0;
 
-    for (int i = 0; i < n; i++)
-        total += min(leftMax[i], rightMax[i]) - height[i];
+    while (left < right) {
+        if (height[left] <= height[right]) {
+            leftMax = max(leftMax, height[left]);
+            left++;
+        } else {
+            rightMax = max(rightMax, height[right]);
+            right--;
+        }
+    }
 
     cout << total;
 
