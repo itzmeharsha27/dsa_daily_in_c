0491e80a644e8096fd7ea7e4b88fef19fcd8d926
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-22 18:21:43 +0530

    Calculate trapped water at one position

--- Day_210/day165_trapping_rain_water.cpp
+++ Day_210/day165_trapping_rain_water.cpp
@@ -4,10 +4,20 @@ using namespace std;
 int main() {
     vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
 
-    cout << "Heights: ";
+    int i = 5;
 
-    for (int x : height)
-        cout << x << " ";
+    int leftMax = 0;
+    int rightMax = 0;
+
+    for (int j = 0; j <= i; j++)
+        leftMax = max(leftMax, height[j]);
+
+    for (int j = i; j < height.size(); j++)
+        rightMax = max(rightMax, height[j]);
+
+    int water = min(leftMax, rightMax) - height[i];
+
+    cout << "Water at index " << i << ": " << water;
 
     return 0;
 }
