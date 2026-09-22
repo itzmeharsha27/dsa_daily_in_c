857523fa889d49cc31e26f08e8b2c9dbe66b22a4
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-22 18:23:02 +0530

    Implement trapping rain water solution

--- Day_210/day165_trapping_rain_water.cpp
+++ Day_210/day165_trapping_rain_water.cpp
@@ -1,35 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+        int total = 0;
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax)
+                    leftMax = height[left];
+                else
+                    total += leftMax - height[left];
+
+                left++;
+            } else {
+                if (height[right] >= rightMax)
+                    rightMax = height[right];
+                else
+                    total += rightMax - height[right];
+
+                right--;
+            }
+        }
+
+        return total;
+    }
+};
+
 int main() {
+    Solution s;
+
     vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
 
-    int left = 0;
-    int right = height.size() - 1;
-
-    int leftMax = 0;
-    int rightMax = 0;
-    int total = 0;
-
-    while (left < right) {
-        if (height[left] <= height[right]) {
-            if (height[left] >= leftMax)
-                leftMax = height[left];
-            else
-                total += leftMax - height[left];
-
-            left++;
-        } else {
-            if (height[right] >= rightMax)
-                rightMax = height[right];
-            else
-                total += rightMax - height[right];
-
-            right--;
-        }
-    }
-
-    cout << total;
+    cout << s.trap(height);
 
     return 0;
 }
