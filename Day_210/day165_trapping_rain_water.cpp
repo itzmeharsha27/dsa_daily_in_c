268a0ef89d2672c17ea1cac5423b7a1bcaec47e2
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-22 18:23:15 +0530

    Test trapping rain water solution

--- Day_210/day165_trapping_rain_water.cpp
+++ Day_210/day165_trapping_rain_water.cpp
@@ -6,25 +6,18 @@ public:
     int trap(vector<int>& height) {
         int left = 0;
         int right = height.size() - 1;
-
         int leftMax = 0;
         int rightMax = 0;
         int total = 0;
 
         while (left < right) {
             if (height[left] <= height[right]) {
-                if (height[left] >= leftMax)
-                    leftMax = height[left];
-                else
-                    total += leftMax - height[left];
-
+                leftMax = max(leftMax, height[left]);
+                total += leftMax - height[left];
                 left++;
             } else {
-                if (height[right] >= rightMax)
-                    rightMax = height[right];
-                else
-                    total += rightMax - height[right];
-
+                rightMax = max(rightMax, height[right]);
+                total += rightMax - height[right];
                 right--;
             }
         }
@@ -36,9 +29,13 @@ public:
 int main() {
     Solution s;
 
-    vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
+    vector<int> a = {0,1,0,2,1,0,1,3,2,1,2,1};
+    vector<int> b = {4,2,0,3,2,5};
+    vector<int> c = {1,2,3,4};
 
-    cout << s.trap(height);
+    cout << s.trap(a) << endl;
+    cout << s.trap(b) << endl;
+    cout << s.trap(c) << endl;
 
     return 0;
 }
