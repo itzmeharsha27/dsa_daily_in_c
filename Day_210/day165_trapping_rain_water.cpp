53c6099b6cba16083bc9b1e5742dc78d501192e7
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-22 18:21:31 +0530

    Add trapping rain water setup

---
+++ Day_210/day165_trapping_rain_water.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
+
+    cout << "Heights: ";
+
+    for (int x : height)
+        cout << x << " ";
+
+    return 0;
+}
