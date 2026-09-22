c288cd8f2ee52dc3c9e78973a21df5c05e2345ea
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-22 18:21:58 +0530

    Add brute force water calculation

--- Day_210/day165_trapping_rain_water.cpp
+++ Day_210/day165_trapping_rain_water.cpp
@@ -4,20 +4,22 @@ using namespace std;
 int main() {
     vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
 
-    int i = 5;
+    int total = 0;
 
-    int leftMax = 0;
-    int rightMax = 0;
+    for (int i = 0; i < height.size(); i++) {
+        int leftMax = 0;
+        int rightMax = 0;
 
-    for (int j = 0; j <= i; j++)
-        leftMax = max(leftMax, height[j]);
+        for (int j = 0; j <= i; j++)
+            leftMax = max(leftMax, height[j]);
 
-    for (int j = i; j < height.size(); j++)
-        rightMax = max(rightMax, height[j]);
+        for (int j = i; j < height.size(); j++)
+            rightMax = max(rightMax, height[j]);
 
-    int water = min(leftMax, rightMax) - height[i];
+        total += min(leftMax, rightMax) - height[i];
+    }
 
-    cout << "Water at index " << i << ": " << water;
+    cout << "Trapped water: " << total;
 
     return 0;
 }
