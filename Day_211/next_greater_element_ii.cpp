21e9cb98313a42bfb6e43cf12029fbd70b63a4d4
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-23 18:14:34 +0530

    Implement solution class

--- Day_211/next_greater_element_ii.cpp
+++ Day_211/next_greater_element_ii.cpp
@@ -1,26 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    vector<int> nums = {1,2,1};
-    int n = nums.size();
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
 
-    vector<int> result(n, -1);
-    stack<int> st;
+        vector<int> result(n, -1);
+        stack<int> st;
 
-    for (int i = 2 * n - 1; i >= 0; i--) {
-        int index = i % n;
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int index = i % n;
 
-        while (!st.empty() && st.top() <= nums[index])
-            st.pop();
+            while (!st.empty() && st.top() <= nums[index])
+                st.pop();
 
-        if (i < n && !st.empty())
-            result[index] = st.top();
+            if (i < n && !st.empty())
+                result[index] = st.top();
 
-        st.push(nums[index]);
+            st.push(nums[index]);
+        }
+
+        return result;
     }
+};
 
-    cout << "Result: ";
+int main() {
+    Solution s;
+
+    vector<int> nums = {1,2,1};
+
+    vector<int> result = s.nextGreaterElements(nums);
 
     for (int x : result)
         cout << x << " ";
