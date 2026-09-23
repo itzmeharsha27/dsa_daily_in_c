3c775b602d5529e32ab5d4f46de7bd4f8f66fcb9
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-23 18:11:08 +0530

    Add next greater element setup

---
+++ Day_211/next_greater_element_ii.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> nums = {1,2,1};
+
+    cout << "Array: ";
+
+    for (int x : nums)
+        cout << x << " ";
+
+    return 0;
+}
