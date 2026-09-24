689520d850da032602dcd6978178b0510aaeb38a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-24 17:41:55 +0530

    Implement maximal rectangle solution

--- Day_212/maximal_rectangle.cpp
+++ Day_212/maximal_rectangle.cpp
@@ -1,29 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int largestRectangle(vector<int>& height) {
-    stack<int> st;
-    int best = 0;
+class Solution {
+public:
+    int largestRectangle(vector<int>& height) {
+        stack<int> st;
+        int best = 0;
 
-    for (int i = 0; i <= height.size(); i++) {
-        int current = (i == height.size()) ? 0 : height[i];
+        for (int i = 0; i <= height.size(); i++) {
+            int current = (i == height.size()) ? 0 : height[i];
 
-        while (!st.empty() && height[st.top()] > current) {
-            int h = height[st.top()];
-            st.pop();
+            while (!st.empty() && height[st.top()] > current) {
+                int h = height[st.top()];
+                st.pop();
 
-            int width = st.empty() ? i : i - st.top() - 1;
+                int width = st.empty() ? i : i - st.top() - 1;
 
-            best = max(best, h * width);
+                best = max(best, h * width);
+            }
+
+            st.push(i);
         }
 
-        st.push(i);
+        return best;
     }
 
-    return best;
-}
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty())
+            return 0;
+
+        int cols = matrix[0].size();
+        vector<int> height(cols, 0);
+        int answer = 0;
+
+        for (auto& row : matrix) {
+            for (int c = 0; c < cols; c++) {
+                if (row[c] == '1')
+                    height[c]++;
+                else
+                    height[c] = 0;
+            }
+
+            answer = max(answer, largestRectangle(height));
+        }
+
+        return answer;
+    }
+};
 
 int main() {
+    Solution s;
+
     vector<vector<char>> matrix = {
         {'1','0','1','0','0'},
         {'1','0','1','1','1'},
@@ -31,23 +58,7 @@ int main() {
         {'1','0','0','1','0'}
     };
 
-    int cols = matrix[0].size();
-    vector<int> height(cols, 0);
-
-    int answer = 0;
-
-    for (auto& row : matrix) {
-        for (int c = 0; c < cols; c++) {
-            if (row[c] == '1')
-                height[c]++;
-            else
-                height[c] = 0;
-        }
-
-        answer = max(answer, largestRectangle(height));
-    }
-
-    cout << answer;
+    cout << s.maximalRectangle(matrix);
 
     return 0;
 }
