9c4cc9117920c1d1e461584f7070ee96ea13f405
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-25 17:46:39 +0530

    Identify asteroid collision condition

--- Day_213/asteroid_collision.cpp
+++ Day_213/asteroid_collision.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main() {
     vector<int> asteroids = {5, 10, -5};
 
-    cout << "Asteroids: ";
-
-    for (int x : asteroids)
-        cout << x << " ";
+    for (int i = 0; i < asteroids.size() - 1; i++) {
+        if (asteroids[i] > 0 && asteroids[i + 1] < 0) {
+            cout << "Collision possible between ";
+            cout << asteroids[i] << " and " << asteroids[i + 1];
+        }
+    }
 
     return 0;
 }
