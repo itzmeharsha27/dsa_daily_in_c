552a05ab9a0d6c84b08fd1840abc85d767df256c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-25 17:45:51 +0530

    Add asteroid collision setup

---
+++ Day_213/asteroid_collision.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> asteroids = {5, 10, -5};
+
+    cout << "Asteroids: ";
+
+    for (int x : asteroids)
+        cout << x << " ";
+
+    return 0;
+}
