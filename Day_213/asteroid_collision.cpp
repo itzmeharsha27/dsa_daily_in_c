6ba150191534f7a2376b0d0da34b9f55ae6ef2ef
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-25 17:52:05 +0530

    Implement asteroid collision solution

--- Day_213/asteroid_collision.cpp
+++ Day_213/asteroid_collision.cpp
@@ -1,45 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> collide(vector<int>& asteroids) {
-    stack<int> st;
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
 
-    for (int asteroid : asteroids) {
-        bool destroyed = false;
+        for (int asteroid : asteroids) {
+            bool destroyed = false;
 
-        while (!st.empty() && st.top() > 0 && asteroid < 0) {
-            if (st.top() < abs(asteroid)) {
-                st.pop();
-            } else if (st.top() == abs(asteroid)) {
-                st.pop();
-                destroyed = true;
-                break;
-            } else {
-                destroyed = true;
-                break;
+            while (!st.empty() && st.top() > 0 && asteroid < 0) {
+                if (st.top() < abs(asteroid)) {
+                    st.pop();
+                } else if (st.top() == abs(asteroid)) {
+                    st.pop();
+                    destroyed = true;
+                    break;
+                } else {
+                    destroyed = true;
+                    break;
+                }
             }
+
+            if (!destroyed)
+                st.push(asteroid);
         }
 
-        if (!destroyed)
-            st.push(asteroid);
+        vector<int> result;
+
+        while (!st.empty()) {
+            result.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(result.begin(), result.end());
+
+        return result;
     }
-
-    vector<int> result;
-
-    while (!st.empty()) {
-        result.push_back(st.top());
-        st.pop();
-    }
-
-    reverse(result.begin(), result.end());
-
-    return result;
-}
+};
 
 int main() {
+    Solution s;
+
     vector<int> asteroids = {5, 10, -5};
 
-    vector<int> result = collide(asteroids);
+    vector<int> result = s.asteroidCollision(asteroids);
 
     for (int x : result)
         cout << x << " ";
