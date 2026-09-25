573175e0591e2172b7385a12bd23c54927cef8ed
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-25 17:51:16 +0530

    Simulate basic asteroid collision

--- Day_213/asteroid_collision.cpp
+++ Day_213/asteroid_collision.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 int main() {
-    vector<int> asteroids = {5, 10, -5};
+    int right = 10;
+    int left = -5;
 
-    for (int i = 0; i < asteroids.size() - 1; i++) {
-        if (asteroids[i] > 0 && asteroids[i + 1] < 0) {
-            cout << "Collision possible between ";
-            cout << asteroids[i] << " and " << asteroids[i + 1];
-        }
-    }
+    if (right > abs(left))
+        cout << right;
+    else if (right < abs(left))
+        cout << left;
+    else
+        cout << "Both destroyed";
 
     return 0;
 }
