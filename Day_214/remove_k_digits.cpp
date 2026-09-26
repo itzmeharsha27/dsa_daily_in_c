dfed0b49ff69c9231166c264fd88adf0171b311c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-26 18:02:15 +0530

    Find digit that increases number

--- Day_214/remove_k_digits.cpp
+++ Day_214/remove_k_digits.cpp
@@ -4,9 +4,12 @@ using namespace std;
 int main() {
     string num = "1432219";
 
-    int removeIndex = 0;
-
-    num.erase(removeIndex, 1);
+    for (int i = 0; i < num.size() - 1; i++) {
+        if (num[i] > num[i + 1]) {
+            num.erase(i, 1);
+            break;
+        }
+    }
 
     cout << num << endl;
 
