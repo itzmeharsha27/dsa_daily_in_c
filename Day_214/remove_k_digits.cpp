9fdff5631a7c3ae52970d14c37951bb9c2513a0c
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-26 18:02:27 +0530

    Repeat greedy digit removal

--- Day_214/remove_k_digits.cpp
+++ Day_214/remove_k_digits.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 int main() {
     string num = "1432219";
+    int k = 3;
 
-    for (int i = 0; i < num.size() - 1; i++) {
-        if (num[i] > num[i + 1]) {
-            num.erase(i, 1);
-            break;
+    while (k > 0) {
+        int index = num.size() - 1;
+
+        for (int i = 0; i < num.size() - 1; i++) {
+            if (num[i] > num[i + 1]) {
+                index = i;
+                break;
+            }
         }
+
+        num.erase(index, 1);
+        k--;
     }
 
     cout << num << endl;
