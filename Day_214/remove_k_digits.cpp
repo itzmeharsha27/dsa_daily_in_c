aa6af6aaccb3f2ae06c518cbe9d75eb0546b6da2
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-26 18:01:49 +0530

    Add remove digits problem setup

---
+++ Day_214/remove_k_digits.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string num = "1432219";
+    int k = 3;
+
+    cout << "Number: " << num << endl;
+    cout << "Digits to remove: " << k << endl;
+
+    return 0;
+}
