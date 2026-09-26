2c6d8616cbb51e65019a925655047d908056d806
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-09-26 18:02:06 +0530

    Add single digit removal

--- Day_214/remove_k_digits.cpp
+++ Day_214/remove_k_digits.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main() {
     string num = "1432219";
-    int k = 3;
 
-    cout << "Number: " << num << endl;
-    cout << "Digits to remove: " << k << endl;
+    int removeIndex = 0;
+
+    num.erase(removeIndex, 1);
+
+    cout << num << endl;
 
     return 0;
 }
